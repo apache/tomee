7f0c3512c9e26ce19f85b4c6309fa76af14bd96c
David Blevins <dblevins@apache.org>
2006-01-03 06:46:29 +0000

    removed dependence on the openejb-core jar
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/branches/v3_0_exp/trunk@445905 13f79535-47bb-0310-9956-ffa450edef68

--- itest/openejb-itests/src/main/java/org/openejb/test/Main.java
+++ itest/openejb-itests/src/main/java/org/openejb/test/Main.java
@@ -1,26 +1,18 @@
 package org.openejb.test;
 
-import java.io.File;
+public class Main {
 
-import org.openejb.loader.SystemInstance;
+    public static void main(String args[]) {
+//        try{
+//            File directory = SystemInstance.get().getHome().getDirectory("lib");
+//            SystemInstance.get().getClassPath().addJarsToPath(directory);
+//            File directory1 = SystemInstance.get().getHome().getDirectory("dist");
+//            SystemInstance.get().getClassPath().addJarsToPath(directory1);
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
 
-
-/**
- * @author <a href="mailto:david.blevins@visi.com">David Blevins</a>
- */
-public class Main{
-
-    public static void main (String args[]) {
-        try{
-            File directory = SystemInstance.get().getHome().getDirectory("lib");
-            SystemInstance.get().getClassPath().addJarsToPath(directory);
-            File directory1 = SystemInstance.get().getHome().getDirectory("dist");
-            SystemInstance.get().getClassPath().addJarsToPath(directory1);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        TestRunner.main( args );
+        TestRunner.main(args);
     }
 
 }
--- itest/openejb-itests/src/main/java/org/openejb/test/RemoteTestServer.java
+++ itest/openejb-itests/src/main/java/org/openejb/test/RemoteTestServer.java
@@ -10,54 +10,52 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.Properties;
 
-import org.openejb.util.JarUtils;
-
 /**
- * 
  * @author <a href="mailto:david.blevins@visi.com">David Blevins</a>
  */
 public class RemoteTestServer implements org.openejb.test.TestServer {
-    
-    static{
+
+    static {
         System.setProperty("noBanner", "true");
     }
-    
+
     /**
      * Has the remote server's instance been already running ?
      */
     private boolean serverHasAlreadyBeenStarted = true;
-    
+
     private Properties properties;
-    
-    public void init(Properties props){
+
+    public void init(Properties props) {
         properties = props;
-        
-        props.put("test.server.class","org.openejb.test.RemoteTestServer");
-        props.put("java.naming.factory.initial","org.openejb.client.RemoteInitialContextFactory");
-        props.put("java.naming.provider.url","127.0.0.1:4201");
-        props.put("java.naming.security.principal","testuser");
-        props.put("java.naming.security.credentials","testpassword");
-        
-        
+
+        props.put("test.server.class", "org.openejb.test.RemoteTestServer");
+        props.put("java.naming.factory.initial", "org.openejb.client.RemoteInitialContextFactory");
+        props.put("java.naming.provider.url", "127.0.0.1:4201");
+        props.put("java.naming.security.principal", "testuser");
+        props.put("java.naming.security.credentials", "testpassword");
+
+
     }
-    
-    public void destroy(){
+
+    public void destroy() {
     }
-    
-    public void start(){
+
+    public void start() {
         if (!connect()) {
-            try{
+            try {
                 System.out.println("[] START SERVER");
                 serverHasAlreadyBeenStarted = false;
                 String version = null;
-                
-                JarUtils.setHandlerSystemProperty();
+
+                URL resource = this.getClass().getResource("openejb-version.properties");
+
                 Properties versionInfo = new Properties();
-                versionInfo.load( new URL( "resource:/openejb-version.properties" ).openConnection().getInputStream() );
-                version = (String)versionInfo.get( "version" );
-                
+                versionInfo.load(resource.openConnection().getInputStream());
+                version = (String) versionInfo.get("version");
+
                 Process server = Runtime.getRuntime().exec("java -jar lib" + File.separator + "openejb-core-" + version + ".jar start -nowait");
-                
+
                 // Pipe the processes STDOUT to ours
                 InputStream out = server.getInputStream();
                 Thread serverOut = new Thread(new Pipe(out, System.out));
@@ -65,15 +63,13 @@ public class RemoteTestServer implements org.openejb.test.TestServer {
                 serverOut.setDaemon(true);
                 serverOut.start();
 
-
-
                 // Pipe the processes STDERR to ours
                 InputStream err = server.getErrorStream();
                 Thread serverErr = new Thread(new Pipe(err, System.err));
 
                 serverErr.setDaemon(true);
                 serverErr.start();
-            } catch (Exception e){
+            } catch (Exception e) {
                 throw new RuntimeException("Cannot start the server.");
             }
             connect(10);
@@ -84,114 +80,113 @@ public class RemoteTestServer implements org.openejb.test.TestServer {
 
     private void oldStart() throws IOException, FileNotFoundException {
         String s = java.io.File.separator;
-        String java = System.getProperty("java.home")+s+"bin"+s+"java";
+        String java = System.getProperty("java.home") + s + "bin" + s + "java";
         String classpath = System.getProperty("java.class.path");
         String openejbHome = System.getProperty("openejb.home");
-        
+
 
         String[] cmd = new String[ 5 ];
-        cmd[ 0 ] = java;
-        cmd[ 1 ] = "-classpath";
-        cmd[ 2 ] = classpath;
-        cmd[ 3 ] = "-Dopenejb.home="+openejbHome;
-        cmd[ 4 ] = "org.openejb.server.Main";
-        for (int i=0; i < cmd.length; i++){
+        cmd[0] = java;
+        cmd[1] = "-classpath";
+        cmd[2] = classpath;
+        cmd[3] = "-Dopenejb.home=" + openejbHome;
+        cmd[4] = "org.openejb.server.Main";
+        for (int i = 0; i < cmd.length; i++) {
             //System.out.println("[] "+cmd[i]);
         }
-        
-        Process remoteServerProcess = Runtime.getRuntime().exec( cmd );
-        
+
+        Process remoteServerProcess = Runtime.getRuntime().exec(cmd);
+
         // it seems as if OpenEJB wouldn't start up till the output stream was read
         final java.io.InputStream is = remoteServerProcess.getInputStream();
         final java.io.OutputStream out = new FileOutputStream("logs/testsuite.out");
-        Thread serverOut = new Thread(new Runnable(){
-                public void run() {
-                    try{
-                        //while ( is.read() != -1 );
-                        int i = is.read();
-                        out.write( i );
-                        while ( i != -1 ){
-                            //System.out.write( i );
-                            i = is.read();
-                            out.write( i );
-                        }
-                    } catch (Exception e){
-                        e.printStackTrace();
+        Thread serverOut = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    //while ( is.read() != -1 );
+                    int i = is.read();
+                    out.write(i);
+                    while (i != -1) {
+                        //System.out.write( i );
+                        i = is.read();
+                        out.write(i);
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
         });
         serverOut.setDaemon(true);
         serverOut.start();
-        
+
         final java.io.InputStream is2 = remoteServerProcess.getErrorStream();
-        Thread serverErr = new Thread(new Runnable(){
-                public void run() {
-                    try{
-                        //while ( is.read() != -1 );
-                        int i = is2.read();
-                        out.write( i );
-                        while ( i != -1 ){
-                            //System.out.write( i );
-                            i = is2.read();
-                            out.write( i );
-                        }
-                    } catch (Exception e){
-                        e.printStackTrace();
+        Thread serverErr = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    //while ( is.read() != -1 );
+                    int i = is2.read();
+                    out.write(i);
+                    while (i != -1) {
+                        //System.out.write( i );
+                        i = is2.read();
+                        out.write(i);
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
         });
         serverErr.setDaemon(true);
         serverErr.start();
     }
 
-    public void stop(){
-        if ( !serverHasAlreadyBeenStarted ) {
-            try{
+    public void stop() {
+        if (!serverHasAlreadyBeenStarted) {
+            try {
                 System.out.println("[] STOP SERVER");
-        
+
                 Socket socket = new Socket("localhost", 4200);
                 OutputStream out = socket.getOutputStream();
-        
-                out.write( "Stop".getBytes() );
 
-            } catch (Exception e){
+                out.write("Stop".getBytes());
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public Properties getContextEnvironment(){
-        return (Properties)properties.clone();
+    public Properties getContextEnvironment() {
+        return (Properties) properties.clone();
     }
 
     private boolean connect() {
-        return connect( 1 );
+        return connect(1);
     }
-    
+
     private boolean connect(int tries) {
         //System.out.println("CONNECT "+ tries);
-        try{
+        try {
             Socket socket = new Socket("localhost", 4200);
             OutputStream out = socket.getOutputStream();
-        } catch (Exception e){
+        } catch (Exception e) {
             //System.out.println(e.getMessage());
-            if ( tries < 2 ) {
+            if (tries < 2) {
                 return false;
             } else {
-                try{
+                try {
                     Thread.sleep(2000);
-                } catch (Exception e2){
+                } catch (Exception e2) {
                     e.printStackTrace();
                 }
                 return connect(--tries);
             }
         }
-        
+
         return true;
     }
 
     private static final class Pipe implements Runnable {
-
 
 
         private final InputStream is;
@@ -210,21 +205,21 @@ public class RemoteTestServer implements org.openejb.test.TestServer {
 
         public void run() {
 
-            try{
+            try {
 
                 int i = is.read();
 
-                out.write( i );
+                out.write(i);
 
-                while ( i != -1 ){
+                while (i != -1) {
 
                     i = is.read();
 
-                    out.write( i );
+                    out.write(i);
 
                 }
 
-            } catch (Exception e){
+            } catch (Exception e) {
 
                 e.printStackTrace();
 
