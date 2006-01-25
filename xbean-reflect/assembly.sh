892d4350c70cc1d27506bd338c1d22e4eaac6c89
David Blevins <dblevins@apache.org>
2006-01-25 00:23:51 +0000

    don't need these
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/branches/v3_0_exp/trunk@445945 13f79535-47bb-0310-9956-ffa450edef68

--- xbean-reflect/assembly.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn clean:clean assembly:assembly && tar tzvf target/openejb-3.0-SNAPSHOT-bin.tar.gz
--- xbean-reflect/try.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd target/
-
-tar xzvf openejb-3.0-SNAPSHOT-bin.tar.gz
-cd openejb-3.0-SNAPSHOT
-./bin/openejb start
-
