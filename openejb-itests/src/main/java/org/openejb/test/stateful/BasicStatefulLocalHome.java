61d7ce0c50767a07fbd4ecf67437fc2d6de57221
Jacek Laskowski <jlaskowski@apache.org>
2006-09-23 14:01:03 +0000

    Remaining files from OPENEJB-172 - iTest: StatefulLocalJndiTests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb3@449245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openejb-itests/src/main/java/org/openejb/test/stateful/BasicStatefulLocalHome.java
@@ -0,0 +1,34 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openejb.test.stateful;
+
+
+/**
+ * The local home interface for BasicStatefulPojoBean.
+ * 
+ * @author <a href="mailto:david.blevins@visi.com">David Blevins</a>
+ * @author <a href="mailto:Richard@Monson-Haefel.com">Richard Monson-Haefel</a>
+ * @author <a href="mailto:nour.mohammad@gmail.com">Mohammad Nour El-Din</a>
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface BasicStatefulLocalHome extends javax.ejb.EJBLocalHome {
+
+    public BasicStatefulLocalObject create(String name) throws javax.ejb.CreateException;
+    
+}
+
---
+++ openejb-itests/src/main/java/org/openejb/test/stateful/BasicStatefulLocalObject.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openejb.test.stateful;
+
+import java.util.Properties;
+
+import org.openejb.test.ApplicationException;
+import org.openejb.test.object.OperationsPolicy;
+
+/**
+ * The local interface for BasicStatefulPojoBean.
+ * 
+ * @author <a href="mailto:david.blevins@visi.com">David Blevins</a>
+ * @author <a href="mailto:Richard@Monson-Haefel.com">Richard Monson-Haefel</a>
+ * @author <a href="mailto:nour.mohammad@gmail.com">Mohammad Nour El-Din</a>
+ * 
+ * @version $Rev$ $Date$
+ */
+public interface BasicStatefulLocalObject extends javax.ejb.EJBLocalObject {
+    
+    /**
+     * Reverses the string passed in then returns it
+     * 
+     */
+    public String businessMethod(String text);
+    
+    /**
+     * Throws an ApplicationException when invoked
+     * 
+     */
+    public void throwApplicationException() throws ApplicationException;
+    
+    /**
+     * Throws a java.lang.NullPointerException when invoked
+     * This is a system exception and should result in the 
+     * destruction of the instance and invalidation of the
+     * remote reference.
+     * 
+     */
+    public void throwSystemException_NullPointer();
+    
+    /**
+     * Returns a report of the bean's 
+     * runtime permissions
+     * 
+     */
+    public Properties getPermissionsReport();
+    
+    /**
+     * Returns a report of the allowed opperations
+     * for one of the bean's methods.
+     * 
+     * @param methodName The method for which to get the allowed opperations report
+     */
+    public OperationsPolicy getAllowedOperationsReport(String methodName);
+}
