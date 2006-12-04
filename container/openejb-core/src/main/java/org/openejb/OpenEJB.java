06788a847dac393d2de492b20e23a36fae587bed
David Blevins <dblevins@apache.org>
2006-12-04 16:44:10 +0000

    Intellij hadn't fully written this to disk on my last commit.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb3@482239 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/openejb/OpenEJB.java
+++ container/openejb-core/src/main/java/org/openejb/OpenEJB.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.openejb.client;
+package org.openejb;
 
 import org.apache.openejb.spi.ApplicationServer;
 
