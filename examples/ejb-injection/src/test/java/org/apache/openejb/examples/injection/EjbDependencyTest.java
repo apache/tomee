556df44535b75d99c729c0650cf51d8dfc04dd3f
Jeremy Whitlock <jwhitlock@apache.org>
2006-12-21 20:43:12 +0000

    Patch from Filippo Diotalevi (OPENEJB-357): Example: Using @EJB to refer to other EJBs. Thanks Filippo for your excellent example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb3@489456 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/ejb-injection/src/test/java/org/apache/openejb/examples/injection/EjbDependencyTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.examples.injection;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.openejb.examples.injection.DataReaderLocal;
+import org.apache.openejb.examples.injection.DataReaderRemote;
+
+import junit.framework.TestCase;
+
+/**
+ *  A test case for DataReaderImpl ejb, testing both the remote and local interface 
+ *
+ */
+public class EjbDependencyTest extends TestCase {
+	
+	private static final String REMOTE_STORE_RESULT = "REMOTE:42";
+	private static final String LOCAL_STORE_RESULT = "LOCAL:42";
+	
+	private InitialContext initialContext;
+
+    protected void setUp() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+        properties.setProperty("openejb.deployments.classpath.include", ".*injection.*");
+
+        initialContext = new InitialContext(properties);
+    }
+
+    public void testViaLocalInterface() throws Exception {
+    	Object object = initialContext.lookup("DataReaderImplBusinessLocal");
+    	
+    	assertNotNull(object);
+    	assertEquals(LOCAL_STORE_RESULT, ((DataReaderLocal)object).readDataFromLocalStore());
+    	assertEquals(REMOTE_STORE_RESULT, ((DataReaderLocal)object).readDataFromRemoteStore());
+    }
+    
+    public void testViaRemoteInterface() throws Exception {
+    	Object object = initialContext.lookup("DataReaderImplBusinessRemote");
+    	
+    	assertNotNull(object);
+    	assertEquals(LOCAL_STORE_RESULT, ((DataReaderRemote)object).readDataFromLocalStore());
+    	assertEquals(REMOTE_STORE_RESULT, ((DataReaderRemote)object).readDataFromRemoteStore());
+    }
+
+}
