6896a89bbcdb16dad83f2ba75a56565c532bdf87
Jeff Genender <jgenender@apache.org>
2007-03-22 17:44:24 +0000

    Wrong package name
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb3@521368 13f79535-47bb-0310-9956-ffa450edef68

--- server/openejb-security/src/test/java/org/apache/openejb/server/security/PropertiesLoginModuleTest.java
+++ server/openejb-security/src/test/java/org/apache/openejb/server/security/PropertiesLoginModuleTest.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.openejb.core.security;
+package org.apache.openejb.server.security;
 
 import junit.framework.TestCase;
 import org.apache.openejb.server.security.GroupPrincipal;
