e1caa9a3fbae7161b398cdbb01b59aafcb021a58
Jacek Laskowski <jlaskowski@apache.org>
2007-04-24 08:17:26 +0000

    OPENEJB-568 JAX-RPC integration improvements
    Thanks Jarek Gawor!
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb3@531824 13f79535-47bb-0310-9956-ffa450edef68

--- server/openejb-axis/src/main/java/org/apache/openejb/server/axis/EjbContainerProvider.java
+++ server/openejb-axis/src/main/java/org/apache/openejb/server/axis/EjbContainerProvider.java
@@ -17,15 +17,19 @@
 package org.apache.openejb.server.axis;
 
 import org.apache.axis.AxisFault;
+import org.apache.axis.Constants;
 import org.apache.axis.Handler;
 import org.apache.axis.Message;
 import org.apache.axis.MessageContext;
 import org.apache.axis.description.OperationDesc;
 import org.apache.axis.description.ParameterDesc;
 import org.apache.axis.description.ServiceDesc;
+import org.apache.axis.encoding.XMLType;
+import org.apache.axis.encoding.DeserializationContext;
 import org.apache.axis.message.RPCElement;
 import org.apache.axis.message.RPCParam;
 import org.apache.axis.message.SOAPEnvelope;
+import org.apache.axis.message.SOAPBodyElement;
 import org.apache.axis.message.SOAPFault;
 import org.apache.axis.providers.java.RPCProvider;
 import org.apache.axis.utils.JavaUtils;
@@ -34,6 +38,7 @@ import org.apache.openejb.DeploymentInfo;
 import org.apache.openejb.InvalidateReferenceException;
 import org.apache.openejb.RpcContainer;
 import org.xml.sax.SAXException;
+import org.xml.sax.InputSource;
 
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
@@ -42,10 +47,16 @@ import javax.xml.rpc.handler.HandlerInfo;
 import javax.xml.rpc.holders.IntHolder;
 import javax.xml.rpc.soap.SOAPFaultException;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.namespace.QName;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
+/**
+ * @version $Rev$ $Date$
+ */
 public class EjbContainerProvider extends RPCProvider {
 
     private final DeploymentInfo ejbDeployment;
@@ -60,7 +71,7 @@ public class EjbContainerProvider extends RPCProvider {
         this.ejbDeployment = ejbDeployment;
         this.handlerInfos = handlerInfos;
     }
-
+    
     public void processMessage(MessageContext msgContext, SOAPEnvelope reqEnv, SOAPEnvelope resEnv, Object obj) throws Exception {
 
         RPCElement body = getBody(reqEnv, msgContext);
@@ -78,7 +89,7 @@ public class EjbContainerProvider extends RPCProvider {
             Object[] arguments = {msgContext, interceptor};
 
             Object result = container.invoke(ejbDeployment.getDeploymentID(), operation.getMethod(), arguments, null);
-
+            
             interceptor.createResult(result);
         } catch (InvalidateReferenceException e) {
             interceptor.createExceptionResult(e.getCause());
@@ -108,18 +119,24 @@ public class EjbContainerProvider extends RPCProvider {
             this.messageContext = msgContext;
             this.operation = operation;
         }
-
+        
         @AroundInvoke
         public Object intercept(InvocationContext context) throws Exception {
-            context.setParameters(getArguments());
-
             HandlerChain handlerChain = new HandlerChainImpl(handlerInfos);
             try {
                 Object invocationResult = null;
 
                 try {
                     if (handlerChain.handleRequest(messageContext)) {
+                        // update arguments as handlers could change the soap msg
+                        context.setParameters(getArguments());
+
                         invocationResult = context.proceed();
+
+                        // update the soap msg so that handlers see invocation result
+                        if (!handlerChain.isEmpty()) {
+                            createResult(invocationResult);
+                        }
 
                     } else {
                         /* The Handler implementation class has the responsibility of setting
@@ -134,13 +151,25 @@ public class EjbContainerProvider extends RPCProvider {
                 }
 
                 handlerChain.handleResponse(messageContext);
+                
+                if (!handlerChain.isEmpty()) {
+                    /* 
+                     * Deserialize the result value from soap msg as handers could have 
+                     * changed it.
+                     */
+                    try {
+                        invocationResult = demarshallResult();
+                    } catch (Exception e) {
+                        // if this fails, return invocationResult from above
+                    }
+                }
 
                 return invocationResult;
             } finally {
                 handlerChain.destroy();
             }
         }
-
+        
         public Object[] getArguments() {
             try {
                 return demarshallArguments();
@@ -148,13 +177,14 @@ public class EjbContainerProvider extends RPCProvider {
                 throw (IllegalStateException) new IllegalStateException("Cannot demarshal the soap parts into arguments").initCause(e);
             }
         }
-
+        
         private Object[] demarshallArguments() throws Exception {
             SOAPMessage message = messageContext.getMessage();
             messageContext.setProperty(org.apache.axis.SOAPPart.ALLOW_FORM_OPTIMIZATION, Boolean.TRUE);
             if (message != null) {
                 message.saveChanges();
             }
+
             try {
                 Message reqMsg = messageContext.getRequestMessage();
                 SOAPEnvelope requestEnvelope = reqMsg.getSOAPEnvelope();
@@ -175,7 +205,7 @@ public class EjbContainerProvider extends RPCProvider {
                 for (int i = 0; i < args.size(); i++) {
                     RPCParam rpcParam = (RPCParam) args.get(i);
                     Object value = rpcParam.getObjectValue();
-
+                    
                     ParameterDesc paramDesc = rpcParam.getParamDesc();
 
                     if (paramDesc != null && paramDesc.getJavaType() != null) {
@@ -191,6 +221,79 @@ public class EjbContainerProvider extends RPCProvider {
             }
         }
 
+        private Object demarshallResult() throws Exception {
+            Message resMsg = messageContext.getResponseMessage();
+            
+            /*
+             * This is not the most efficient way to deserialize the result
+             * but could not find better or more reliable way to do this.
+             */
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            resMsg.writeTo(out);
+            ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+            
+            DeserializationContext dser = 
+                new DeserializationContext(new InputSource(in), resMsg.getMessageContext(), null);
+            dser.parse();
+            SOAPEnvelope responseEnvelope = dser.getEnvelope();
+            
+            SOAPBodyElement bodyEl = responseEnvelope.getFirstBody();
+            if (bodyEl == null) {
+                return null;
+            }
+            
+            QName returnType = operation.getReturnType();
+            if (XMLType.AXIS_VOID.equals(returnType)) {
+                return null;
+            }
+
+            Object result = null;
+
+            if (bodyEl instanceof RPCElement) {
+                RPCElement body = (RPCElement)bodyEl;
+                body.setNeedDeser(true);
+                Vector args = null;
+                try {
+                    args = body.getParams();
+                } catch (SAXException e) {
+                    if (e.getException() != null) {
+                        throw e.getException();
+                    }
+                    throw e;
+                }
+
+                QName returnParamQName = operation.getReturnQName();
+                if (args != null && args.size() > 0) {
+
+                    if (returnParamQName == null) {
+                        RPCParam param = (RPCParam) args.get(0);
+                        result = param.getObjectValue();
+                    } else {
+                        for (int i = 0; i < args.size(); i++) {
+                            RPCParam param = (RPCParam) args.get(i);
+                            if (returnParamQName.equals(param.getQName())) {
+                                result = param.getObjectValue();
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            } else {
+                try {
+                    result = bodyEl.getValueAsType(returnType);
+                } catch (Exception e) {
+                    result = bodyEl;
+                }
+            }
+
+            if (operation.getReturnClass() != null) {
+                result = JavaUtils.convert(result, operation.getReturnClass());
+            }
+            
+            return result;
+        }
+
         public void createResult(Object object) {
             messageContext.setPastPivot(true);
             try {
@@ -203,6 +306,7 @@ public class EjbContainerProvider extends RPCProvider {
                 ServiceDesc serviceDescription = messageContext.getService().getServiceDescription();
                 RPCElement responseBody = createResponseBody(requestBody, messageContext, operation, serviceDescription, object, responseEnvelope, getInOutParams());
 
+                responseEnvelope.removeBody();
                 responseEnvelope.addBodyElement(responseBody);
             } catch (Exception e) {
                 throw new RuntimeException("Failed while creating response message body", e);
@@ -211,8 +315,16 @@ public class EjbContainerProvider extends RPCProvider {
 
         public void createExceptionResult(Throwable exception) {
             messageContext.setPastPivot(true);
-
-            SOAPFault fault = new SOAPFault(new AxisFault("Server", "Server Error", null, null));
+            
+            AxisFault axisFault = null;
+            if (exception instanceof Exception) {
+                axisFault = AxisFault.makeFault((Exception)exception);
+                axisFault.setFaultCodeAsString(Constants.FAULT_SERVER_GENERAL);
+            } else {
+                axisFault = new AxisFault("Server", "Server Error", null, null);
+            }
+            
+            SOAPFault fault = new SOAPFault(axisFault);
             SOAPEnvelope envelope = new SOAPEnvelope();
             envelope.addBodyElement(fault);
             Message message = new Message(envelope);
@@ -224,6 +336,5 @@ public class EjbContainerProvider extends RPCProvider {
             return new ArrayList(); //TODO collect out an inout params in demarshalArguments
         }
     }
-
 
 }
