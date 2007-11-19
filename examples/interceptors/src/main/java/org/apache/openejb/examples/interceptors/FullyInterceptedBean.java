675822c8a8afb2918da8903003bd4370ddd90cdd
Jacek Laskowski <jlaskowski@apache.org>
2007-11-19 22:55:45 +0000

    OPENEJB-723 New example: interceptors
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@596476 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/interceptors/src/main/java/org/apache/openejb/examples/interceptors/FullyInterceptedBean.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.examples.interceptors;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptors;
+import javax.interceptor.InvocationContext;
+
+/**
+ * @version $Rev$ $Date$
+ */
+@Stateless
+@Local
+@Interceptors( { ClassLevelInterceptorOne.class, ClassLevelInterceptorTwo.class })
+public class FullyInterceptedBean extends FullyInterceptedSuperClass implements FullyIntercepted {
+
+    @Interceptors( { MethodLevelInterceptorOne.class, MethodLevelInterceptorTwo.class })
+    public String businessMethod() {
+        return "businessMethod";
+    }
+
+    @AroundInvoke
+    private Object beanClassBusinessMethodInterceptor(InvocationContext ic) throws Exception {
+        return ic.proceed() + "-bean";
+    }
+}
