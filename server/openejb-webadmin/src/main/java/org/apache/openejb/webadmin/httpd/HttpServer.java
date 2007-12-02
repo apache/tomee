c05f379c8801b8294b5413122a8e377307d8b7f2
Jacek Laskowski <jlaskowski@apache.org>
2007-12-02 11:10:41 +0000

    Fix license headers
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@600269 13f79535-47bb-0310-9956-ffa450edef68

--- server/openejb-webadmin/src/main/java/org/apache/openejb/webadmin/httpd/HttpServer.java
+++ server/openejb-webadmin/src/main/java/org/apache/openejb/webadmin/httpd/HttpServer.java
@@ -1,46 +1,18 @@
 /**
- * Redistribution and use of this software and associated documentation
- * ("Software"), with or without modification, are permitted provided
- * that the following conditions are met:
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * 1. Redistributions of source code must retain copyright
- *    statements and notices.  Redistributions must also contain a
- *    copy of this document.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * 2. Redistributions in binary form must reproduce the
- *    above copyright notice, this list of conditions and the
- *    following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. The name "OpenEJB" must not be used to endorse or promote
- *    products derived from this Software without prior written
- *    permission of The OpenEJB Group.  For written permission,
- *    please contact dev@openejb.org.
- *
- * 4. Products derived from this Software may not be called "OpenEJB"
- *    nor may "OpenEJB" appear in their names without prior written
- *    permission of The OpenEJB Group. OpenEJB is a registered
- *    trademark of The OpenEJB Group.
- *
- * 5. Due credit should be given to the OpenEJB Project
- *    (http://www.openejb.org/).
- *
- * THIS SOFTWARE IS PROVIDED BY THE OPENEJB GROUP AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE OPENEJB GROUP OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Copyright 2001 (C) The OpenEJB Group. All Rights Reserved.
- *
- * $Id: HttpServer.java 446024 2006-02-21 08:45:52Z dblevins $
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.openejb.webadmin.httpd;
 
