7a2a50669ca8318d09951b26c118d5538bdf1a3c
David Blevins <dblevins@apache.org>
2007-12-08 07:58:47 +0000

    OPENEJB-360: Example using JPA This example could be more elaborate on the JPA side, but it at least shows how to get started.
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@602344 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/injection-of-entitymanager/src/main/java/org/superbiz/injection/jpa/MoviesImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.superbiz.injection.jpa;
+
+import org.superbiz.injection.jpa.Movie;
+import org.superbiz.injection.jpa.Movies;
+
+import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.PersistenceContextType;
+import java.util.List;
+
+@Stateful(name = "Movies")
+public class MoviesImpl implements Movies {
+
+    @PersistenceContext(unitName = "movie-unit", type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
+    public void addMovie(Movie movie) throws Exception {
+        entityManager.persist(movie);
+    }
+
+    public void deleteMovie(Movie movie) throws Exception {
+        entityManager.remove(movie);
+    }
+
+    public List<Movie> getMovies() throws Exception {
+        Query query = entityManager.createQuery("SELECT m from Movie as m");
+        return query.getResultList();
+    }
+
+}
