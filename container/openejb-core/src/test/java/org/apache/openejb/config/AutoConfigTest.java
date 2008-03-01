4f40d2e2635874274d89d67f84d59704e42a2b3d
David Blevins <dblevins@apache.org>
2008-03-01 22:32:11 +0000

    moved to the right package
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@632667 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/AutoConfigTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/AutoConfigTest.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.openejb.core.stateless;
+package org.apache.openejb.config;
 
 import junit.framework.TestCase;
 
