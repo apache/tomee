6f87359874d6b53b4b1df4e4f23dd09a9b91dba7
David Blevins <dblevins@apache.org>
2008-03-02 03:35:12 +0000

    Small utility class for getting the url or file object of the jar or directory containing a specific class Sort of a linux command "which" of classes
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@632706 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-loader/src/main/java/org/apache/openejb/loader/JarLocation.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.loader;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class JarLocation {
+
+    public static File get() {
+        return jarLocation(JarLocation.class);
+    }
+
+    public static File jarLocation(Class clazz) {
+        try {
+            String classFileName = clazz.getName().replace(".", "/") + ".class";
+
+            URL classURL = clazz.getClassLoader().getResource(classFileName);
+
+            URI uri = classURL.toURI();
+            if (uri.getPath() == null){
+                uri = new URI(uri.getSchemeSpecificPart());
+            }
+
+            String path = uri.getPath();
+            if (path.contains("!")){
+                path = path.substring(0, path.indexOf('!'));
+            } else {
+                path = path.substring(0, path.length() - classFileName.length());
+            }
+
+            return new File(path);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
