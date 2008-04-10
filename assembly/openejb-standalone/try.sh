6ea524f92a181426c0ba7b7eff65bce78eb497b7
David Blevins <dblevins@apache.org>
2008-04-10 21:19:33 +0000

    updated version
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@646965 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/openejb-standalone/try.sh
+++ assembly/openejb-standalone/try.sh
@@ -17,12 +17,12 @@
 #
 cd "$(dirname "$0")"
 
-JAR=$PWD/../../itests/openejb-itests-app/target/openejb-itests-app-3.0.0-SNAPSHOT.ear
+JAR=$PWD/../../itests/openejb-itests-app/target/openejb-itests-app-3.1-SNAPSHOT.ear
 
 cd target/
 
-tar xzvf openejb-3.0.0-SNAPSHOT-bin.tar.gz
-cd openejb-3.0.0-SNAPSHOT
+tar xzvf openejb-3.1-SNAPSHOT.tar.gz
+cd openejb-3.1-SNAPSHOT
 cp $JAR apps/
 ./bin/openejb start &
 sleep 1
