50d9df2ae6d5c582b6603fecb64b795766958f6f
David Blevins <dblevins@apache.org>
2008-05-21 02:05:14 +0000

    OPENEJB-348: Example: Minimal MessageDriven Bean via @MessageDriven
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@658530 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/simple-mdb/src/test/java/org/superbiz/mdb/ChatBeanTest.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+//START SNIPPET: code
+package org.superbiz.mdb;
+
+import junit.framework.TestCase;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
+
+public class ChatBeanTest extends TestCase {
+
+    public void test() throws Exception {
+        Properties p = new Properties();
+        p.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+        InitialContext context = new InitialContext(p);
+
+        MessagingClientLocal bean = (MessagingClientLocal) context.lookup("MessagingClientBeanLocal");
+
+        bean.sendMessage("Hello World!");
+
+        assertEquals(bean.receiveMessage(), "Hello, Test Case!");
+
+        bean.sendMessage("How are you?");
+
+        assertEquals(bean.receiveMessage(), "I'm doing well.");
+
+        bean.sendMessage("Still spinning?");
+
+        assertEquals(bean.receiveMessage(), "Once every day, as usual.");
+    }
+}
+//END SNIPPET: code
