c47957825f4b0e627dfc80117ec8ecbe509011fb
David Blevins <dblevins@apache.org>
2008-06-03 04:46:16 +0000

    Previous commit message (revision 662623) was supposed to be: Key the Entity and MappedSuperclass objects by the "class" attribute Fixed resulting test case incompatabilities -- the "class" attribute is required to be unique
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@662644 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-jee/src/main/java/org/apache/openejb/jee/jpa/EntityMappings.java
+++ container/openejb-jee/src/main/java/org/apache/openejb/jee/jpa/EntityMappings.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.openejb.jee.jpa;
 
 import org.apache.openejb.jee.KeyedCollection;
