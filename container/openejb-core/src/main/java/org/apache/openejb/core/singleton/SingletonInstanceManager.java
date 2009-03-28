61509f6c0c58c74daebb2d5e020fb934724f11f7
David Blevins <dblevins@apache.org>
2009-03-28 00:58:53 +0000

    OPENEJB-1012: Multi-threaded Singleton Lazy Instantiation Issue New code uses an AtomicReference to hold a FutureTask object.  The thread to win the Atomic compare and swap operation is the thread that will construct the singleton via futureTask.run()
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@759412 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/core/singleton/SingletonInstanceManager.java
+++ container/openejb-core/src/main/java/org/apache/openejb/core/singleton/SingletonInstanceManager.java
@@ -27,15 +27,22 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
+import javax.ejb.NoSuchEJBException;
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.xml.ws.WebServiceContext;
 
 import org.apache.openejb.Injection;
 import org.apache.openejb.OpenEJBException;
+import org.apache.openejb.ApplicationException;
 import org.apache.openejb.core.BaseContext;
 import org.apache.openejb.core.CoreDeploymentInfo;
 import org.apache.openejb.core.Operation;
@@ -60,143 +67,171 @@ public class SingletonInstanceManager {
         this.securityService = securityService;
     }
 
-    /**
-     * Removes an instance from the pool and returns it for use
-     * by the container in business methods.
-     *
-     * If the pool is at it's limit the StrictPooling flag will
-     * cause this thread to wait.
-     *
-     * If StrictPooling is not enabled this method will create a
-     * new stateless bean instance performing all required injection
-     * and callbacks before returning it in a method ready state.
-     * 
-     * @param callContext
-     * @return
-     * @throws OpenEJBException
-     */
-    public Instance getInstance(ThreadContext callContext)
-            throws OpenEJBException {
-        CoreDeploymentInfo deploymentInfo = callContext.getDeploymentInfo();
+    public Instance getInstance(final ThreadContext callContext) throws OpenEJBException {
+        final CoreDeploymentInfo deploymentInfo = callContext.getDeploymentInfo();
         Data data = (Data) deploymentInfo.getContainerData();
+        AtomicReference<Future<Instance>> singleton = data.singleton;
+        try {
+            // Has the singleton been created yet?
+            // If there is a Future object in the AtomicReference, then
+            // it's either been created or is being created now.
+            Future<Instance> singletonFuture = singleton.get();
+            if (singletonFuture != null) return singletonFuture.get();
 
-        if (data.instance == null) {
-
-            Class beanClass = deploymentInfo.getBeanClass();
-            ObjectRecipe objectRecipe = new ObjectRecipe(beanClass);
-            objectRecipe.allow(Option.FIELD_INJECTION);
-            objectRecipe.allow(Option.PRIVATE_PROPERTIES);
-            objectRecipe.allow(Option.IGNORE_MISSING_PROPERTIES);
-            objectRecipe.allow(Option.NAMED_PARAMETERS);
-
-            Operation originalOperation = callContext.getCurrentOperation();
-            BaseContext.State[] originalAllowedStates = callContext.getCurrentAllowedStates();
-
-            try {
-                Context ctx = deploymentInfo.getJndiEnc();                
-                SessionContext sessionContext;
-                // This needs to be synchronized as this code is multi-threaded.
-                // In between the lookup and the bind a bind may take place in another Thread.
-                // This is a fix for GERONIMO-3444
-                synchronized(this){
-                    try {                    
-                        sessionContext = (SessionContext) ctx.lookup("java:comp/EJBContext");
-                    } catch (NamingException e1) {
-                        sessionContext = createSessionContext();
-                        // TODO: This should work
-                        ctx.bind("java:comp/EJBContext", sessionContext);
-                    }                  
+            // The singleton has not been created nor is being created
+            // We will construct this FutureTask and compete with the
+            // other threads for the right to create the singleton
+            FutureTask<Instance> task = new FutureTask<Instance>(new Callable<Instance>() {
+                public Instance call() throws Exception {
+                    return createInstance(callContext, deploymentInfo);
                 }
-                if (javax.ejb.SessionBean.class.isAssignableFrom(beanClass) || hasSetSessionContext(beanClass)) {
-                    callContext.setCurrentOperation(Operation.INJECTION);
-                    callContext.setCurrentAllowedStates(SingletonContext.getStates());
-                    objectRecipe.setProperty("sessionContext", sessionContext);
-                }     
-                
-                // This is a fix for GERONIMO-3444
-                synchronized(this){
-                    try {
-                        ctx.lookup("java:comp/WebServiceContext");
-                    } catch (NamingException e) {
-                        WebServiceContext wsContext;
-                        wsContext = new EjbWsContext(sessionContext);
-                        ctx.bind("java:comp/WebServiceContext", wsContext);
-                    }
+            });
+
+
+            do {
+                // If our FutureTask was the one to win the slot
+                // than we are the ones responsisble for creating
+                // the singleton while the others wait.
+                if (singleton.compareAndSet(null, task)) {
+                    task.run();
                 }
 
-                fillInjectionProperties(objectRecipe, beanClass, deploymentInfo, ctx);
-
-                Object bean = objectRecipe.create(beanClass.getClassLoader());
-                Map unsetProperties = objectRecipe.getUnsetProperties();
-                if (unsetProperties.size() > 0) {
-                    for (Object property : unsetProperties.keySet()) {
-                        logger.warning("Injection: No such property '" + property + "' in class " + beanClass.getName());
-                    }
-                }
-
-                HashMap<String, Object> interceptorInstances = new HashMap<String, Object>();
-                for (InterceptorData interceptorData : deploymentInfo.getAllInterceptors()) {
-                    if (interceptorData.getInterceptorClass().equals(beanClass)) continue;
-
-                    Class clazz = interceptorData.getInterceptorClass();
-                    ObjectRecipe interceptorRecipe = new ObjectRecipe(clazz);
-                    interceptorRecipe.allow(Option.FIELD_INJECTION);
-                    interceptorRecipe.allow(Option.PRIVATE_PROPERTIES);
-                    interceptorRecipe.allow(Option.IGNORE_MISSING_PROPERTIES);
-                    interceptorRecipe.allow(Option.NAMED_PARAMETERS);
-
-                    fillInjectionProperties(interceptorRecipe, clazz, deploymentInfo, ctx);
-
-                    try {
-                        Object interceptorInstance = interceptorRecipe.create(clazz.getClassLoader());
-                        interceptorInstances.put(clazz.getName(), interceptorInstance);
-                    } catch (ConstructionException e) {
-                        throw new Exception("Failed to create interceptor: " + clazz.getName(), e);
-                    }
-                }
-
-                interceptorInstances.put(beanClass.getName(), bean);
+                // If we didn't win the slot and no other FutureTask
+                // has been set by a different thread, than we need
+                // to try again.
+            } while ((singletonFuture = singleton.get()) == null);
 
 
-                callContext.setCurrentOperation(Operation.POST_CONSTRUCT);
-                callContext.setCurrentAllowedStates(SingletonContext.getStates());
+            // At this point we can safely return the singleton
+            return singletonFuture.get();
 
-                List<InterceptorData> callbackInterceptors = deploymentInfo.getCallbackInterceptors();
-                InterceptorStack interceptorStack = new InterceptorStack(bean, null, Operation.POST_CONSTRUCT, callbackInterceptors, interceptorInstances);
-                interceptorStack.invoke();
-
-                if (bean instanceof SessionBean){
-                    callContext.setCurrentOperation(Operation.CREATE);
-                    callContext.setCurrentAllowedStates(SingletonContext.getStates());
-                    Method create = deploymentInfo.getCreateMethod();
-                    interceptorStack = new InterceptorStack(bean, create, Operation.CREATE, new ArrayList<InterceptorData>(), new HashMap());
-                    interceptorStack.invoke();
-                }
-
-                ReadWriteLock lock;
-                if (deploymentInfo.isBeanManagedConcurrency()){
-                    // Bean-Managed Concurrency
-                    lock = new BeanManagedLock();
-                } else {
-                    // Container-Managed Concurrency
-                    lock = new ReentrantReadWriteLock();
-                }
-
-                data.instance = new Instance(bean, interceptorInstances, lock);
-            } catch (Throwable e) {
-                if (e instanceof java.lang.reflect.InvocationTargetException) {
-                    e = ((java.lang.reflect.InvocationTargetException) e).getTargetException();
-                }
-                String t = "The bean instance threw a system exception:" + e;
-                logger.error(t, e);
-                throw new org.apache.openejb.ApplicationException(new RemoteException("Cannot obtain a free instance.", e));
-            } finally {
-                callContext.setCurrentOperation(originalOperation);
-                callContext.setCurrentAllowedStates(originalAllowedStates);
+        } catch (InterruptedException e) {
+            Thread.interrupted();
+            throw new ApplicationException(new NoSuchEJBException("Singleton initialization interrupted").initCause(e));
+        } catch (ExecutionException e) {
+            Throwable throwable = e.getCause();
+            if (throwable instanceof ApplicationException) {
+                throw (ApplicationException) throwable;
             }
-        }
 
-        return data.instance;
+            throw new ApplicationException(new NoSuchEJBException("Singleton initialization failed").initCause(e.getCause()));
+        }
+    }
+
+    private Instance createInstance(ThreadContext callContext, CoreDeploymentInfo deploymentInfo) throws org.apache.openejb.ApplicationException {
+        Class beanClass = deploymentInfo.getBeanClass();
+        ObjectRecipe objectRecipe = new ObjectRecipe(beanClass);
+        objectRecipe.allow(Option.FIELD_INJECTION);
+        objectRecipe.allow(Option.PRIVATE_PROPERTIES);
+        objectRecipe.allow(Option.IGNORE_MISSING_PROPERTIES);
+        objectRecipe.allow(Option.NAMED_PARAMETERS);
+
+        Operation originalOperation = callContext.getCurrentOperation();
+        BaseContext.State[] originalAllowedStates = callContext.getCurrentAllowedStates();
+
+        try {
+            Context ctx = deploymentInfo.getJndiEnc();
+            SessionContext sessionContext;
+            // This needs to be synchronized as this code is multi-threaded.
+            // In between the lookup and the bind a bind may take place in another Thread.
+            // This is a fix for GERONIMO-3444
+            synchronized(this){
+                try {
+                    sessionContext = (SessionContext) ctx.lookup("java:comp/EJBContext");
+                } catch (NamingException e1) {
+                    sessionContext = createSessionContext();
+                    // TODO: This should work
+                    ctx.bind("java:comp/EJBContext", sessionContext);
+                }
+            }
+
+            if (SessionBean.class.isAssignableFrom(beanClass) || hasSetSessionContext(beanClass)) {
+                callContext.setCurrentOperation(Operation.INJECTION);
+                callContext.setCurrentAllowedStates(SingletonContext.getStates());
+                objectRecipe.setProperty("sessionContext", sessionContext);
+            }
+
+            // This is a fix for GERONIMO-3444
+            synchronized(this){
+                try {
+                    ctx.lookup("java:comp/WebServiceContext");
+                } catch (NamingException e) {
+                    WebServiceContext wsContext;
+                    wsContext = new EjbWsContext(sessionContext);
+                    ctx.bind("java:comp/WebServiceContext", wsContext);
+                }
+            }
+
+            fillInjectionProperties(objectRecipe, beanClass, deploymentInfo, ctx);
+
+            Object bean = objectRecipe.create(beanClass.getClassLoader());
+            Map unsetProperties = objectRecipe.getUnsetProperties();
+            if (unsetProperties.size() > 0) {
+                for (Object property : unsetProperties.keySet()) {
+                    logger.warning("Injection: No such property '" + property + "' in class " + beanClass.getName());
+                }
+            }
+
+            HashMap<String, Object> interceptorInstances = new HashMap<String, Object>();
+            for (InterceptorData interceptorData : deploymentInfo.getAllInterceptors()) {
+                if (interceptorData.getInterceptorClass().equals(beanClass)) continue;
+
+                Class clazz = interceptorData.getInterceptorClass();
+                ObjectRecipe interceptorRecipe = new ObjectRecipe(clazz);
+                interceptorRecipe.allow(Option.FIELD_INJECTION);
+                interceptorRecipe.allow(Option.PRIVATE_PROPERTIES);
+                interceptorRecipe.allow(Option.IGNORE_MISSING_PROPERTIES);
+                interceptorRecipe.allow(Option.NAMED_PARAMETERS);
+
+                fillInjectionProperties(interceptorRecipe, clazz, deploymentInfo, ctx);
+
+                try {
+                    Object interceptorInstance = interceptorRecipe.create(clazz.getClassLoader());
+                    interceptorInstances.put(clazz.getName(), interceptorInstance);
+                } catch (ConstructionException e) {
+                    throw new Exception("Failed to create interceptor: " + clazz.getName(), e);
+                }
+            }
+
+            interceptorInstances.put(beanClass.getName(), bean);
+
+
+            callContext.setCurrentOperation(Operation.POST_CONSTRUCT);
+            callContext.setCurrentAllowedStates(SingletonContext.getStates());
+
+            List<InterceptorData> callbackInterceptors = deploymentInfo.getCallbackInterceptors();
+            InterceptorStack interceptorStack = new InterceptorStack(bean, null, Operation.POST_CONSTRUCT, callbackInterceptors, interceptorInstances);
+            interceptorStack.invoke();
+
+            if (bean instanceof SessionBean){
+                callContext.setCurrentOperation(Operation.CREATE);
+                callContext.setCurrentAllowedStates(SingletonContext.getStates());
+                Method create = deploymentInfo.getCreateMethod();
+                interceptorStack = new InterceptorStack(bean, create, Operation.CREATE, new ArrayList<InterceptorData>(), new HashMap());
+                interceptorStack.invoke();
+            }
+
+            ReadWriteLock lock;
+            if (deploymentInfo.isBeanManagedConcurrency()){
+                // Bean-Managed Concurrency
+                lock = new BeanManagedLock();
+            } else {
+                // Container-Managed Concurrency
+                lock = new ReentrantReadWriteLock();
+            }
+
+            return new Instance(bean, interceptorInstances, lock);
+        } catch (Throwable e) {
+            if (e instanceof java.lang.reflect.InvocationTargetException) {
+                e = ((java.lang.reflect.InvocationTargetException) e).getTargetException();
+            }
+            String t = "The bean instance threw a system exception:" + e;
+            logger.error(t, e);
+            throw new ApplicationException(new NoSuchEJBException("Singleton failed to initialize").initCause(e));
+        } finally {
+            callContext.setCurrentOperation(originalOperation);
+            callContext.setCurrentAllowedStates(originalAllowedStates);
+        }
     }
 
     private static void fillInjectionProperties(ObjectRecipe objectRecipe, Class clazz, CoreDeploymentInfo deploymentInfo, Context context) {
@@ -252,10 +287,22 @@ public class SingletonInstanceManager {
     public void freeInstance(ThreadContext callContext) {
         CoreDeploymentInfo deploymentInfo = callContext.getDeploymentInfo();
         Data data = (Data) deploymentInfo.getContainerData();
-        Instance instance = data.instance;
+        Future<Instance> instanceFuture = data.singleton.get();
 
         // Possible the instance was never created
-        if (instance == null) return;
+        if (instanceFuture == null) return;
+
+        Instance instance = null;
+        try {
+            instance = instanceFuture.get();
+        } catch (InterruptedException e) {
+            Thread.interrupted();
+            logger.error("Singleton shutdown failed because the thread was interrupted: "+deploymentInfo.getDeploymentID(), e);
+            return;
+        } catch (ExecutionException e) {
+            // Instance was never initialized
+        }
+
 
         try {
             callContext.setCurrentOperation(Operation.PRE_DESTROY);
@@ -297,7 +344,7 @@ public class SingletonInstanceManager {
     }
 
     private static final class Data {
-        private Instance instance;
+        private final AtomicReference<Future<Instance>> singleton = new AtomicReference<Future<Instance>>();
     }
 
 
