0629d1fa8e158e5b8cd89d59bbfc0fc26096a23f
David Blevins <dblevins@apache.org>
2009-06-03 00:42:06 +0000

    Added serialization id and removed cruft from web.xml
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@781230 13f79535-47bb-0310-9956-ffa450edef68

--- examples/webapps/moviefun/src/main/java/org/superbiz/moviefun/Movie.java
+++ examples/webapps/moviefun/src/main/java/org/superbiz/moviefun/Movie.java
@@ -16,15 +16,19 @@
  */
 package org.superbiz.moviefun;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Entity
-public class Movie {
+public class Movie implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
