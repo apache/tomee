ea41df746603b54c832cc0e5b8cc3049ceaf1e7f
David Blevins <dblevins@apache.org>
2009-11-20 22:30:37 +0000

    Partial support for OPENEJB-1113: @ManagedBean Container
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@882745 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-core/src/main/java/org/apache/openejb/core/managed/ManagedHomeHandler.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.core.managed;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import javax.ejb.RemoveException;
+
+import org.apache.openejb.InterfaceType;
+import org.apache.openejb.DeploymentInfo;
+import org.apache.openejb.core.ivm.EjbHomeProxyHandler;
+import org.apache.openejb.core.ivm.EjbObjectProxyHandler;
+import org.apache.openejb.util.proxy.ProxyManager;
+
+public class ManagedHomeHandler extends EjbHomeProxyHandler {
+
+    public ManagedHomeHandler(DeploymentInfo deploymentInfo, InterfaceType interfaceType, List<Class> interfaces) {
+        super(deploymentInfo, interfaceType, interfaces);
+    }
+
+    public Object createProxy(Object primaryKey) {
+        Object proxy = super.createProxy(primaryKey);
+        EjbObjectProxyHandler handler = (EjbObjectProxyHandler) ProxyManager.getInvocationHandler(proxy);
+
+        registerHandler(handler.getRegistryId(), handler);
+
+        return proxy;
+
+    }
+
+    protected Object findX(Class interfce, Method method, Object[] args, Object proxy) throws Throwable {
+        throw new UnsupportedOperationException("Stateful beans may not have find methods");
+    }
+
+    protected Object removeByPrimaryKey(Class interfce, Method method, Object[] args, Object proxy) throws Throwable {
+        throw new RemoveException("Session objects are private resources and do not have primary keys");
+    }
+
+    protected EjbObjectProxyHandler newEjbObjectHandler(DeploymentInfo deploymentInfo, Object pk, InterfaceType interfaceType, List<Class> interfaces) {
+        return new ManagedObjectHandler(getDeploymentInfo(), pk, interfaceType, interfaces);
+    }
+
+}
