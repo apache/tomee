a7ebd9b3e07142237cfaa9853215e5ca2784d057
David Blevins <dblevins@apache.org>
2010-02-23 03:49:56 +0000

    Great JUnitRunner by Quintin Beukes, OPENEJB-1078
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@915166 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-junit/src/main/java/org/apache/openejb/junit/OpenEjbRunner.java
@@ -0,0 +1,167 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * TODO: Decide what to do when the user already specifies a login module/configuration to use. We need to either provide a way to authenticate against this, or disable the securityRole option. Discuss with dblevins
+ */
+package org.apache.openejb.junit;
+
+import junit.framework.TestCase;
+import org.apache.openejb.junit.context.OpenEjbTestContext;
+import org.apache.openejb.junit.context.TestContext;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.InitializationError;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * To make your own runner, extend this class replacing {@link #newTestContext(java.lang.Class) }
+ * and {@link #newTestContext(java.lang.Class, java.lang.reflect.Method) }, which
+ * would return an implementation of {@link TestContext}
+ *
+ * @author quintin
+ */
+public class OpenEjbRunner extends Runner {
+    private Runner delegate;
+
+    /**
+     * Test class
+     */
+    private Class<?> testClazz;
+
+    /**
+     * Stores the TestContext where only the class configuration is used.
+     */
+    private TestContext classTestContext;
+
+    /**
+     * Creates a new runner (called by JUnit)
+     *
+     * @param testClazz
+     * @throws InitializationError
+     */
+    public OpenEjbRunner(Class<?> testClazz) throws InitializationError {
+        try {
+            delegate = getDelegateRunner(testClazz);
+        }
+        catch (Throwable e) {
+            throw new InitializationError(Arrays.asList(e));
+        }
+    }
+
+    /**
+     * Determines, based on the test class which runner we should delegate the
+     * actual JUnit logic to.
+     *
+     * @param testClazz
+     * @return the delegate runner
+     * @throws Throwable NFC! Not like you need to declare you're throwing an Error
+     */
+    protected Runner getDelegateRunner(Class<?> testClazz) throws Throwable {
+        if (TestCase.class.isAssignableFrom(testClazz)) {
+            throw new UnsupportedOperationException("JUnit 3 tests not supported yet.");
+        } else {
+            return new JUnit4Runner(this, testClazz);
+        }
+    }
+
+    /**
+     * @param clazz
+     * @return a new class level context
+     * @see #newTestContext(java.lang.String)
+     */
+    public TestContext newTestContext() {
+        return newTestContext((String) null);
+    }
+
+    /**
+     * This implementation returns a singleton context. Contexts can be reused and aren't
+     * attached to a specific instance, so in cases where none of the test's methods
+     * have their own configurations, we don't need to construct a new context with
+     * every call. This counts even for classes where only some methods have their
+     * own contexts, we still benefit from reuse. In those few cases where there is
+     * only a single method that doesn't have configuration, we might store the
+     * context for no reason, and not free the memory throughout the run, though this
+     * is probably quite rare and doesn't really matter.
+     *
+     * @param clazz
+     * @return a new class level context
+     */
+    public TestContext newTestContext(String roleName) {
+        if (classTestContext == null) {
+            classTestContext = new OpenEjbTestContext(testClazz);
+        }
+        return classTestContext;
+    }
+
+    /**
+     * @param clazz
+     * @param method
+     * @return a new method level context
+     * @see #newTestContext(java.lang.reflect.Method, java.lang.String)
+     */
+    public TestContext newTestContext(Method method) {
+        return newTestContext(method, null);
+    }
+
+    /**
+     * This accepts a null method. Might make it easier to initialize new contexts
+     * from the delegate runner (which calls back here). The cohesion isn't that great
+     * but is the best I could come up with for delegating to both JUnit 3 and 4.
+     * <p/>
+     * The alternative was a context factory - though that introduced a new class and
+     * made extending it more complex as 3 classes are needed - a runner to identify the
+     * factory, a factory and a context. Either that or you need to specify the
+     * factory class in every test.
+     *
+     * @param clazz
+     * @param method
+     * @param role   Role to execute the context in.
+     * @return a new method level context
+     */
+    public TestContext newTestContext(Method method, String roleName) {
+        if (method == null) {
+            return newTestContext(roleName);
+        } else {
+            return new OpenEjbTestContext(method, roleName);
+        }
+    }
+
+    @Override
+    public Description getDescription() {
+        return delegate.getDescription();
+    }
+
+    @Override
+    public void run(RunNotifier runNotifier) {
+        delegate.run(runNotifier);
+    }
+
+    @Override
+    public int testCount() {
+        return delegate.testCount();
+    }
+
+    /**
+     * @return the test class being run by this runner
+     */
+    protected Class<?> getTestClass() {
+        return testClazz;
+    }
+}
---
+++ container/openejb-junit/src/main/java/org/apache/openejb/junit/annotations/RunTestAs.java
@@ -0,0 +1,38 @@
+/*
+ * Unless otherwise stated, this program in binary or source code
+ * format and any accompanying media/files/resources are
+ * Copyright 2009 Kunye Mining Solutions
+ * 
+ * Contact Details
+ * ------------------------------------------------
+ *   Web Site: http://www.kunye.net
+ *  Telephone: +27 12 807 3590/1
+ *        Fax: +27 12 807 3592
+ *       Cell: +27 82 855 5907
+ *     E-Mail: akruger@kunye.net
+ * 
+ * Postal Address
+ * ------------------------------------------------
+ *  P.O. Box 70032
+ *  Die Wilgers
+ *  Pretoria
+ *  0041
+ */
+package org.apache.openejb.junit.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author quintin
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RunTestAs {
+    /**
+     * Role as which the test should be run
+     */
+    String value();
+}
