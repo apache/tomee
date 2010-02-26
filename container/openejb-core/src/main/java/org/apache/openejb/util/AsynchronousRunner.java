d65a96e0fea0c5d17fa3c262f249017088951dbe
David Blevins <dblevins@apache.org>
2010-02-26 22:10:10 +0000

    reformatting
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@916839 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/util/AsynchronousRunner.java
+++ container/openejb-core/src/main/java/org/apache/openejb/util/AsynchronousRunner.java
@@ -17,14 +17,10 @@
 package org.apache.openejb.util;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Utility class used to invoke methods asynchronously, using a given {@link Executor}.
@@ -39,8 +35,9 @@ public class AsynchronousRunner {
 
     /**
      * Performs the given method invocation asynchronously
-     * @param object The object which will have the method invoked
-     * @param method The method to be invoked
+     *
+     * @param object    The object which will have the method invoked
+     * @param method    The method to be invoked
      * @param arguments The invocation arguments
      * @return A {@link Future} containing the method return value
      */
@@ -54,6 +51,7 @@ public class AsynchronousRunner {
     /**
      * A {@link Callable} implementation which just delegates the execution using
      * {@link Method#invoke(Object, Object[])}
+     *
      * @author luis
      */
     private class MethodInvoker implements Callable<Object> {
