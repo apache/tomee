df8ff8736d243a130b8c89d3f6181a67f2d6926f
Karan Singh Malhi <kmalhi@apache.org>
2010-07-02 14:32:47 +0000

    A much concise version of the test with much better coverage. Added coverage for both Stateless, Stateful beans (local and remote)
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@960006 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/rules/CheckNoBusinessMethodTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/rules/CheckNoBusinessMethodTest.java
@@ -13,23 +13,33 @@ import javax.ejb.SessionContext;
 
 import org.apache.openejb.OpenEJBException;
 import org.apache.openejb.jee.EjbJar;
+import org.apache.openejb.jee.StatefulBean;
 import org.apache.openejb.jee.StatelessBean;
 import org.junit.runner.RunWith;
 
 @RunWith(ValidationRunner.class)
 public class CheckNoBusinessMethodTest {
-  // ===========START Region for testing key no.busines.method.args for local EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class has a method with the same name as the one in the Local interface, but the types of arguments are different.
-   */
-  @Keys({ "no.busines.method.args" })
-  public EjbJar noBusinessMethodArgsLocal() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
+  @Keys({ "no.busines.method.case", "no.busines.method.case", "no.busines.method.case", "no.busines.method.case", "no.busines.method.args", "no.busines.method.args",
+      "no.busines.method.args", "no.busines.method.args", "no.busines.method", "no.busines.method", "no.busines.method", "no.busines.method", })
+  public EjbJar noBusinessMethodArgs() throws OpenEJBException {
+    // System.setProperty("openejb.validation.output.level", "VERBOSE");
     EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(FooSession.class);
-    bean.setLocalHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$FooLocalHome");
-    bean.setLocal("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$FooLocal");
-    ejbJar.addEnterpriseBean(bean);
+    StatelessBean stateLessLocal = new StatelessBean(FooStatelessSession.class);
+    stateLessLocal.setLocalHome("org.apache.openejb.config.rules.DummyTest$FooLocalHome");
+    stateLessLocal.setLocal("org.apache.openejb.config.rules.DummyTest$FooLocal");
+    StatefulBean statefulLocal = new StatefulBean(FooStatefulSession.class);
+    statefulLocal.setLocalHome("org.apache.openejb.config.rules.DummyTest$FooLocalHome");
+    statefulLocal.setLocal("org.apache.openejb.config.rules.DummyTest$FooLocal");
+    StatelessBean stateLessRemote = new StatelessBean(FooRemoteStatelessSession.class);
+    stateLessRemote.setHome("org.apache.openejb.config.rules.DummyTest$FooRemoteHome");
+    stateLessRemote.setRemote("org.apache.openejb.config.rules.DummyTest$FooRemote");
+    StatefulBean statefulRemote = new StatefulBean(FooRemoteStatefulSession.class);
+    statefulRemote.setHome("org.apache.openejb.config.rules.DummyTest$FooRemoteHome");
+    statefulRemote.setRemote("org.apache.openejb.config.rules.DummyTest$FooRemote");
+    ejbJar.addEnterpriseBean(stateLessLocal);
+    ejbJar.addEnterpriseBean(statefulLocal);
+    ejbJar.addEnterpriseBean(stateLessRemote);
+    ejbJar.addEnterpriseBean(statefulRemote);
     return ejbJar;
   }
 
@@ -39,11 +49,18 @@ public class CheckNoBusinessMethodTest {
 
   private static interface FooLocal extends EJBLocalObject {
     void foo(String x, String y);
+
+    // this method is not implemented by the bean class
+    void foo1();
   }
 
-  private static class FooSession implements SessionBean {
+  private static class FooStatelessSession implements SessionBean {
     // method name is same as in the Local interface, except arguments are different
     public void foo(int x, String y) {
+    }
+
+    // method name has a different case
+    public void Foo(String x, String y) {
     }
 
     public void ejbCreate() {
@@ -66,81 +83,53 @@ public class CheckNoBusinessMethodTest {
     }
   }
 
-  // =========== END Region for testing key no.busines.method.args for local EJB's==========
-  // ===========START Region for testing key no.busines.method.args for Remote EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class has a method with the same name as the one in the Remote interface, but the types of arguments are different.
-   */
-  @Keys({ "no.busines.method.args" })
-  public EjbJar noBusinessMethodArgsRemote() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
-    EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(FooSessionRemote.class);
-    bean.setHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$FooRemoteHome");
-    bean.setRemote("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$FooRemote");
-    ejbJar.addEnterpriseBean(bean);
-    return ejbJar;
+  private static class FooStatefulSession implements SessionBean {
+    // method name is same as in the Local interface, except arguments are different
+    public void foo(int x, String y) {
+    }
+
+    // method name has a different case
+    public void Foo(String x, String y) {
+    }
+
+    public void ejbCreate() {
+    }
+
+    @Override
+    public void ejbActivate() throws EJBException, RemoteException {
+    }
+
+    @Override
+    public void ejbPassivate() throws EJBException, RemoteException {
+    }
+
+    @Override
+    public void ejbRemove() throws EJBException, RemoteException {
+    }
+
+    @Override
+    public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException {
+    }
   }
 
+  // =================
   private static interface FooRemoteHome extends EJBHome {
     FooRemote create() throws RemoteException, CreateException;
   }
 
   private static interface FooRemote extends EJBObject {
     void foo(String x, String y) throws RemoteException;
+
+    // This method is not implemented by the bean class
+    void foo1() throws RemoteException;
   }
 
-  private static class FooSessionRemote implements SessionBean {
+  private static class FooRemoteStatelessSession implements SessionBean {
     // method name is same as in the Remote interface, except arguments are different
     public void foo(int x, String y) {
     }
 
-    public void ejbCreate() {
-    }
-
-    @Override
-    public void ejbActivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbPassivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbRemove() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException {
-    }
-  }
-
-  // =========== END Region for testing key no.busines.method.args for remote EJB's==========
-  // ===========START Region for testing key no.busines.method.case for local EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class has a method with the same name as the one in the Local interface, but the method names have a different case.
-   */
-  @Keys({ "no.busines.method.case" })
-  public EjbJar noBusinessMethodCaseLocal() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
-    EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(BarSession.class);
-    bean.setLocalHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BarLocalHome");
-    bean.setLocal("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BarLocal");
-    ejbJar.addEnterpriseBean(bean);
-    return ejbJar;
-  }
-
-  private static interface BarLocalHome extends EJBLocalHome {
-    BarLocal create() throws CreateException;
-  }
-
-  private static interface BarLocal extends EJBLocalObject {
-    void foo(String x, String y);
-  }
-
-  private static class BarSession implements SessionBean {
-    // method name is same as in the Local interface, except the method name is in different case
+    // method name has a different case
     public void Foo(String x, String y) {
     }
 
@@ -164,32 +153,12 @@ public class CheckNoBusinessMethodTest {
     }
   }
 
-  // =========== END Region for testing key no.busines.method.case for local EJB's==========
-  // ===========START Region for testing key no.busines.method.case for remote EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class has a method with the same name as the one in the Remote interface, but the method names have a different case.
-   */
-  @Keys({ "no.busines.method.case" })
-  public EjbJar noBusinessMethodCaseRemote() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
-    EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(BarSessionRemote.class);
-    bean.setHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BarRemoteHome");
-    bean.setRemote("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BarRemote");
-    ejbJar.addEnterpriseBean(bean);
-    return ejbJar;
-  }
+  private static class FooRemoteStatefulSession implements SessionBean {
+    // method name is same as in the Remote interface, except arguments are different
+    public void foo(int x, String y) {
+    }
 
-  private static interface BarRemoteHome extends EJBHome {
-    BarRemote create() throws RemoteException, CreateException;
-  }
-
-  private static interface BarRemote extends EJBObject {
-    void foo(String x, String y) throws RemoteException;
-  }
-
-  private static class BarSessionRemote implements SessionBean {
-    // method name is same as in the Remote interface, except the method name is in different case
+    // method name has a different case
     public void Foo(String x, String y) {
     }
 
@@ -212,97 +181,4 @@ public class CheckNoBusinessMethodTest {
     public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException {
     }
   }
-
-  // =========== END Region for testing key no.busines.method.case for remote EJB's==========
-  // ===========START Region for testing key no.busines.method for local EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class does not have the method defined in its Local interface
-   */
-  @Keys({ "no.busines.method" })
-  public EjbJar noBusinessMethodLocal() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
-    EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(BazSession.class);
-    bean.setLocalHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BazLocalHome");
-    bean.setLocal("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BazLocal");
-    ejbJar.addEnterpriseBean(bean);
-    return ejbJar;
-  }
-
-  private static interface BazLocalHome extends EJBLocalHome {
-    BazLocal create() throws CreateException;
-  }
-
-  private static interface BazLocal extends EJBLocalObject {
-    void foo(String x, String y);
-  }
-
-  private static class BazSession implements SessionBean {
-    // method name is same as in the Local interface, except the method name is in different case
-    public void ejbCreate() {
-    }
-
-    @Override
-    public void ejbActivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbPassivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbRemove() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException {
-    }
-  }
-
-  // =========== END Region for testing key no.busines.method for local EJB's==========
-  // ===========START Region for testing key no.busines.method for remote EJB's==========
-  /**
-   * Test success scenario: This test will succeed when an EJB class does not have the method defined in its Remote interface
-   */
-  @Keys({ "no.busines.method" })
-  public EjbJar noBusinessMethodRemote() throws OpenEJBException {
-    System.setProperty("openejb.validation.output.level", "VERBOSE");
-    EjbJar ejbJar = new EjbJar();
-    StatelessBean bean = new StatelessBean(BazSessionRemote.class);
-    bean.setHome("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BazRemoteHome");
-    bean.setRemote("org.apache.openejb.config.rules.CheckNoBusinessMethodTest$BazRemote");
-    ejbJar.addEnterpriseBean(bean);
-    return ejbJar;
-  }
-
-  private static interface BazRemoteHome extends EJBHome {
-    BazRemote create() throws RemoteException, CreateException;
-  }
-
-  private static interface BazRemote extends EJBObject {
-    void foo(String x, String y) throws RemoteException;
-  }
-
-  private static class BazSessionRemote implements SessionBean {
-    // method name is same as in the Local interface, except the method name is in different case
-    public void ejbCreate() {
-    }
-
-    @Override
-    public void ejbActivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbPassivate() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void ejbRemove() throws EJBException, RemoteException {
-    }
-
-    @Override
-    public void setSessionContext(SessionContext arg0) throws EJBException, RemoteException {
-    }
-  }
-  // =========== END Region for testing key no.busines.method for remote EJB's==========
 }
