641ffff742cece3f4143b9d1a773f64a1f65f41a
Jonathan Gallimore <jgallimore@apache.org>
2010-07-06 19:33:23 +0000

    OPENEJB-1299 patch from Shawn Jiang
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@960991 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/core/interceptor/ReflectionInvocationContext.java
+++ container/openejb-core/src/main/java/org/apache/openejb/core/interceptor/ReflectionInvocationContext.java
@@ -163,24 +163,11 @@ public class ReflectionInvocationContext implements InvocationContext {
             this.args = args;
         }
         public Object invoke() throws Exception {
-            Method targetMethod = getMethod(target.getClass(), method.getName(), method.getParameterTypes());
-            targetMethod.setAccessible(true);
-            Object value = targetMethod.invoke(target, args);
+            
+            Object value = method.invoke(target, args);
             return value;
         }
 
-        private Method getMethod(Class<?> cls, String methodName, Class<?>[] parameterTypes) throws NoSuchMethodException {
-            try {
-                Method method = cls.getDeclaredMethod(methodName, parameterTypes);
-                return method;
-            } catch (NoSuchMethodException e) {
-                if (! (Object.class.equals(cls))) {
-                    return getMethod(cls.getSuperclass(), methodName, parameterTypes);
-                }
-
-                throw e;
-            }
-        }
 
         public String toString() {
             return method.getDeclaringClass().getName() + "." + method.getName();
