2781a203e0bd6360ad0fecc5a42ec43ef174eb8a
Karan Singh Malhi <kmalhi@apache.org>
2010-07-10 03:12:29 +0000

    Added Apache license, reformatted with 4 spaces instead of 2, fixed imports
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@962749 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/rules/InvokeMethod.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/rules/InvokeMethod.java
@@ -1,17 +1,25 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.openejb.config.rules;
 
 import static org.apache.openejb.config.rules.ValidationAssertions.assertFailures;
 import static org.junit.Assert.fail;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -30,82 +38,83 @@ import org.junit.runners.model.Statement;
  * This Statement is the one which runs the test.
  */
 public class InvokeMethod extends Statement {
-  private ConfigurationFactory config;
-  private Assembler assembler;
-  // The test method
-  private final FrameworkMethod testMethod;
-  // The TestCase instance
-  private Object target;
-  // These are all the keys defined in org.apache.openejb.config.rules.Messages.properties
-  private static Set<String> allKeys;
-  static {
-    ResourceBundle bundle = ResourceBundle.getBundle("org.apache.openejb.config.rules.Messages");
-    allKeys = bundle.keySet();
-  }
-
-  public InvokeMethod(FrameworkMethod testMethod, Object target) {
-    this.testMethod = testMethod;
-    this.target = target;
-  }
-
-  @Override
-  public void evaluate() throws Throwable {
-    List<String> expectedKeys = validateKeys();
-    setUp();
-    Object obj = testMethod.invokeExplosively(target);
-    if (obj instanceof EjbJar) {
-      EjbJar ejbJar = (EjbJar) obj;
-      try {
-        assembler.createApplication(config.configureApplication(ejbJar));
-        fail("A ValidationFailedException should have been thrown");
-      } catch (ValidationFailedException vfe) {
-        assertFailures(expectedKeys, vfe);
-      }
+    private ConfigurationFactory config;
+    private Assembler assembler;
+    // The test method
+    private final FrameworkMethod testMethod;
+    // The TestCase instance
+    private Object target;
+    // These are all the keys defined in org.apache.openejb.config.rules.Messages.properties
+    private static Set<String> allKeys;
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("org.apache.openejb.config.rules.Messages");
+        allKeys = bundle.keySet();
     }
-    tearDown();
-  }
 
-  private void setUp() throws Exception {
-    config = new ConfigurationFactory();
-    assembler = new Assembler();
-    assembler.createTransactionManager(config.configureService(TransactionServiceInfo.class));
-    assembler.createSecurityService(config.configureService(SecurityServiceInfo.class));
-  }
-
-  private void tearDown() {}
-
-  /**
-   * Tests to see if the keys specified in the @Keys annotation are also available in the org.apache.openejb.config.rules.Messages.properties file. If there are any invalid keys,
-   * then it throws an exception and causes the test to error out. If all the keys are valid, then it writes those keys to a file. This list of keys can then be compared with all
-   * the Keys in org.apache.openejb.config.rules.Messages.properties and one can then find out the "test coverage" of the keys i.e. this tool can be used to find keys for which
-   * tests have not yet been written
-   * 
-   * @return
-   * @throws Exception
-   */
-  private List<String> validateKeys() throws Exception {
-    Keys annotation = testMethod.getAnnotation(Keys.class);
-    Key[] keys = annotation.value();
-    ArrayList<String> wrongKeys = new ArrayList<String>();
-    for (Key key : keys) {
-      if (allKeys.contains("1." + key.value())) {
-        continue;
-      } else {
-        wrongKeys.add(key.value());
-      }
+    public InvokeMethod(FrameworkMethod testMethod, Object target) {
+        this.testMethod = testMethod;
+        this.target = target;
     }
-    if (wrongKeys.isEmpty()) {
-      ArrayList<String> validKeys = new ArrayList<String>();
-      for (Key key : keys) {
-        for (int i = 0; i < key.count(); i++) {
-          validKeys.add(key.value());
+
+    @Override
+    public void evaluate() throws Throwable {
+        List<String> expectedKeys = validateKeys();
+        setUp();
+        Object obj = testMethod.invokeExplosively(target);
+        if (obj instanceof EjbJar) {
+            EjbJar ejbJar = (EjbJar) obj;
+            try {
+                assembler.createApplication(config.configureApplication(ejbJar));
+                fail("A ValidationFailedException should have been thrown");
+            } catch (ValidationFailedException vfe) {
+                assertFailures(expectedKeys, vfe);
+            }
         }
-      }
-      return validKeys;
-    } else {
-      String commaDelimitedKeys = Join.join(",", wrongKeys);
-      throw new Exception("The following keys listed in the @Keys annotation on the method " + testMethod.getName() + "() of " + testMethod.getMethod().getDeclaringClass()
-          + " are invalid : " + commaDelimitedKeys + " . Only keys listed in org.apache.openejb.config.rules.Messages.properties are allowed to be used in this annotation. ");
+        tearDown();
     }
-  }
+
+    private void setUp() throws Exception {
+        config = new ConfigurationFactory();
+        assembler = new Assembler();
+        assembler.createTransactionManager(config.configureService(TransactionServiceInfo.class));
+        assembler.createSecurityService(config.configureService(SecurityServiceInfo.class));
+    }
+
+    private void tearDown() {}
+
+    /**
+     * Tests to see if the keys specified in the @Keys annotation are also available in the org.apache.openejb.config.rules.Messages.properties file. If there are any invalid keys,
+     * then it throws an exception and causes the test to error out. If all the keys are valid, then it returns those keys. This list of keys can then be compared with all the Keys
+     * in org.apache.openejb.config.rules.Messages.properties and one can then find out the "test coverage" of the keys i.e. this tool can be used to find keys for which tests have
+     * not yet been written
+     * 
+     * @return
+     * @throws Exception
+     */
+    private List<String> validateKeys() throws Exception {
+        Keys annotation = testMethod.getAnnotation(Keys.class);
+        Key[] keys = annotation.value();
+        ArrayList<String> wrongKeys = new ArrayList<String>();
+        for (Key key : keys) {
+            if (allKeys.contains("1." + key.value())) {
+                continue;
+            } else {
+                wrongKeys.add(key.value());
+            }
+        }
+        if (wrongKeys.isEmpty()) {
+            ArrayList<String> validKeys = new ArrayList<String>();
+            for (Key key : keys) {
+                for (int i = 0; i < key.count(); i++) {
+                    validKeys.add(key.value());
+                }
+            }
+            return validKeys;
+        } else {
+            String commaDelimitedKeys = Join.join(",", wrongKeys);
+            throw new Exception("The following keys listed in the @Keys annotation on the method " + testMethod.getName() + "() of " + testMethod.getMethod().getDeclaringClass()
+                    + " are invalid : " + commaDelimitedKeys
+                    + " . Only keys listed in org.apache.openejb.config.rules.Messages.properties are allowed to be used in this annotation. ");
+        }
+    }
 }
--- container/openejb-core/src/test/java/org/apache/openejb/config/rules/ValidationRunner.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/rules/ValidationRunner.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.openejb.config.rules;
 
 import java.util.List;
@@ -22,50 +38,50 @@ import org.junit.runners.model.Statement;
  * found, then the Runner throws and exception resulting in your test case not being allowed to run.
  */
 public class ValidationRunner extends BlockJUnit4ClassRunner {
-  public ValidationRunner(Class<?> klass) throws InitializationError {
-    super(klass);
-  }
-
-  /**
-   * Flags an error if you have annotated a test with both @Test and @Keys. Any method annotated with @Test will be ignored anyways.
-   */
-  @Override
-  protected void collectInitializationErrors(List<Throwable> errors) {
-    super.collectInitializationErrors(errors);
-    List<FrameworkMethod> methodsAnnotatedWithKeys = getTestClass().getAnnotatedMethods(Keys.class);
-    for (FrameworkMethod frameworkMethod : methodsAnnotatedWithKeys) {
-      if (frameworkMethod.getAnnotation(Test.class) != null) {
-        String gripe = "The method " + frameworkMethod.getName() + "() can only be annotated with @Keys";
-        errors.add(new Exception(gripe));
-      }
+    public ValidationRunner(Class<?> klass) throws InitializationError {
+        super(klass);
     }
-  }
 
-  @Override
-  protected Statement methodBlock(FrameworkMethod method) {
-    Object test;
-    try {
-      test = new ReflectiveCallable() {
-        @Override
-        protected Object runReflectiveCall() throws Throwable {
-          return createTest();
+    /**
+     * Flags an error if you have annotated a test with both @Test and @Keys. Any method annotated with @Test will be ignored anyways.
+     */
+    @Override
+    protected void collectInitializationErrors(List<Throwable> errors) {
+        super.collectInitializationErrors(errors);
+        List<FrameworkMethod> methodsAnnotatedWithKeys = getTestClass().getAnnotatedMethods(Keys.class);
+        for (FrameworkMethod frameworkMethod : methodsAnnotatedWithKeys) {
+            if (frameworkMethod.getAnnotation(Test.class) != null) {
+                String gripe = "The method " + frameworkMethod.getName() + "() can only be annotated with @Keys";
+                errors.add(new Exception(gripe));
+            }
         }
-      }.run();
-    } catch (Throwable e) {
-      return new Fail(e);
     }
-    Statement statement = new InvokeMethod(method, test);
-    statement = withBefores(method, test, statement);
-    statement = withAfters(method, test, statement);
-    return statement;
-  }
 
-  /**
-   * By default JUnit includes all methods annotated with @Test. This method only allows methods annotated with @Keys to be included in the list of methods to be run in a TestCase.
-   * Any @Test methods will be ignored
-   */
-  @Override
-  protected List<FrameworkMethod> computeTestMethods() {
-    return getTestClass().getAnnotatedMethods(Keys.class);
-  }
+    @Override
+    protected Statement methodBlock(FrameworkMethod method) {
+        Object test;
+        try {
+            test = new ReflectiveCallable() {
+                @Override
+                protected Object runReflectiveCall() throws Throwable {
+                    return createTest();
+                }
+            }.run();
+        } catch (Throwable e) {
+            return new Fail(e);
+        }
+        Statement statement = new InvokeMethod(method, test);
+        statement = withBefores(method, test, statement);
+        statement = withAfters(method, test, statement);
+        return statement;
+    }
+
+    /**
+     * By default JUnit includes all methods annotated with @Test. This method only allows methods annotated with @Keys to be included in the list of methods to be run in a
+     * TestCase. Any @Test methods will be ignored
+     */
+    @Override
+    protected List<FrameworkMethod> computeTestMethods() {
+        return getTestClass().getAnnotatedMethods(Keys.class);
+    }
 }
