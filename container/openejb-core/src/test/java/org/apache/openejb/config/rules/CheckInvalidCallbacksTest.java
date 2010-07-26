1a66ad0797de8a860cb97ff2c9c60fc6fe83553c
Karan Singh Malhi <kmalhi@apache.org>
2010-07-26 18:11:14 +0000

    Tested some more callback keys. Fixed logic for checking for invalid usage of callback annotations while implementing SessionSynchronization
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@979393 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/rules/CheckInvalidCallbacksTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/rules/CheckInvalidCallbacksTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.openejb.config.rules;
 
+import java.rmi.RemoteException;
 import java.util.concurrent.Callable;
 
 import javax.annotation.PostConstruct;
@@ -23,12 +24,15 @@ import javax.annotation.PreDestroy;
 import javax.ejb.AfterBegin;
 import javax.ejb.AfterCompletion;
 import javax.ejb.BeforeCompletion;
+import javax.ejb.EJBException;
 import javax.ejb.PostActivate;
 import javax.ejb.PrePassivate;
+import javax.ejb.SessionSynchronization;
 
 import junit.framework.TestCase;
 
 import org.apache.openejb.jee.EjbJar;
+import org.apache.openejb.jee.NamedMethod;
 import org.apache.openejb.jee.SingletonBean;
 import org.apache.openejb.jee.StatefulBean;
 import org.apache.openejb.jee.StatelessBean;
@@ -40,13 +44,20 @@ import org.junit.runner.RunWith;
 @RunWith(ValidationRunner.class)
 public class CheckInvalidCallbacksTest extends TestCase {
     @Keys( { @Key(value = "ignoredAnnotation", count = 10, type = KeyType.WARNING), @Key("callback.missing.possibleTypo"), @Key("callback.badReturnType"),
-            @Key("callback.badModifier"), @Key("callback.invalidArguments"), @Key("aroundInvoke.missing") })
+            @Key("callback.badModifier"), @Key("callback.invalidArguments"), @Key("aroundInvoke.missing"), @Key("callback.missing"),
+            @Key(value="callback.sessionSynchronization.invalidUse",count=2) })
     public EjbJar test() throws Exception {
         EjbJar ejbJar = new EjbJar();
         StatelessBean testBean = ejbJar.addEnterpriseBean(new StatelessBean("TestStateless", TestBean.class));
         testBean.addAroundInvoke("wrongMethod");
+        testBean.addPostConstruct("wrongMethod");
         ejbJar.addEnterpriseBean(new SingletonBean("TestSingleton", TestBean.class));
         ejbJar.addEnterpriseBean(new StatefulBean("FooStateful", FooBean.class));
+        ejbJar.addEnterpriseBean(new StatefulBean("BarStateful", BarBean.class));
+        StatefulBean starBean = ejbJar.addEnterpriseBean(new StatefulBean("StarStateful", StarBean.class));
+        starBean.setAfterBeginMethod(new NamedMethod("myAfterBegin"));
+        starBean.setBeforeCompletionMethod(new NamedMethod("myBeforeCompletion"));
+        starBean.setAfterCompletionMethod(new NamedMethod("myAfterCompletion"));
         return ejbJar;
     }
 
@@ -103,5 +114,42 @@ public class CheckInvalidCallbacksTest extends TestCase {
 
         @AfterCompletion
         public void afterCompletionTypo() {}
+    }
+
+    public static class BarBean implements SessionSynchronization {
+        @AfterBegin
+        public void myAfterBegin() {}
+
+        @BeforeCompletion
+        public void myBeforeCompletion() {}
+
+        @AfterCompletion
+        public void myAfterCompletion(boolean committed) {}
+
+        @Override
+        public void afterBegin() throws EJBException, RemoteException {}
+
+        @Override
+        public void afterCompletion(boolean arg0) throws EJBException, RemoteException {}
+
+        @Override
+        public void beforeCompletion() throws EJBException, RemoteException {}
+    }
+
+    public static class StarBean implements SessionSynchronization {
+        public void myAfterBegin() {}
+
+        public void myBeforeCompletion() {}
+
+        public void myAfterCompletion(boolean committed) {}
+
+        @Override
+        public void afterBegin() throws EJBException, RemoteException {}
+
+        @Override
+        public void afterCompletion(boolean arg0) throws EJBException, RemoteException {}
+
+        @Override
+        public void beforeCompletion() throws EJBException, RemoteException {}
     }
 }
