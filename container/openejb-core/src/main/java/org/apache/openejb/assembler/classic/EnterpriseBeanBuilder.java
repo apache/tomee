49f18bf0cd260eefe5d729c375e731f45fa2620d
Jarek Gawor <gawor@apache.org>
2010-08-17 17:10:02 +0000

    OPENEJB-1331: Ensure global, app, module jndi contexts are non-empty: bind app/AppName and module/ModuleName if available otherwise add a dummy object
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@986399 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/EnterpriseBeanBuilder.java
+++ container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/EnterpriseBeanBuilder.java
@@ -138,7 +138,7 @@ class EnterpriseBeanBuilder {
 
         // build the enc
         JndiEncBuilder jndiEncBuilder = new JndiEncBuilder(bean.jndiEnc, injections, transactionType, moduleContext.getId(), moduleContext.getClassLoader());
-        Context compJndiContext = jndiEncBuilder.build(true);
+        Context compJndiContext = jndiEncBuilder.build(JndiEncBuilder.JndiScope.comp);
         bind(compJndiContext, "module", moduleContext.getModuleJndiContext());
         bind(compJndiContext, "app", moduleContext.getAppContext().getAppJndiContext());
         bind(compJndiContext, "global", moduleContext.getAppContext().getGlobalJndiContext());
