ca4abf30b33901970d0f38c01b039b99a99e8625
Jean-Louis Monteiro <jlmonteiro@apache.org>
2010-12-16 15:20:18 +0000

    OPENEJB-1410 Dynamic DS routing. - Apache headers - indentation - refactoring - test case completion - javadoc
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1050003 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/resource/jdbc/RoutedDataSource.java
+++ container/openejb-core/src/main/java/org/apache/openejb/resource/jdbc/RoutedDataSource.java
@@ -1,3 +1,20 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.openejb.resource.jdbc;
 
 import java.io.PrintWriter;
@@ -26,75 +43,75 @@ public class RoutedDataSource implements DataSource {
     }
 
     public void setRouter(String router) {
-        ContainerSystem containerSystem = SystemInstance.get().getComponent(
-                ContainerSystem.class);
+        ContainerSystem containerSystem = SystemInstance.get().getComponent(ContainerSystem.class);
 
         Object o = null;
         Context ctx = containerSystem.getJNDIContext();
         try {
             o = ctx.lookup(OPENEJB_RESOURCE_PREFIX + router);
+
         } catch (NamingException e) {
-            throw new IllegalArgumentException("Can't find router [" + router
-                    + "]", e);
+            throw new IllegalArgumentException("Can't find router [" + router + "]", e);
         }
 
         if (o instanceof Router) {
             delegate = (Router) o;
+
         } else {
             throw new IllegalArgumentException(o + " is not a router");
         }
     }
 
     public PrintWriter getLogWriter() throws SQLException {
-        if (ds() == null) {
+        if (getTargetDataSource() == null) {
             return null;
         }
-        return ds().getLogWriter();
+        return getTargetDataSource().getLogWriter();
     }
 
     public void setLogWriter(PrintWriter out) throws SQLException {
-        if (ds() != null) {
-            ds().setLogWriter(out);
+        if (getTargetDataSource() != null) {
+            getTargetDataSource().setLogWriter(out);
         }
     }
 
     public void setLoginTimeout(int seconds) throws SQLException {
-        if (ds() != null) {
-            ds().setLoginTimeout(seconds);
+        if (getTargetDataSource() != null) {
+            getTargetDataSource().setLoginTimeout(seconds);
         }
     }
 
     public int getLoginTimeout() throws SQLException {
-        if (ds() == null) {
+        if (getTargetDataSource() == null) {
             return -1;
         }
-        return ds().getLoginTimeout();
+        return getTargetDataSource().getLoginTimeout();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T unwrap(Class<T> iface) throws SQLException {
-        if (ds() == null) {
+        if (getTargetDataSource() == null) {
             return null;
         }
-        return (T) callByReflection(ds(), "unwrap",
-                new Class<?>[] { Class.class },  new Object[] { iface });
+        return (T) callByReflection(getTargetDataSource(), "unwrap",
+                new Class<?>[]{Class.class}, new Object[]{iface});
     }
 
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        if (ds() == null) {
+        if (getTargetDataSource() == null) {
             return false;
         }
-        return (Boolean) callByReflection(ds(), "isWrapperFor",
-                new Class<?>[] { Class.class },  new Object[] { iface });
+        return (Boolean) callByReflection(getTargetDataSource(), "isWrapperFor",
+                new Class<?>[]{Class.class}, new Object[]{iface});
     }
 
     public Connection getConnection() throws SQLException {
-        return ds().getConnection();
+        return getTargetDataSource().getConnection();
     }
 
     public Connection getConnection(String username, String password)
             throws SQLException {
-        return ds().getConnection(username, password);
+        return getTargetDataSource().getConnection(username, password);
     }
 
     public Router getDelegate() {
@@ -104,7 +121,7 @@ public class RoutedDataSource implements DataSource {
         return delegate;
     }
 
-    private DataSource ds() {
+    private DataSource getTargetDataSource() {
         return getDelegate().getDataSource();
     }
 
@@ -112,18 +129,18 @@ public class RoutedDataSource implements DataSource {
      * This method is used to avoir to fork two projects to implement
      * datasource of the jre5 and jre6
      *
-     * @param ds the wrapped datasource
-     * @param mtdName the method to call
+     * @param ds         the wrapped datasource
+     * @param mtdName    the method to call
      * @param paramTypes the parameter type
-     * @param args the arguments
+     * @param args       the arguments
      * @return the return value of the method
      */
-    private Object callByReflection(DataSource ds, String mtdName,
-            Class<?>[] paramTypes, Object[] args) {
+    private Object callByReflection(DataSource ds, String mtdName, Class<?>[] paramTypes, Object[] args) {
         Method mtd;
         try {
             mtd = ds.getClass().getDeclaredMethod(mtdName, paramTypes);
             return mtd.invoke(ds, args);
+
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
