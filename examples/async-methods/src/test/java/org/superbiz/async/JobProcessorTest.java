77d844b1b115bbdc17bf1d097531f850761073fb
David Blevins <dblevins@apache.org>
2011-04-09 00:38:52 +0000

    OPENEJB-1511: Example: @Asynchronous Methods
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1090498 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/async-methods/src/test/java/org/superbiz/async/JobProcessorTest.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.superbiz.async;
+
+import junit.framework.TestCase;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class JobProcessorTest extends TestCase {
+
+    public void test() throws Exception {
+
+        final Context context = EJBContainer.createEJBContainer().getContext();
+
+        final JobProcessor processor = (JobProcessor) context.lookup("java:global/async-methods/JobProcessor");
+
+        final long start = System.nanoTime();
+
+        // Queue up a bunch of work
+        final Future<String> red = processor.addJob("red");
+        final Future<String> orange = processor.addJob("orange");
+        final Future<String> yellow = processor.addJob("yellow");
+        final Future<String> green = processor.addJob("green");
+        final Future<String> blue = processor.addJob("blue");
+        final Future<String> violet = processor.addJob("violet");
+
+        // Wait for the result -- 1 minute worth of work
+        assertEquals("blue", blue.get());
+        assertEquals("orange", orange.get());
+        assertEquals("green", green.get());
+        assertEquals("red", red.get());
+        assertEquals("yellow", yellow.get());
+        assertEquals("violet", violet.get());
+
+        // How long did it take?
+        final long total = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start);
+
+        // Execution should be around 9 - 21 seconds
+        assertTrue("" + total, total > 9);
+        assertTrue("" + total, total < 21);
+
+    }
+
+
+}
