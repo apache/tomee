6fe0f95381fb08daf27288222c6f3d30ab2fce9d
David Blevins <dblevins@apache.org>
2011-04-10 17:10:18 +0000

    OPENEJB-1519: Update existing examples to EJB 3.1
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1090835 13f79535-47bb-0310-9956-ffa450edef68

--- examples/jpa-hibernate/src/test/java/org/superbiz/injection/h3jpa/MoviesTest.java
+++ examples/jpa-hibernate/src/test/java/org/superbiz/injection/h3jpa/MoviesTest.java
@@ -18,8 +18,8 @@ package org.superbiz.injection.h3jpa;
 
 import junit.framework.TestCase;
 
+import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
-import javax.naming.InitialContext;
 import java.util.List;
 import java.util.Properties;
 
@@ -29,20 +29,13 @@ import java.util.Properties;
 public class MoviesTest extends TestCase {
 
     public void test() throws Exception {
-        Properties p = new Properties();
-        p.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+        final Properties p = new Properties();
         p.put("movieDatabase", "new://Resource?type=DataSource");
         p.put("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
         p.put("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
 
-        p.put("movieDatabaseUnmanaged", "new://Resource?type=DataSource");
-        p.put("movieDatabaseUnmanaged.JdbcDriver", "org.hsqldb.jdbcDriver");
-        p.put("movieDatabaseUnmanaged.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
-        p.put("movieDatabaseUnmanaged.JtaManaged", "false");
-
-        Context context = new InitialContext(p);
-
-        Movies movies = (Movies) context.lookup("MoviesLocal");
+        final Context context = EJBContainer.createEJBContainer(p).getContext();
+        Movies movies = (Movies) context.lookup("java:global/jpa-hibernate/Movies");
 
         movies.addMovie(new Movie("Quentin Tarantino", "Reservoir Dogs", 1992));
         movies.addMovie(new Movie("Joel Coen", "Fargo", 1996));
