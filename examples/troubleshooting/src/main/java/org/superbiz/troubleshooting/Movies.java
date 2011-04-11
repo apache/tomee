577b6d7c56b45218b25a285433e991c00895ae90
David Blevins <dblevins@apache.org>
2011-04-11 11:05:46 +0000

    OPENEJB-1519: Update existing examples to EJB 3.1
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1091024 13f79535-47bb-0310-9956-ffa450edef68

--- examples/troubleshooting/src/main/java/org/superbiz/troubleshooting/Movies.java
+++ examples/troubleshooting/src/main/java/org/superbiz/troubleshooting/Movies.java
@@ -16,13 +16,31 @@
  */
 package org.superbiz.troubleshooting;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
 import java.util.List;
 
-public interface Movies {
-    void addMovie(Movie movie) throws Exception;
+//START SNIPPET: code
+@Stateless
+public class Movies {
 
-    void deleteMovie(Movie movie) throws Exception;
+    @PersistenceContext(unitName = "movie-unit", type = PersistenceContextType.TRANSACTION)
+    private EntityManager entityManager;
 
-    List<Movie> getMovies() throws Exception;
+    public void addMovie(Movie movie) throws Exception {
+        entityManager.persist(movie);
+    }
+
+    public void deleteMovie(Movie movie) throws Exception {
+        entityManager.remove(movie);
+    }
+
+    public List<Movie> getMovies() throws Exception {
+        Query query = entityManager.createQuery("SELECT m from Movie as m");
+        return query.getResultList();
+    }
 
 }
