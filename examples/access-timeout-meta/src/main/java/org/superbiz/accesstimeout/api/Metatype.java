2b0b290aff8571e08fee713b6c7b2a5269ac26f2
David Blevins <dblevins@apache.org>
2011-04-12 10:38:17 +0000

    Meta version of access timeout
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1091363 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/access-timeout-meta/src/main/java/org/superbiz/accesstimeout/api/Metatype.java
@@ -0,0 +1,12 @@
+package org.superbiz.accesstimeout.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Metatype
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface Metatype {
+}
