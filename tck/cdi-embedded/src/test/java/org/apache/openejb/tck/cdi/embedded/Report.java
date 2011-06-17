d1f45bdb01844fae0b66cab0a9fa42ec557fcbaf
David Blevins <dblevins@apache.org>
2011-06-17 20:42:36 +0000

    Refined the reporting ability slightly
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1137015 13f79535-47bb-0310-9956-ffa450edef68

--- tck/cdi-embedded/src/test/java/org/apache/openejb/tck/cdi/embedded/Report.java
+++ tck/cdi-embedded/src/test/java/org/apache/openejb/tck/cdi/embedded/Report.java
@@ -23,10 +23,15 @@ import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @version $Rev$ $Date$
@@ -40,7 +45,7 @@ public class Report {
     private final LinkedList<TestClass> classes = new LinkedList<TestClass>();
 
     private void main() throws Exception {
-        final File file = new File("/Users/dblevins/work/uber/geronimo-tck-public-trunk/jcdi-tck-runner/latest.xml");
+        final File file = new File("/Users/dblevins/work/uber/geronimo-tck-public-trunk/jcdi-tck-runner/target/surefire-reports/testng-results.xml");
 
         final SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
 
@@ -60,18 +65,45 @@ public class Report {
 
         Collections.sort(classes);
 
-        int i = 0;
+        final File report = new File(file.getParentFile(), file.getName().replaceAll(".xml$", ".txt"));
+        final PrintStream out = new PrintStream(new FileOutputStream(report));
+        printResults(out);
+        out.close();
+    }
+
+    private void printResults(PrintStream out) {
+
+        Map<Status, AtomicInteger> totals = new HashMap<Status, AtomicInteger>();
+        for (Status status : Status.values()) {
+            totals.put(status, new AtomicInteger());
+        }
+
         for (TestClass testClass : classes) {
 
             for (TestResult result : testClass.getResults()) {
-                if (result.status == Status.PASS) continue;
+                if (result.name.equals("beforeClass")) continue;
+                if (result.name.equals("afterClass")) continue;
+                if (result.name.equals("afterSuite")) continue;
+                if (result.name.equals("beforeSuite")) continue;
+//                if (result.status == Status.PASS) continue;
+                totals.get(result.status).getAndIncrement();
 
-                System.out.printf("%s(%s)\n", result.name, testClass.name);
+                out.printf("%s - %s(%s)\n", result.status, result.name, testClass.name);
             }
         }
-        i++;
 
-        System.out.println(i);
+        out.println("\n\n");
+
+        int total = 0;
+
+        for (Map.Entry<Status, AtomicInteger> entry : totals.entrySet()) {
+            final int i = entry.getValue().get();
+            total += i;
+            out.printf("%5s %s\n", i, entry.getKey());
+        }
+
+        out.printf("%5s %s\n", total, "Total");
+
     }
 
     public static enum Status {
