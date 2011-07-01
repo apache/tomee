66e0f80e3a45a55614532c1bf2b2682615fd38e1
Romain Manni-Bucau <rmannibucau@apache.org>
2011-07-01 17:46:47 +0000

    fixing cdi tests i broke last commit
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1142006 13f79535-47bb-0310-9956-ffa450edef68

--- tck/bval-embedded/src/test/java/org/apache/openejb/tck/bval/impl/StandaloneContainersImpl.java
+++ tck/bval-embedded/src/test/java/org/apache/openejb/tck/bval/impl/StandaloneContainersImpl.java
@@ -1,7 +1,5 @@
 package org.apache.openejb.tck.bval.impl;
 
-import org.apache.openejb.tck.impl.Archive;
-
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,66 +13,63 @@ import java.util.Collection;
  * @author Romain Manni-Bucau
  */
 public class StandaloneContainersImpl extends org.apache.openejb.tck.impl.StandaloneContainersImpl {
-    @Override public boolean deploy(Collection<Class<?>> classes, Collection<URL> xmls) {
-        if ( xmls == null || !xmls.iterator().hasNext() ) {
-			Thread.currentThread().setContextClassLoader( new IgnoringValidationXmlClassLoader() );
-			return true;
-		}
-
-		URL validationXmlUrl = xmls.iterator().next();
-		Thread.currentThread().setContextClassLoader( new CustomValidationXmlClassLoader( validationXmlUrl.getPath() ) );
+    @Override
+    public boolean deploy(Collection<Class<?>> classes, Collection<URL> xmls) {
+        if (xmls == null || !xmls.iterator().hasNext()) {
+            Thread.currentThread().setContextClassLoader(new IgnoringValidationXmlClassLoader());
+        } else {
+            URL validationXmlUrl = xmls.iterator().next();
+            Thread.currentThread().setContextClassLoader(new CustomValidationXmlClassLoader(validationXmlUrl.getPath()));
+        }
 
         return super.deploy(classes, xmls);
     }
 
     private static class CustomValidationXmlClassLoader extends ClassLoader {
-		private final String customValidationXmlPath;
+        private final String customValidationXmlPath;
 
-		CustomValidationXmlClassLoader(String pathToCustomValidationXml) {
-			super( CustomValidationXmlClassLoader.class.getClassLoader() );
-			customValidationXmlPath = pathToCustomValidationXml;
-		}
+        CustomValidationXmlClassLoader(String pathToCustomValidationXml) {
+            super(CustomValidationXmlClassLoader.class.getClassLoader());
+            customValidationXmlPath = pathToCustomValidationXml;
+        }
 
-		public InputStream getResourceAsStream(String path) {
-			InputStream in;
-			if ( "META-INF/validation.xml".equals( path ) ) {
-				if ( customValidationXmlPath.contains( ".jar!" ) ) {
-					path = customValidationXmlPath.substring( customValidationXmlPath.indexOf( "!" ) + 2 );
-					in = super.getResourceAsStream( path );
-				}
-				else {
-					in = loadFromDisk();
-				}
-			}
-			else {
-				in = super.getResourceAsStream( path );
-			}
-			return in;
-		}
+        public InputStream getResourceAsStream(String path) {
+            InputStream in;
+            if ("META-INF/validation.xml".equals(path)) {
+                if (customValidationXmlPath.contains(".jar!")) {
+                    path = customValidationXmlPath.substring(customValidationXmlPath.indexOf("!") + 2);
+                    in = super.getResourceAsStream(path);
+                } else {
+                    in = loadFromDisk();
+                }
+            } else {
+                in = super.getResourceAsStream(path);
+            }
+            return in;
+        }
 
-		private InputStream loadFromDisk() {
-			InputStream in;
-			try {
-				in = new BufferedInputStream( new FileInputStream( customValidationXmlPath ) );
-			}
-			catch (IOException ioe) {
-				String msg = "Unble to load " + customValidationXmlPath + " from  disk";
-				throw new RuntimeException( msg );
-			}
-			return in;
-		}
-	}
+        private InputStream loadFromDisk() {
+            InputStream in;
+            try {
+                in = new BufferedInputStream(new FileInputStream(customValidationXmlPath));
+            } catch (IOException ioe) {
+                String msg = "Unble to load " + customValidationXmlPath + " from  disk";
+                throw new RuntimeException(msg);
+            }
+            return in;
+        }
+    }
 
-	private static class IgnoringValidationXmlClassLoader extends ClassLoader {
-		IgnoringValidationXmlClassLoader() {
-			super( IgnoringValidationXmlClassLoader.class.getClassLoader() );
-		}
+    private static class IgnoringValidationXmlClassLoader extends ClassLoader {
+        IgnoringValidationXmlClassLoader() {
+            super(IgnoringValidationXmlClassLoader.class.getClassLoader());
+        }
 
-		public InputStream getResourceAsStream(String path) {
-			if ( "META-INF/validation.xml".equals( path ) ) {
-				return null;
-			}
-			return super.getResourceAsStream( path );
-		}
-	}
+        public InputStream getResourceAsStream(String path) {
+            if ("META-INF/validation.xml".equals(path)) {
+                return null;
+            }
+            return super.getResourceAsStream(path);
+        }
+    }
 }
