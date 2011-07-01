2c0239eaa602e0f3758ef996dce629a61e4677e4
Romain Manni-Bucau <rmannibucau@apache.org>
2011-07-01 17:47:33 +0000

    fixing cdi tests i broke last commit
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1142007 13f79535-47bb-0310-9956-ffa450edef68

--- tck/tck-common/src/main/java/org/apache/openejb/tck/impl/Archive.java
+++ tck/tck-common/src/main/java/org/apache/openejb/tck/impl/Archive.java
@@ -1,20 +1,31 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
+ * Licensed to the Apache Software Foundation (ASF) under one or
+ * more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with
+ * this work for additional information regarding copyright
+ * ownership.
+ * The ASF licenses this file to You under the Apache License,
+ * Version 2.0
+ * (the "License"); you may not use this file except in
+ * compliance with
  * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
+ *  Unless required by applicable law or agreed to in writing,
+ *  software
+ *  distributed under the License is distributed on an "AS IS"
+ *  BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied.
+ *  See the License for the specific language governing
+ *  permissions and
  *  limitations under the License.
  */
 package org.apache.openejb.tck.impl;
+
+import org.apache.openejb.tck.util.ZipUtil;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -41,85 +52,77 @@ public class Archive {
     private final InputStream in;
 
 
-    public Archive(Iterable<URL> urls, Iterable<Class<?>> classes) {
+    public Archive(Iterable<URL> urls, Iterable<Class<?>>
+            classes) {
 
         try {
             ClassLoader loader = Archive.class.getClassLoader();
 
             // Create the ZIP file
-            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            final ByteArrayOutputStream byteArrayOutputStream =
+                    new ByteArrayOutputStream();
 
-            final ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(byteArrayOutputStream));
+            final ZipOutputStream out = new ZipOutputStream(new
+                    BufferedOutputStream(byteArrayOutputStream));
 
             this.name = name(classes);
 
             for (Class clazz : classes) {
 
-                String name = clazz.getName().replace(".", "/") + ".class";
-                // cla  zz.getName().replace('.', File.separatorChar) + ".class"; // shouldn't work under windows
+                String name = clazz.getName().replace(".", "/")
+                        + ".class";
+                // cla  zz.getName().replace('.',
+                // File.separatorChar) + ".class"; // shouldn't
+                // work under windows
 
                 URL resource = loader.getResource(name);
                 if (resource == null) {
-                    String path = clazz.getProtectionDomain().getCodeSource() + "!" + name;
+                    String path =
+                            clazz.getProtectionDomain().getCodeSource() + "!" + name;
                     try {
                         resource = new URL(path);
                     } catch (MalformedURLException mue) {
-                        fail("can't find " + clazz.getName() + " neither from " + name + " nor from " + path);
+                        fail("can't find " + clazz.getName() + "neither from" + name + " nor from" + path);
                     }
                 }
                 assertNotNull(resource);
                 InputStream in = new BufferedInputStream(resource.openStream());
-
                 // Add ZIP entry to output stream.
                 out.putNextEntry(new ZipEntry(name));
-
-                int i;
-                while ((i = in.read()) != -1) {
-                    out.write(i);
-                }
+                ZipUtil.copy(in, out);
 
                 // Complete the entry
                 out.closeEntry();
             }
 
-            boolean willBeDeployed = false;
-            int i;
             for (final URL url : urls) {
 
-                final String fileName = new File(url.getFile()).getName();
+                final String fileName = new
+                        File(url.getFile()).getName();
                 final String name = "META-INF/" + fileName;
 
                 out.putNextEntry(new ZipEntry(name));
 
-                final InputStream in = new BufferedInputStream(url.openStream());
+                final InputStream in = new
+                        BufferedInputStream(url.openStream());
 
-                while ((i = in.read()) != -1) {
-                    out.write(i);
-                }
+                ZipUtil.copy(in, out);
 
                 in.close();
-
-                willBeDeployed = willBeDeployed
-                        || "persistence.xml".equals(fileName)
-                        || "ejb-jar.xml".equals(fileName) || "beans.xml".equals(fileName)
-                        || "application.xml".equals(fileName) || "application-client.xml".equals(fileName); // TODO: put eveything in a list: org.apache.openejb.config.DeploymentLoader.discoverModuleType()
             }
 
-            if (!willBeDeployed) { // force it to be a module
+            if (System.getProperty("force.deployment") != null) {
                 out.putNextEntry(new ZipEntry("META-INF/beans.xml"));
                 final InputStream in = new ByteArrayInputStream("<beans />".getBytes());
-
-                while ((i = in.read()) != -1) {
-                    out.write(i);
-                }
-
+                ZipUtil.copy(in, out);
                 in.close();
             }
 
             // Complete the ZIP file
             out.close();
 
-            this.in = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            this.in = new
+                    ByteArrayInputStream(byteArrayOutputStream.toByteArray());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -135,7 +138,8 @@ public class Archive {
 
     private String name(Iterable<Class<?>> classes) {
         for (Class<?> clazz : classes) {
-            if (clazz.getName().endsWith("AbstractJSR299Test")) continue;
+            if (clazz.getName().endsWith("AbstractJSR299Test"))
+                continue;
             if (clazz.getName().endsWith("Test")) {
                 return clazz.getName() + ".jar";
             }
@@ -144,3 +148,4 @@ public class Archive {
         return "test-archive.jar";
     }
 }
+
