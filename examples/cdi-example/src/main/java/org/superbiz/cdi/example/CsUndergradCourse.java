5cad34794f7704adae46b79f482679532124d6d1
Karan Singh Malhi <kmalhi@apache.org>
2011-07-09 03:20:52 +0000

    Reformatted, added headers, added cdi-example to examples pom.xml
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1144583 13f79535-47bb-0310-9956-ffa450edef68

--- examples/cdi-example/src/main/java/org/superbiz/cdi/example/CsUndergradCourse.java
+++ examples/cdi-example/src/main/java/org/superbiz/cdi/example/CsUndergradCourse.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.superbiz.cdi.example;
 
 import javax.ejb.Stateless;
@@ -5,40 +21,30 @@ import javax.inject.Inject;
 
 @Stateless
 public class CsUndergradCourse implements Course {
-	private String courseName;
-	private int capacity;
-	
-	@Inject
-	private Faculty faculty;
-	
-	public CsUndergradCourse() {
-		this.courseName = "CDI 101 - Introduction to CDI";
-		this.capacity = 100;
-	}
-
-	public String getCourseName() {
-		return courseName;
-	}
-
-	public void setCourseName(String courseName) {
-		this.courseName = courseName;
-	}
-
-	public int getCapacity() {
-		return capacity;
-	}
-
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
-
-	public Faculty getFaculty() {
-		return faculty;
-	}
-
-	public void setFaculty(Faculty faculty) {
-		this.faculty = faculty;
-	}
-	
-	
+    private String courseName;
+    private int capacity;
+    @Inject
+    private Faculty faculty;
+    public CsUndergradCourse() {
+        this.courseName = "CDI 101 - Introduction to CDI";
+        this.capacity = 100;
+    }
+    public String getCourseName() {
+        return courseName;
+    }
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public Faculty getFaculty() {
+        return faculty;
+    }
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
 }
