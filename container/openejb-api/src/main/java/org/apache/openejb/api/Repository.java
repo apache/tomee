c06ac01312894c804629172f955c2d12e4d28287
Romain Manni-Bucau <rmannibucau@apache.org>
2011-08-01 13:15:27 +0000

    @Documented on @Repository
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1152758 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-api/src/main/java/org/apache/openejb/api/Repository.java
+++ container/openejb-api/src/main/java/org/apache/openejb/api/Repository.java
@@ -1,16 +1,14 @@
 package org.apache.openejb.api;
 
 import javax.persistence.PersistenceContext;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author rmannibucau
  */
 @Target({ ElementType.TYPE, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Repository {
     PersistenceContext context() default @PersistenceContext;
     String jndiName() default "";
