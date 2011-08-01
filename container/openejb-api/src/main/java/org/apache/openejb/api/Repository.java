dca262daf11dea4b45356caccc5bd824ecb4e7a4
Romain Manni-Bucau <rmannibucau@apache.org>
2011-08-01 13:20:42 +0000

    removing stars imports into @Repository
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1152760 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-api/src/main/java/org/apache/openejb/api/Repository.java
+++ container/openejb-api/src/main/java/org/apache/openejb/api/Repository.java
@@ -1,7 +1,11 @@
 package org.apache.openejb.api;
 
 import javax.persistence.PersistenceContext;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author rmannibucau
