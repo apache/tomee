c7bb958e9ae4450ddd57b1981a600a2e305c39b4
Romain Manni-Bucau <rmannibucau@apache.org>
2011-08-18 21:52:40 +0000

    if the app speicfy a jaxrs servlet it should be the openejb one (org.apache.openejb.server.rest.OpenEJBRestServlet) and automatic deployment is ignored
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1159429 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/openejb-rest/src/main/java/org/apache/openejb/server/rest/OpenEJBRestServlet.java
@@ -0,0 +1,11 @@
+package org.apache.openejb.server.rest;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Nothing to do, cf AnnotationDeployer.deploy().
+ *
+ * @author rmannibucau
+ */
+public class OpenEJBRestServlet extends HttpServlet {
+}
