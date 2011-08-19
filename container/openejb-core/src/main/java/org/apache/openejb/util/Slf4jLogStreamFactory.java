8f9e489a0e95a2ac5887f5e693c251b349f40790
Jean-Louis Monteiro <jlmonteiro@apache.org>
2011-08-19 13:46:18 +0000

    OPENEJB-1408 Add SLF4J factory Thanks Michael Glauche!
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1159637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-core/src/main/java/org/apache/openejb/util/Slf4jLogStreamFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.util;
+
+/**
+ * Simple Slf4jLogStream, not much configuration needed, as slf4j is just a facade 
+ *
+ */
+
+public class Slf4jLogStreamFactory implements LogStreamFactory {
+
+	@Override
+	public LogStream createLogStream(LogCategory logCategory) {
+		return new Slf4jLogStream(logCategory);
+	}
+
+}
