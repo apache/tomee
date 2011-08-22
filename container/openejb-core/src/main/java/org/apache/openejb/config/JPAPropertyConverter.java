bd7d92f714b2a2a2563018eea511494ba9ef448f
Romain Manni-Bucau <rmannibucau@apache.org>
2011-08-22 05:03:45 +0000

    adding JPAPropertyConverter - to enhance ASAP
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1160107 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-core/src/main/java/org/apache/openejb/config/JPAPropertyConverter.java
@@ -0,0 +1,46 @@
+package org.apache.openejb.config;
+
+import org.apache.openejb.loader.SystemInstance;
+
+/**
+ * @author rmannibucau
+ */
+public final class JPAPropertyConverter {
+    private JPAPropertyConverter() {
+        // no-op
+    }
+
+    public static class Pair {
+        private String key;
+        private String value;
+
+        public Pair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    // TODO: manage more properties
+    public static Pair toOpenJPAValue(String key, String value) {
+        if (!Boolean.parseBoolean(SystemInstance.get().getProperty("openejb.convert-jpa-properties", "false"))) {
+            return null;
+        }
+
+        if (key.startsWith("eclipselink.ddl-generation")) {
+            if ("create-tables".equals(value)) {
+                return new Pair("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)");
+            } else if ("drop-and-create-tables".equals("value")) {
+                return new Pair("openjpa.jdbc.SynchronizeMappings", "buildSchema(SchemaAction='add,deleteTableContents')");
+            }
+        }
+        return null;
+    }
+}
