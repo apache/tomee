d02d1f8c66a185b70cb2b6b45e1375a10a76fa12
David Blevins <dblevins@apache.org>
2011-09-11 22:51:58 +0000

    Guts for embedded TomEE setup
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1169566 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assembly/openejb-tomcat/tomee-embedded/src/main/java/org/apache/tomee/embedded/Configuration.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tomee.embedded;
+
+/**
+* @version $Rev$ $Date$
+*/
+public class Configuration {
+
+    private int httpPort = 8080;
+    private int stopPort = 8005;
+    protected String dir;
+
+    public int getHttpPort() {
+        return httpPort;
+    }
+
+    public void setHttpPort(int httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    public int getStopPort() {
+        return stopPort;
+    }
+
+    public void setStopPort(int stopPort) {
+        this.stopPort = stopPort;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+}
