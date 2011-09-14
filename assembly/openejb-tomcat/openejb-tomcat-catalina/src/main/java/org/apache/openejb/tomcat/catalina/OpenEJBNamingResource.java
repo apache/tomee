0e03fca4d9ccd7c9a0b14b6326b0ce6eb0200ffb
David Blevins <dblevins@apache.org>
2011-09-14 23:12:17 +0000

    null check on all env types that might be affected by null type from 'lookup' refs
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1170895 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/openejb-tomcat/openejb-tomcat-catalina/src/main/java/org/apache/openejb/tomcat/catalina/OpenEJBNamingResource.java
+++ assembly/openejb-tomcat/openejb-tomcat-catalina/src/main/java/org/apache/openejb/tomcat/catalina/OpenEJBNamingResource.java
@@ -1,27 +1,76 @@
 package org.apache.openejb.tomcat.catalina;
 
+import org.apache.catalina.deploy.ContextEjb;
 import org.apache.catalina.deploy.ContextEnvironment;
+import org.apache.catalina.deploy.ContextLocalEjb;
+import org.apache.catalina.deploy.ContextResource;
 import org.apache.catalina.deploy.ContextResourceEnvRef;
+import org.apache.catalina.deploy.ContextResourceLink;
+import org.apache.catalina.deploy.ContextService;
+import org.apache.catalina.deploy.MessageDestinationRef;
 import org.apache.catalina.deploy.NamingResources;
+import org.apache.catalina.deploy.ResourceBase;
 
-/**
- * @author rmannibucau
- */
 public class OpenEJBNamingResource extends NamingResources {
+
+    @Override
     public void addEnvironment(ContextEnvironment environment) {
-        // tomcat uses a hastable to store entry type, null values are not allowed
-        if (environment.getType() == null) {
-            environment.setType("");
-        }
+        normalize(environment);
         super.addEnvironment(environment);
     }
 
     @Override
     public void addResourceEnvRef(ContextResourceEnvRef ref) {
-        // tomcat uses a hastable to store entry type, null values are not allowed
+        normalize(ref);
+        super.addResourceEnvRef(ref);
+    }
+
+    @Override
+    public void addEjb(ContextEjb ref) {
+        normalize(ref);
+        super.addEjb(ref);
+    }
+
+    @Override
+    public void addLocalEjb(ContextLocalEjb ref) {
+        normalize(ref);
+        super.addLocalEjb(ref);
+    }
+
+    @Override
+    public void addResource(ContextResource ref) {
+        normalize(ref);
+        super.addResource(ref);
+    }
+
+    @Override
+    public void addMessageDestinationRef(MessageDestinationRef ref) {
+        normalize(ref);
+        super.addMessageDestinationRef(ref);
+    }
+
+    @Override
+    public void addService(ContextService ref) {
+        normalize(ref);
+        super.addService(ref);
+    }
+
+    @Override
+    public void addResourceLink(ContextResourceLink ref) {
+        normalize(ref);
+        super.addResourceLink(ref);
+    }
+
+    /**
+     * tomcat uses a hastable to store entry type, null values are not allowed
+     * <p/>
+     * These occur when the reference is decalred using a 'lookup' attribute These do not have a type associated
+     *
+     * @param ref
+     */
+    private void normalize(ResourceBase ref) {
         if (ref.getType() == null) {
             ref.setType("");
         }
-        super.addResourceEnvRef(ref);
     }
 }
