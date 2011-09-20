4f3b13dddd0633b020fd24951133d5296f392e4f
Romain Manni-Bucau <rmannibucau@apache.org>
2011-09-20 12:27:20 +0000

    allowing to manage web app version for each app
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1173110 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/openejb-tomcat/openejb-tomcat-catalina/src/main/java/org/apache/openejb/tomcat/catalina/OpenEJBContextConfig.java
+++ assembly/openejb-tomcat/openejb-tomcat-catalina/src/main/java/org/apache/openejb/tomcat/catalina/OpenEJBContextConfig.java
@@ -1,5 +1,6 @@
 package org.apache.openejb.tomcat.catalina;
 
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.deploy.WebXml;
 import org.apache.catalina.startup.ContextConfig;
 
@@ -8,12 +9,28 @@ import org.apache.catalina.startup.ContextConfig;
  */
 public class OpenEJBContextConfig extends ContextConfig {
     @Override protected WebXml createWebXml() {
-        return new OpenEJBWebXml();
+        String prefix = "";
+        if (context instanceof StandardContext) {
+            StandardContext standardContext = (StandardContext) context;
+            prefix = standardContext.getEncodedPath();
+            if (prefix.startsWith("/")) {
+                prefix = prefix.substring(1);
+            }
+        }
+        return new OpenEJBWebXml(prefix);
     }
 
     private class OpenEJBWebXml extends WebXml {
+        public static final String OPENEJB_WEB_XML_MAJOR_VERSION_PROPERTY = "openejb.web.xml.major";
+        private String prefix;
+
+        public OpenEJBWebXml(String prefix) {
+            this.prefix = prefix;
+        }
+
         @Override public int getMajorVersion() {
-            return Integer.getInteger("openejb.web.xml.major", super.getMajorVersion());
+            return Integer.getInteger(prefix + "." + OPENEJB_WEB_XML_MAJOR_VERSION_PROPERTY,
+                    Integer.getInteger(OPENEJB_WEB_XML_MAJOR_VERSION_PROPERTY, super.getMajorVersion()));
         }
     }
 }
