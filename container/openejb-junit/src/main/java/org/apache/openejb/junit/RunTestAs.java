9159d439dcfbc6755e599fdac690a25955b2bfc3
David Blevins <dblevins@apache.org>
2011-09-25 07:45:27 +0000

    added missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1175319 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-junit/src/main/java/org/apache/openejb/junit/RunTestAs.java
+++ container/openejb-junit/src/main/java/org/apache/openejb/junit/RunTestAs.java
@@ -1,22 +1,18 @@
 /*
- * Unless otherwise stated, this program in binary or source code
- * format and any accompanying media/files/resources are
- * Copyright 2009 Kunye Mining Solutions
- * 
- * Contact Details
- * ------------------------------------------------
- *   Web Site: http://www.kunye.net
- *  Telephone: +27 12 807 3590/1
- *        Fax: +27 12 807 3592
- *       Cell: +27 82 855 5907
- *     E-Mail: akruger@kunye.net
- * 
- * Postal Address
- * ------------------------------------------------
- *  P.O. Box 70032
- *  Die Wilgers
- *  Pretoria
- *  0041
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+    * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.openejb.junit;
 
