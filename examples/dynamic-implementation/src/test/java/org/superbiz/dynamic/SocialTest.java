7c27eb629b847be4d192e635f3b739d976396b51
Romain Manni-Bucau <rmannibucau@apache.org>
2011-09-28 21:43:25 +0000

    adding dynamic-implementation example
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb3@1177076 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/dynamic-implementation/src/test/java/org/superbiz/dynamic/SocialTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+    * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.superbiz.dynamic;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ejb.embeddable.EJBContainer;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author rmannibucau
+ */
+public class SocialTest {
+    private static SocialBean social;
+    private static EJBContainer container;
+
+    @BeforeClass public static void init() throws Exception {
+        container = EJBContainer.createEJBContainer();
+        social = (SocialBean) container.getContext().lookup("java:global/dynamic-implementation/SocialBean");
+    }
+
+    @AfterClass public static void close() {
+        container.close();
+    }
+
+    @Test public void simple() {
+        assertTrue(social.facebookStatus().contains("think"));
+        assertTrue(social.twitterStatus().contains("eat"));
+        assertTrue(social.status().contains("virtual"));
+    }
+}
