cc871b629ded682b268f05fe8add82b088c1ffae
Romain Manni-Bucau <rmannibucau@apache.org>
2011-10-07 13:57:31 +0000

    overriding javax.persistence properties earlier since we test it earlier to force some needed values if they are missing TOMEE-75: Resolve default persistence provider before Assembly
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1180040 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/PersistenceBuilder.java
+++ container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/PersistenceBuilder.java
@@ -38,54 +38,15 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class PersistenceBuilder {
-
     public static final Logger logger = Logger.getInstance(LogCategory.OPENEJB_STARTUP, PersistenceBuilder.class);
-
-    public static final String PROVIDER_PROP = "javax.persistence.provider";
-
-    public static final String TRANSACTIONTYPE_PROP = "javax.persistence.transactionType";
-
-    public static final String JTADATASOURCE_PROP = "javax.persistence.jtaDataSource";
-
-    public static final String NON_JTADATASOURCE_PROP = "javax.persistence.nonJtaDataSource";
-
-    private static final String DEFAULT_PERSISTENCE_PROVIDER = "org.apache.openjpa.persistence.PersistenceProviderImpl";
 
     /**
      * External handler which handles adding a runtime ClassTransformer to the classloader.
      */
     private final PersistenceClassLoaderHandler persistenceClassLoaderHandler;
 
-    /**
-     * If set, overrides the persistence provider class name in the persistence.xml.
-     */
-    private String providerEnv;
-
-    /**
-     * If set, overrides the transaction type in the persistence.xml.
-     */
-    private String transactionTypeEnv;
-
-    /**
-     * If set, overrides the jta data source class name in the persistence.xml.
-     */
-    private String jtaDataSourceEnv;
-
-    /**
-     * If set, overrides the non-jta data source class name in the persistence.xml.
-     */
-    private String nonJtaDataSourceEnv;
-
     public PersistenceBuilder(PersistenceClassLoaderHandler persistenceClassLoaderHandler) {
-        loadSystemProps();
         this.persistenceClassLoaderHandler = persistenceClassLoaderHandler;
-    }
-
-    private void loadSystemProps() {
-        providerEnv = SystemInstance.get().getProperty(PROVIDER_PROP);
-        transactionTypeEnv = SystemInstance.get().getProperty(TRANSACTIONTYPE_PROP);
-        jtaDataSourceEnv = SystemInstance.get().getProperty(JTADATASOURCE_PROP);
-        nonJtaDataSourceEnv = SystemInstance.get().getProperty(NON_JTADATASOURCE_PROP);
     }
 
     public EntityManagerFactory createEntityManagerFactory(PersistenceUnitInfo info, ClassLoader classLoader) throws Exception {
@@ -98,11 +59,7 @@ public class PersistenceBuilder {
         unitInfo.setPersistenceUnitName(info.name);
 
         // Persistence Provider Class Name
-        if (providerEnv != null) {
-            unitInfo.setPersistenceProviderClassName(providerEnv);
-        } else {
-            unitInfo.setPersistenceProviderClassName(info.provider);
-        }
+        unitInfo.setPersistenceProviderClassName(info.provider);
 
         // ClassLoader
         unitInfo.setClassLoader(classLoader);
@@ -114,7 +71,6 @@ public class PersistenceBuilder {
 
         // JTA Datasource
         String jtaDataSourceId = info.jtaDataSource;
-        if (jtaDataSourceEnv != null) jtaDataSourceId = jtaDataSourceEnv;
         if (jtaDataSourceId != null) {
             if (!SystemInstance.get().hasProperty("openejb.geronimo")) {
 
@@ -151,22 +107,11 @@ public class PersistenceBuilder {
         unitInfo.setValidationMode(validationMode);
         
         // Persistence Unit Transaction Type
-        if (transactionTypeEnv != null) {
-            try {
-                // Override with sys vars
-                PersistenceUnitTransactionType type = Enum.valueOf(PersistenceUnitTransactionType.class, transactionTypeEnv.toUpperCase());
-                unitInfo.setTransactionType(type);
-            } catch (IllegalArgumentException e) {
-                throw (IllegalArgumentException)(new IllegalArgumentException("Unknown " + TRANSACTIONTYPE_PROP + ", valid options are " + PersistenceUnitTransactionType.JTA + " or " + PersistenceUnitTransactionType.RESOURCE_LOCAL).initCause(e));
-            }
-        } else {
-            PersistenceUnitTransactionType type = Enum.valueOf(PersistenceUnitTransactionType.class, info.transactionType);
-            unitInfo.setTransactionType(type);
-        }
+        PersistenceUnitTransactionType type = Enum.valueOf(PersistenceUnitTransactionType.class, info.transactionType);
+        unitInfo.setTransactionType(type);
 
         // Non JTA Datasource
         String nonJtaDataSourceId = info.nonJtaDataSource;
-        if (nonJtaDataSourceEnv != null) nonJtaDataSourceId = nonJtaDataSourceEnv;
         if (nonJtaDataSourceId != null) {
             if (!SystemInstance.get().hasProperty("openejb.geronimo")) {
                 try {
@@ -185,9 +130,6 @@ public class PersistenceBuilder {
 
         // create the persistence provider
         String persistenceProviderClassName = unitInfo.getPersistenceProviderClassName();
-        if (persistenceProviderClassName == null) {
-            persistenceProviderClassName = DEFAULT_PERSISTENCE_PROVIDER;
-        }
         unitInfo.setPersistenceProviderClassName(persistenceProviderClassName);
 
         final long start = System.nanoTime();
