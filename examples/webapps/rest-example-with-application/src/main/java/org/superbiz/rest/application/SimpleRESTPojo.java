127637af030268c4c1f1edfe80ae9d83f9749758
Romain Manni-Bucau <rmannibucau@apache.org>
2011-10-11 15:19:38 +0000

    adding an example with an applicationpath (rest) TOMEE-73: Example: JAX-RS example with Application
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1181835 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/webapps/rest-example-with-application/src/main/java/org/superbiz/rest/application/SimpleRESTPojo.java
@@ -0,0 +1,15 @@
+package org.superbiz.rest.application;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.Date;
+
+/**
+ * @author rmannibucau
+ */
+@Path("/pojo")
+public class SimpleRESTPojo {
+    @GET public String pojo() {
+        return "pojo ok @ " + new Date().toString();
+    }
+}
