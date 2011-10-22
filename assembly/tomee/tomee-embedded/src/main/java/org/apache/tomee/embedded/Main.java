1f9821c1b312d608e5d75a7dfc80aba076ae319a
Jonathan Gallimore <jgallimore@apache.org>
2011-10-22 15:23:19 +0000

    Create an all-in-one jar that can be extracted from a .war and started. Maybe useful for doing something like 'java -jar myapp.war' TOMEE-96: All-in-one TomEE Embedded jar
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1187723 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assembly/tomee/tomee-embedded/src/main/java/org/apache/tomee/embedded/Main.java
@@ -0,0 +1,45 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tomee.embedded;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Container container = new Container();
+			container.start();
+			
+			for (String filename : args) {
+				File file = new File(filename);
+				if (! file.exists()) {
+					System.out.println(file.getAbsolutePath() + " does not exist, skipping");
+					continue;
+				}
+				
+				String filenameWithoutExtension = file.getName().replaceAll("\\.[A-Za-z]+$", "");
+				container.deploy(filenameWithoutExtension, file);
+			}
+			
+			container.await();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
