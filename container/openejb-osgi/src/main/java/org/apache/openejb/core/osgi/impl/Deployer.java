63e31c34d9444460f7d3f3c5ace11cdd9e53f0fc
Romain Manni-Bucau <rmannibucau@apache.org>
2011-11-27 21:51:07 +0000

    trying to make a simple deployment working under OSGi OPENEJB-1735: OSGi Support on Apache Karaf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1206854 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-osgi/src/main/java/org/apache/openejb/core/osgi/impl/Deployer.java
+++ container/openejb-osgi/src/main/java/org/apache/openejb/core/osgi/impl/Deployer.java
@@ -32,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.Properties;
@@ -54,44 +55,50 @@ public class Deployer implements BundleListener {
     }
 
     private void deploy(Bundle bundle) {
+        final ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(new OSGIClassLoader(bundle));
+
         LOGGER.info(String.format("[Deployer] Bundle %s has been started", bundle.getSymbolicName()));
 
         LOGGER.info(String.format("[Deployer] Checking whether it's an EJB module"));
-        Enumeration<URL> e = bundle.findEntries("META-INF", "ejb-jar.xml", false);
-        if (e != null && e.hasMoreElements()) {
-            URL ejbJarUrl = e.nextElement();
+        try {
+            Enumeration<URL> e = bundle.findEntries("META-INF", "ejb-jar.xml", false);
+            if (e != null && e.hasMoreElements()) {
+                URL ejbJarUrl = e.nextElement();
 
-            LOGGER.info("[Deployer] It's an EJB module: " + ejbJarUrl);
+                LOGGER.info("[Deployer] It's an EJB module: " + ejbJarUrl);
 
-            LOGGER.info("[Deployer] Deploying onto OpenEJB");
+                LOGGER.info("[Deployer] Deploying onto OpenEJB");
 
-            String location = bundle.getLocation();
-            LOGGER.info("[Deployer] bundle location: " + location);
-            try {
-                File file = new File(new URL(location).getFile());
+                String location = bundle.getLocation();
+                LOGGER.info("[Deployer] bundle location: " + location);
                 try {
-                    DeploymentLoader deploymentLoader = new DeploymentLoader();
-                    AppModule appModule = deploymentLoader.load(file);
+                    File file = new File(new URL(location).getFile());
+                    try {
+                        DeploymentLoader deploymentLoader = new DeploymentLoader();
+                        AppModule appModule = deploymentLoader.load(file);
 
-                    ConfigurationFactory configurationFactory = new ConfigurationFactory();
-                    AppInfo appInfo = configurationFactory.configureApplication(appModule);
+                        ConfigurationFactory configurationFactory = new ConfigurationFactory();
+                        AppInfo appInfo = configurationFactory.configureApplication(appModule);
 
-                    Assembler assembler = SystemInstance.get().getComponent(Assembler.class);
-                    LOGGER.debug("Assembler : " + assembler);
-                    LOGGER.debug("AppInfo id: " + appInfo.appId);
-                    assembler.createApplication(appInfo);
+                        Assembler assembler = SystemInstance.get().getComponent(Assembler.class);
+                        LOGGER.debug("Assembler : " + assembler);
+                        LOGGER.debug("AppInfo id: " + appInfo.appId);
+                        assembler.createApplication(appInfo);
 
-                    LOGGER.info("[Deployer] Application deployed: " + appInfo.path);
+                        LOGGER.info("[Deployer] Application deployed: " + appInfo.path);
 
-                    registerService(bundle, appInfo);
+                        registerService(bundle, appInfo);
 
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+                    } catch (Exception ex) {
+                        LOGGER.error("can't deploy " + ejbJarUrl.toExternalForm(), ex);
+                    }
+                } catch (Exception ex1) {
+                    LOGGER.error("can't deploy " + ejbJarUrl.toExternalForm(), ex1);
                 }
-            } catch (Exception ex1) {
-                ex1.printStackTrace();
             }
-
+        } finally {
+            Thread.currentThread().setContextClassLoader(oldCl);
         }
     }
 
@@ -119,14 +126,59 @@ public class Deployer implements BundleListener {
             for (EnterpriseBeanInfo ejbInfo : ejbJarInfo.enterpriseBeans) {
                 try {
                     context.registerService(ejbInfo.businessRemote.toArray(new String[ejbInfo.businessRemote.size()]), bundle.loadClass(
-                            ejbInfo.ejbClass).newInstance(), new Properties());
+                        ejbInfo.ejbClass).newInstance(), new Properties());
                     LOGGER.info(String.format(
-                            "[Deployer] Service object %s registered under the class names: %s", ejbInfo.ejbClass,
-                            ejbInfo.businessRemote));
+                        "[Deployer] Service object %s registered under the class names: %s", ejbInfo.ejbClass,
+                        ejbInfo.businessRemote));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class OSGIClassLoader extends ClassLoader {
+        private final Bundle backingBundle;
+
+        public OSGIClassLoader(Bundle bundle) {
+            super(null);
+            backingBundle = bundle;
+        }
+
+        protected Class findClass(String name) throws ClassNotFoundException {
+            try {
+                return this.backingBundle.loadClass(name);
+            } catch (ClassNotFoundException cnfe) {
+                throw new ClassNotFoundException(name + " not found from bundle [" + backingBundle.getSymbolicName() + "]", cnfe);
+            } catch (NoClassDefFoundError ncdfe) {
+                NoClassDefFoundError e = new NoClassDefFoundError(name + " not found from bundle [" + backingBundle + "]");
+                e.initCause(ncdfe);
+                throw e;
+            }
+        }
+
+        protected URL findResource(String name) {
+            return backingBundle.getResource(name);
+        }
+
+        protected Enumeration findResources(String name) throws IOException {
+            return backingBundle.getResources(name);
+        }
+
+        public URL getResource(String name) {
+            return findResource(name);
+        }
+
+        protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            Class clazz = findClass(name);
+            if (resolve) {
+                resolveClass(clazz);
+            }
+            return clazz;
+        }
+
+        public String toString() {
+            return "OSGIClassLoader for [" + backingBundle + "]";
         }
     }
 }
