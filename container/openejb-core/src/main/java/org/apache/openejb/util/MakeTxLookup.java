c4b3193e4177f332e396287038d01dcdee40521d
Romain Manni-Bucau <rmannibucau@apache.org>
2011-12-15 22:40:42 +0000

    OPENEJB-1722 managing new JtaPlatform for hibernate 4
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1214977 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/util/MakeTxLookup.java
+++ container/openejb-core/src/main/java/org/apache/openejb/util/MakeTxLookup.java
@@ -17,8 +17,10 @@
 package org.apache.openejb.util;
 
 import org.apache.xbean.asm.ClassWriter;
+import org.apache.xbean.asm.Label;
 import org.apache.xbean.asm.MethodVisitor;
 import org.apache.xbean.asm.Opcodes;
+import org.apache.xbean.asm.Type;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,6 +29,7 @@ import java.io.IOException;
 public class MakeTxLookup implements Opcodes {
 
     public static final String HIBERNATE_FACTORY = "org.apache.openejb.hibernate.TransactionManagerLookup";
+    public static final String HIBERNATE_NEW_FACTORY = "org.apache.openejb.hibernate.OpenEJBJtaPlatform";
     public static final String TOPLINK_FACTORY = "org.apache.openejb.toplink.JTATransactionController";
     public static final String ECLIPSELINK_FACTORY = "org.apache.openejb.eclipselink.JTATransactionController";
 
@@ -35,8 +38,65 @@ public class MakeTxLookup implements Opcodes {
         File file = new File(args[0]);
 
         createHibernteStrategy(file);
+        createNewHibernateStrategy(file);
         createTopLinkStrategy(file);
         createEclipseLinkStrategy(file);
+    }
+
+    private static void createNewHibernateStrategy(File basedir) throws Exception {
+        ClassWriter cw = new ClassWriter(0);
+        MethodVisitor mv;
+
+        cw.visit(V1_6, ACC_PUBLIC + ACC_SUPER, "org/apache/openejb/hibernate/OpenEJBJtaPlatform", null, "org/hibernate/service/jta/platform/internal/AbstractJtaPlatform", null);
+
+        {
+            mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitMethodInsn(INVOKESPECIAL, "org/hibernate/service/jta/platform/internal/AbstractJtaPlatform", "<init>", "()V");
+            mv.visitInsn(RETURN);
+            mv.visitMaxs(1, 1);
+            mv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PROTECTED, "locateTransactionManager", "()Ljavax/transaction/TransactionManager;", null, null);
+            mv.visitCode();
+            mv.visitMethodInsn(INVOKESTATIC, "org/apache/openejb/OpenEJB", "getTransactionManager", "()Ljavax/transaction/TransactionManager;");
+            mv.visitInsn(ARETURN);
+            mv.visitMaxs(1, 1);
+            mv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PROTECTED, "locateUserTransaction", "()Ljavax/transaction/UserTransaction;", null, null);
+            mv.visitCode();
+            Label l0 = new Label();
+            Label l1 = new Label();
+            Label l2 = new Label();
+            mv.visitTryCatchBlock(l0, l1, l2, "javax/naming/NamingException");
+            mv.visitLabel(l0);
+            mv.visitMethodInsn(INVOKESTATIC, "org/apache/openejb/loader/SystemInstance", "get", "()Lorg/apache/openejb/loader/SystemInstance;");
+            mv.visitLdcInsn(Type.getType("Lorg/apache/openejb/spi/ContainerSystem;"));
+            mv.visitMethodInsn(INVOKEVIRTUAL, "org/apache/openejb/loader/SystemInstance", "getComponent", "(Ljava/lang/Class;)Ljava/lang/Object;");
+            mv.visitTypeInsn(CHECKCAST, "org/apache/openejb/spi/ContainerSystem");
+            mv.visitMethodInsn(INVOKEINTERFACE, "org/apache/openejb/spi/ContainerSystem", "getJNDIContext", "()Ljavax/naming/Context;");
+            mv.visitLdcInsn("comp/UserTransaction");
+            mv.visitMethodInsn(INVOKEINTERFACE, "javax/naming/Context", "lookup", "(Ljava/lang/String;)Ljava/lang/Object;");
+            mv.visitTypeInsn(CHECKCAST, "javax/transaction/UserTransaction");
+            mv.visitLabel(l1);
+            mv.visitInsn(ARETURN);
+            mv.visitLabel(l2);
+            mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[]{"javax/naming/NamingException"});
+            mv.visitVarInsn(ASTORE, 1);
+            mv.visitInsn(ACONST_NULL);
+            mv.visitInsn(ARETURN);
+            mv.visitMaxs(2, 2);
+            mv.visitEnd();
+        }
+        cw.visitEnd();
+
+        String factory = HIBERNATE_NEW_FACTORY;
+        String classFilePath = factory.replace('.', '/');
+        write(basedir, cw, classFilePath);
     }
 
     private static void createHibernteStrategy(File baseDir) throws Exception {
@@ -45,7 +105,7 @@ public class MakeTxLookup implements Opcodes {
 
         String classFilePath = factory.replace('.', '/');
 
-        String sourceFileName = factory.substring(factory.lastIndexOf('.')+1, factory.length()) + ".java";
+        String sourceFileName = factory.substring(factory.lastIndexOf('.') + 1, factory.length()) + ".java";
 
         ClassWriter cw = new ClassWriter(0);
         MethodVisitor mv;
@@ -99,7 +159,7 @@ public class MakeTxLookup implements Opcodes {
 
         String classFilePath = factory.replace('.', '/');
 
-        String sourceFileName = factory.substring(factory.lastIndexOf('.')+1, factory.length()) + ".java";
+        String sourceFileName = factory.substring(factory.lastIndexOf('.') + 1, factory.length()) + ".java";
 
 
         ClassWriter cw = new ClassWriter(0);
@@ -138,7 +198,7 @@ public class MakeTxLookup implements Opcodes {
 
         String classFilePath = factory.replace('.', '/');
 
-        String sourceFileName = factory.substring(factory.lastIndexOf('.')+1, factory.length()) + ".java";
+        String sourceFileName = factory.substring(factory.lastIndexOf('.') + 1, factory.length()) + ".java";
 
 
         ClassWriter cw = new ClassWriter(0);
