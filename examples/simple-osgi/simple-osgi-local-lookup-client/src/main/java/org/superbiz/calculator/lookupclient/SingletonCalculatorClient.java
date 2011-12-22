edd42875bcb59ff0c33f1642e108f9cf5e4a2627
Romain Manni-Bucau <rmannibucau@apache.org>
2011-12-22 20:41:47 +0000

    updating the osgi simple example with another bundle as client and with a remote client OPENEJB-1736: Example: OSGi on Apache Karaf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1222439 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/simple-osgi/simple-osgi-local-lookup-client/src/main/java/org/superbiz/calculator/lookupclient/SingletonCalculatorClient.java
@@ -0,0 +1,47 @@
+package org.superbiz.calculator.lookupclient;
+
+import org.superbiz.osgi.calculator.CalculatorLocal;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+@Singleton
+@Startup
+public class SingletonCalculatorClient {
+    @EJB
+    private CalculatorLocal calculator;
+
+    @PostConstruct
+    public void logInit() {
+        System.out.println();
+        checkCalculator();
+        tryLookup();
+        System.out.println();
+    }
+
+    private void tryLookup() {
+        Properties p = new Properties();
+        p.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.localclient.LocalInitialContextFactory");
+        try {
+            Context ctx = new InitialContext(p);
+            CalculatorLocal local = (CalculatorLocal) ctx.lookup("CalculatorBeanLocal");
+            System.out.println("lookup OK: " + local.sayHello());
+        } catch (NamingException e) {
+            System.out.println("can't lookup bean: " + e.getMessage());
+        }
+    }
+
+    private void checkCalculator() {
+        if (calculator == null) {
+            System.out.println(calculator + " is null -> FAILED!");
+        } else {
+            System.out.println("calculator OK: " + calculator.sayHello());
+        }
+    }
+}
