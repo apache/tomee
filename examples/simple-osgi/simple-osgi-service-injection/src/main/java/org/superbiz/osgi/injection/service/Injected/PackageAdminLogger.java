a44ca41be4c1debf906f90240bbe730cfe3b3b7e
Romain Manni-Bucau <rmannibucau@apache.org>
2011-12-23 09:01:09 +0000

    adding simple-osgi-service-injection example OPENEJB-1735: OSGi Support on Apache Karaf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1222611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/simple-osgi/simple-osgi-service-injection/src/main/java/org/superbiz/osgi/injection/service/Injected/PackageAdminLogger.java
@@ -0,0 +1,25 @@
+package org.superbiz.osgi.injection.service.Injected;
+
+import org.osgi.service.startlevel.StartLevel;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+@Singleton
+@Startup
+public class PackageAdminLogger {
+    @Inject
+    private StartLevel sl;
+
+    @PostConstruct
+    public void init() {
+        System.out.println();
+        if (sl != null) {
+            System.out.println("start level is " + sl.getStartLevel());
+        } else {
+            System.out.println("start level is null -> FAILED");
+        }
+    }
+}
