693443b8c3619f274034d2d30877f6b72b96700a
Romain Manni-Bucau <rmannibucau@apache.org>
2011-12-24 10:37:10 +0000

    adding openejb:list and openejb:bundles karaf commands OPENEJB-1735: OSGi Support on Apache Karaf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1222955 13f79535-47bb-0310-9956-ffa450edef68

---
+++ osgi/openejb-karaf-commands/src/main/java/org/apache/openejb/karaf/table/Lines.java
@@ -0,0 +1,49 @@
+package org.apache.openejb.karaf.table;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Lines {
+    private List<Line> lines = new ArrayList<Line>();
+
+    public void add(Line line) {
+        if (!lines.isEmpty() && lines.iterator().next().getColumns().length != line.getColumns().length) {
+            throw new IllegalArgumentException("columns should have all the same size");
+        }
+        lines.add(line);
+    }
+
+    public void print(final PrintStream out) {
+        print(out, true);
+    }
+
+    public void print(final PrintStream out, boolean headers) {
+        final Iterator<Line> it = lines.iterator();
+        if (!it.hasNext()) {
+            return;
+        }
+
+        int[] max = max(lines);
+        it.next().print(max, out, headers);
+        while (it.hasNext()) {
+            it.next().print(max, out);
+        }
+    }
+
+    private static int[] max(final List<Line> lines) {
+        int[] max = new int[lines.iterator().next().getColumns().length];
+        for (Line line : lines) {
+            for (int i = 0; i < max.length; i++) {
+                int ll = line.getColumns()[i].length();
+                if (max[i] == 0) { // init
+                    max[i] = ll;
+                } else if (max[i] < ll) {
+                    max[i] = ll;
+                }
+            }
+        }
+        return max;
+    }
+}
