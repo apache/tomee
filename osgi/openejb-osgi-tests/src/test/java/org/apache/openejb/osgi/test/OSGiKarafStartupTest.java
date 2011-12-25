64f4c5a6bc1dad5f0fca38c33467e9cc758d99f2
Romain Manni-Bucau <rmannibucau@apache.org>
2011-12-25 23:01:58 +0000

    adding an integration test for OSGi OPENEJB-1735: OSGi Support on Apache Karaf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1224656 13f79535-47bb-0310-9956-ffa450edef68

---
+++ osgi/openejb-osgi-tests/src/test/java/org/apache/openejb/osgi/test/OSGiKarafStartupTest.java
@@ -0,0 +1,94 @@
+package org.apache.openejb.osgi.test;
+
+import org.apache.felix.service.command.CommandProcessor;
+import org.apache.felix.service.command.CommandSession;
+import org.apache.openejb.OpenEJB;
+import org.apache.openejb.loader.SystemInstance;
+import org.apache.openejb.spi.ContainerSystem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.TestProbeBuilder;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.junit.ProbeBuilder;
+
+import javax.inject.Inject;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import static org.apache.karaf.tooling.exam.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.osgi.framework.Constants.DYNAMICIMPORT_PACKAGE;
+
+@RunWith(JUnit4TestRunner.class)
+public class OSGiKarafStartupTest {
+    @Inject
+    private CommandProcessor processor;
+
+    @Configuration
+    public Option[] configure() throws Exception {
+        final Properties jreProperties = new Properties();
+        jreProperties.load(new FileReader("../apache-karafee/src/main/filtered-resources/etc/jre.properties"));
+        final String[] packages = trim(jreProperties.getProperty("jre-1.6").split(","));
+
+        return options(
+                karafDistributionConfiguration()
+                        .frameworkUrl(
+                                maven().groupId("org.apache.openejb").artifactId("apache-karafee").versionAsInProject().type("tar.gz"))
+                        .name("Apache Karafee")
+                        .karafVersion("2.2.4"),
+                felix()
+
+                // , vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+        );
+    }
+
+    @ProbeBuilder
+    public TestProbeBuilder probeConfiguration(final TestProbeBuilder probe) {
+        return probe.setHeader(DYNAMICIMPORT_PACKAGE, "*,org.apache.felix.service.*;status=provisional");
+    }
+
+    private static String[] trim(String[] split) {
+        final String[] trimmed = new String[split.length];
+        for (int i = 0; i < split.length; i++) {
+            trimmed[i] = split[i].trim();
+        }
+        return trimmed;
+    }
+
+    @Test
+    public void openejbIsStarted() {
+        assertTrue(OpenEJB.isInitialized());
+        assertNotNull(SystemInstance.get().getComponent(ContainerSystem.class));
+    }
+
+    @Test
+    public void commandsAreInstalled() throws Exception {
+        assertCommand("openejb:list", "Name");
+    }
+
+    private void assertCommand(final String cmd, final String... expected) throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final PrintStream psOut = new PrintStream(out);
+        final ByteArrayOutputStream err = new ByteArrayOutputStream();
+        final PrintStream psErr = new PrintStream(out);
+
+        final CommandSession session = processor.createSession(System.in, psOut, psErr);
+        session.execute(cmd);
+        session.getConsole().flush();
+        session.close();
+
+        assertTrue(err.toString().isEmpty());
+        final String sout = out.toString();
+        for (String s : expected) {
+            assertTrue(sout.contains(s));
+        }
+    }
+}
