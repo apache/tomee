f347f1f0ed915f77bf3408b38b951e46e196c04d
Romain Manni-Bucau <rmannibucau@apache.org>
2012-01-05 21:09:52 +0000

    fixing corrupted comments
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1227837 13f79535-47bb-0310-9956-ffa450edef68

--- arquillian-tomee/arquillian-tomee-tests/src/test/java/org/apache/openejb/arquillian/tests/filterenventry/PojoServletFilter.java
+++ arquillian-tomee/arquillian-tomee-tests/src/test/java/org/apache/openejb/arquillian/tests/filterenventry/PojoServletFilter.java
@@ -63,12 +63,12 @@ public class PojoServletFilter implements Filter {
     @Resource(name = "optionDefault")
     private Character optionDefault;
 
-    / * TODO: Enable this resource after functionality is fixed
+    /* TODO: Enable this resource after functionality is fixed
     @Resource
     */
     private Code defaultCode;
 
-    / * TODO: Enable this resource after functionality is fixed
+    /* TODO: Enable this resource after functionality is fixed
             @Resource
             @SuppressWarnings("unchecked")
     */
