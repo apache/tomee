2b6fa418d2ba7f10e141841d6066accd8476e08a
David Blevins <dblevins@apache.org>
2012-01-05 18:41:44 +0000

    have it use the core version OPENEJB-1755: Repackaging LocalInitialContextFactory into org.apache.openejb.core
    git-svn-id: https://svn.apache.org/repos/asf/openejb/branches/openejb-4.0.0-beta-2@1227736 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/openejb/client/LocalInitialContextFactory.java
+++ container/openejb-core/src/main/java/org/openejb/client/LocalInitialContextFactory.java
@@ -10,5 +10,5 @@ package org.openejb.client;
  * @version $Revision$ $Date$
  * @deprecated use org.apache.openejb.core.LocalInitialContextFactory
  */
-public class LocalInitialContextFactory extends org.apache.openejb.client.LocalInitialContextFactory {
+public class LocalInitialContextFactory extends org.apache.openejb.core.LocalInitialContextFactory {
 }
