b1788cfe72924f464db1dd8601e885b1665a2691
David Blevins <dblevins@apache.org>
2012-01-05 18:41:17 +0000

    re-added the backwards compatible class OPENEJB-1755: Repackaging LocalInitialContextFactory into org.apache.openejb.core
    git-svn-id: https://svn.apache.org/repos/asf/openejb/branches/openejb-4.0.0-beta-2@1227735 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-core/src/main/java/org/openejb/client/LocalInitialContextFactory.java
@@ -0,0 +1,14 @@
+/* =====================================================================
+ *
+ * Copyright (c) 2011 David Blevins.  All rights reserved.
+ *
+ * =====================================================================
+ */
+package org.openejb.client;
+
+/**
+ * @version $Revision$ $Date$
+ * @deprecated use org.apache.openejb.core.LocalInitialContextFactory
+ */
+public class LocalInitialContextFactory extends org.apache.openejb.client.LocalInitialContextFactory {
+}
