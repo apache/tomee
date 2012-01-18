6635341febe1be24b5de280a192468e4de8aad81
Romain Manni-Bucau <rmannibucau@apache.org>
2012-01-18 07:59:13 +0000

    adding OpenEJBGroovyShell in a separated module since it will probably be used by multiple server modules
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1232777 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/openejb-groovy/src/test/java/org/apache/openejb/server/groovy/OpenEJBGroovyShellTest.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.server.groovy;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ejb.embeddable.EJBContainer;
+
+import static org.junit.Assert.assertEquals;
+
+public class OpenEJBGroovyShellTest {
+    private static EJBContainer container;
+
+    @BeforeClass
+    public static void start() {
+        container = EJBContainer.createEJBContainer();
+    }
+
+    @AfterClass
+    public static void close() {
+        container.close();
+    }
+
+    @Test
+    public void call() {
+        final OpenEJBGroovyShell shell = new OpenEJBGroovyShell();
+        final Object out = shell.evaluate("Foo.foo()");
+        assertEquals("foo", out);
+    }
+}
