b864c6659e8751ba1d9996331e555bbf79d153d1
Romain Manni-Bucau <rmannibucau@apache.org>
2012-01-25 16:21:06 +0000

    managing http url in provisioning.properties
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1235809 13f79535-47bb-0310-9956-ffa450edef68

--- utils/openejb-provisionning/src/main/java/org/apache/openejb/resolver/http/Helper.java
+++ utils/openejb-provisionning/src/main/java/org/apache/openejb/resolver/http/Helper.java
@@ -27,7 +27,6 @@ import java.net.ProxySelector;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 
 public class Helper {
     private static final int CONNECT_TIMEOUT = 10000;
@@ -37,7 +36,6 @@ public class Helper {
     }
 
     public static void copyTryingProxies(final URI source, final File destination) throws Exception {
-        final List<Proxy> proxies = ProxySelector.getDefault().select(source);
         final URL url = source.toURL();
         for (Proxy proxy : ProxySelector.getDefault().select(source)) {
             InputStream is;
