06999f468b45f66c8015e51e0181e555434024dc
Romain Manni-Bucau <rmannibucau@apache.org>
2012-01-26 16:58:38 +0000

    adding missing EmptyResourcesClassLoader in last commit
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1236291 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-core/src/main/java/org/apache/openejb/core/EmptyResourcesClassLoader.java
@@ -0,0 +1,22 @@
+package org.apache.openejb.core;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+public class EmptyResourcesClassLoader extends ClassLoader {
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        return new Enumeration<URL>() {
+            @Override
+            public boolean hasMoreElements() {
+                return false;
+            }
+
+            @Override
+            public URL nextElement() {
+                throw new UnsupportedOperationException("this enumeration has no elements");
+            }
+        };
+    }
+}
