ec6779d1d24b29cfe1237840e7abdcd2cc9e93af
David Blevins <dblevins@apache.org>
2012-01-30 01:53:51 +0000

    Excellent patch from Daniel Siwiec, OPENEJB-1612: Example: Sharing of @RequestScoped beans Thank you, Daniel!
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1237525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/cdi-request-scope/src/test/java/org/superbiz/cdi/requestscope/RestaurantTest.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.superbiz.cdi.requestscope;
+
+import javax.ejb.EJB;
+import javax.ejb.embeddable.EJBContainer;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestaurantTest {
+	
+	private static final String TOMATO_SOUP = "Tomato Soup";
+	private static final String POTATO_SOUP = "Potato Soup";
+	private EJBContainer container;
+	
+	@EJB
+	private Waiter joe;
+	
+	@Before
+    public void startContainer() throws Exception {
+        container = EJBContainer.createEJBContainer();
+        container.getContext().bind("inject", this);
+    }
+	
+	@Test
+	public void orderSoup(){
+		String soup = joe.orderSoup(TOMATO_SOUP);
+		assertEquals(TOMATO_SOUP, soup);
+		soup = joe.orderSoup(POTATO_SOUP);
+		assertEquals(POTATO_SOUP, soup);
+	}
+	
+	@After
+	public void closeContainer() throws Exception {
+		container.close();
+	}
+}
