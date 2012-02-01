72fa07a08aa5897b9dceb5fecbe1cca678633f22
Romain Manni-Bucau <rmannibucau@apache.org>
2012-02-01 23:36:33 +0000

    trying to dump descriptors then use them instead of scanning - to enhance a lot
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1239405 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/OutputGeneratedDescriptors.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/OutputGeneratedDescriptors.java
@@ -173,18 +173,28 @@ public class OutputGeneratedDescriptors implements DynamicDeployer {
 
     private void writeEjbJar(EjbModule ejbModule) {
         try {
+            final File tempFile = tempFile("ejb-jar-", ejbModule.getModuleId() + ".xml");
+            writeEjbJar(tempFile, ejbModule);
+            logger.info("Dumping Generated ejb-jar.xml to: " + tempFile.getAbsolutePath());
+        } catch (IOException e) {
+            // no-op
+        }
+    }
+
+    public static void writeEjbJar(final File output, final EjbModule ejbModule) {
+        try {
             EjbJar ejbJar = ejbModule.getEjbJar();
-            File tempFile = tempFile("ejb-jar-", ejbModule.getModuleId() + ".xml");
-            FileOutputStream fout = new FileOutputStream(tempFile);
+            FileOutputStream fout = new FileOutputStream(output);
             BufferedOutputStream out = new BufferedOutputStream(fout);
             try {
                 JaxbJavaee.marshal(EjbJar.class, ejbJar, out);
-                logger.info("Dumping Generated ejb-jar.xml to: " + tempFile.getAbsolutePath());
-            } catch (JAXBException e) {
+            } catch (JAXBException ignored) {
+                // no-op
             } finally {
                 out.close();
             }
         } catch (Exception e) {
+            // no-op
         }
     }
 }
