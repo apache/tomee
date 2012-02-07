a8fea1d1f786ba6a243ab19caa71c8e55e0c2b9a
Romain Manni-Bucau <rmannibucau@apache.org>
2012-02-07 21:46:26 +0000

    making the StatefulStatefulTimeoutTest working
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1241647 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/core/stateful/StatefulStatefulTimeoutTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/core/stateful/StatefulStatefulTimeoutTest.java
@@ -3,15 +3,17 @@ package org.apache.openejb.core.stateful;
 import org.apache.openejb.jee.EnterpriseBean;
 import org.apache.openejb.jee.StatefulBean;
 import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.junit.Configuration;
 import org.apache.openejb.junit.Module;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.ejb.NoSuchEJBException;
 import javax.ejb.Stateful;
 import javax.ejb.StatefulTimeout;
 import javax.naming.Context;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertNotNull;
@@ -32,22 +34,29 @@ public class StatefulStatefulTimeoutTest {
         public void foo() {}
     }
 
+    @Configuration
+    public Properties properties() {
+        final Properties properties = new Properties();
+        properties.setProperty("Default Stateful Container.Frequency", "1seconds");
+        return properties;
+    }
+
     @Module
     public EnterpriseBean stateful() {
         return new StatefulBean("TimedOutStateful", TimedOutStateful.class);
     }
 
-    @Test @Ignore
+    @Test
     public void checkBeanIsCleaned() throws Exception {
         assertNotNull(context);
         TimedOutStateful stateful = (TimedOutStateful) context.lookup("global/StatefulStatefulTimeoutTest/stateful/TimedOutStateful");
         stateful.foo();
-        Thread.sleep(8000);
+        Thread.sleep(6000);
         try {
             stateful.foo();
             fail();
-        } catch (Exception e) {
-            System.out.println("got " + e.getClass());
+        } catch (NoSuchEJBException e) {
+            // ok
         }
     }
 }
