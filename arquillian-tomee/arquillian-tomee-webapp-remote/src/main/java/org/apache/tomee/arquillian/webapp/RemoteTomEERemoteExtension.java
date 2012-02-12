cf20f929076b1a47c9c146543bdb3fd763ba2bb7
David Blevins <dblevins@apache.org>
2012-02-12 18:49:06 +0000

    Repackaged code in arquillian-tomee-webapp-remote Added arquillian-tomee-webapp-remote to the build TOMEE-127, TOME-136
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1243295 13f79535-47bb-0310-9956-ffa450edef68

--- arquillian-tomee/arquillian-tomee-webapp-remote/src/main/java/org/apache/tomee/arquillian/webapp/RemoteTomEERemoteExtension.java
+++ arquillian-tomee/arquillian-tomee-webapp-remote/src/main/java/org/apache/tomee/arquillian/webapp/RemoteTomEERemoteExtension.java
@@ -15,18 +15,12 @@
  *  limitations under the License.
  */
 
-package org.apache.openejb.arquillian.remote;
+package org.apache.tomee.arquillian.webapp;
 
 import org.jboss.arquillian.container.test.spi.RemoteLoadableExtension;
-import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
-public class RemoteTomEEEJBEnricherArchiveAppender implements AuxiliaryArchiveAppender {
-    @Override public Archive<?> createAuxiliaryArchive() {
-        return ShrinkWrap.create(JavaArchive.class, "arquillian-tomee-testenricher-ejb.jar")
-                   .addClasses(RemoteTomEEObserver.class, RemoteTomEERemoteExtension.class)
-                   .addAsServiceProvider(RemoteLoadableExtension.class, RemoteTomEERemoteExtension.class);
+public class RemoteTomEERemoteExtension implements RemoteLoadableExtension {
+    @Override public void register(ExtensionBuilder builder) {
+       builder.observer(RemoteTomEEObserver.class);
     }
 }
