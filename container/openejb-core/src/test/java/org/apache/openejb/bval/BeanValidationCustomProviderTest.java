c74bf1a2f31c027533654526349e82d31974e64c
Romain Manni-Bucau <rmannibucau@apache.org>
2012-02-14 13:46:59 +0000

    OPENEJB-1773 using a custom valition provider to be able to switch easily at runtime of provider
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1243918 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/bval/BeanValidationCustomProviderTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/bval/BeanValidationCustomProviderTest.java
@@ -16,7 +16,9 @@
  */
 package org.apache.openejb.bval;
 
+import org.apache.bval.jsr303.ApacheValidationProvider;
 import org.apache.openejb.assembler.classic.ValidatorBuilder;
+import org.apache.openejb.bval.util.CustomValidatorProvider;
 import org.apache.openejb.jee.Empty;
 import org.apache.openejb.jee.StatelessBean;
 import org.apache.openejb.junit.ApplicationComposer;
@@ -31,19 +33,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.validation.ConstraintValidatorFactory;
-import javax.validation.ConstraintViolation;
-import javax.validation.MessageInterpolator;
-import javax.validation.TraversableResolver;
 import javax.validation.Validator;
-import javax.validation.ValidatorContext;
-import javax.validation.ValidatorFactory;
-import javax.validation.metadata.BeanDescriptor;
-import javax.validation.spi.BootstrapState;
-import javax.validation.spi.ConfigurationState;
-import javax.validation.spi.ValidationProvider;
-import java.io.InputStream;
-import java.util.Set;
 
 import static org.junit.Assert.assertTrue;
 
@@ -54,11 +44,13 @@ public class BeanValidationCustomProviderTest {
 
     @BeforeClass
     public static void initProvider() {
+        CustomValidatorProvider.provider = new CustomValidatorProvider.NullValidatorProvider();
         System.setProperty(ValidatorBuilder.VALIDATION_PROVIDER_KEY, CustomValidatorProvider.class.getName());
     }
 
     @AfterClass
     public static void resetProvider() {
+        CustomValidatorProvider.provider = new ApacheValidationProvider();
         System.clearProperty(ValidatorBuilder.VALIDATION_PROVIDER_KEY);
     }
 
@@ -82,156 +74,6 @@ public class BeanValidationCustomProviderTest {
 
     @Test
     public void valid() {
-        assertTrue(bean.getValidator() instanceof CustomValidator);
-    }
-
-    public static class CustomValidatorProvider implements ValidationProvider<NullConfig> {
-        @Override
-        public NullConfig createSpecializedConfiguration(BootstrapState state) {
-            return new NullConfig();
-        }
-
-        @Override
-        public javax.validation.Configuration<?> createGenericConfiguration(BootstrapState state) {
-            return new NullConfig();
-        }
-
-        @Override
-        public ValidatorFactory buildValidatorFactory(ConfigurationState configurationState) {
-            return null;
-        }
-    }
-
-    private static class NullConfig implements javax.validation.Configuration<NullConfig> {
-        @Override
-        public NullConfig ignoreXmlConfiguration() {
-            return null;
-        }
-
-        @Override
-        public NullConfig messageInterpolator(MessageInterpolator interpolator) {
-            return null;
-        }
-
-        @Override
-        public NullConfig traversableResolver(TraversableResolver resolver) {
-            return null;
-        }
-
-        @Override
-        public NullConfig constraintValidatorFactory(ConstraintValidatorFactory constraintValidatorFactory) {
-            return null;
-        }
-
-        @Override
-        public NullConfig addMapping(InputStream stream) {
-            return null;
-        }
-
-        @Override
-        public NullConfig addProperty(String name, String value) {
-            return null;
-        }
-
-        @Override
-        public MessageInterpolator getDefaultMessageInterpolator() {
-            return null;
-        }
-
-        @Override
-        public TraversableResolver getDefaultTraversableResolver() {
-            return null;
-        }
-
-        @Override
-        public ConstraintValidatorFactory getDefaultConstraintValidatorFactory() {
-            return null;
-        }
-
-        @Override
-        public ValidatorFactory buildValidatorFactory() {
-            return new CustomValidatorFactory();
-        }
-    }
-
-    private static class CustomValidatorFactory implements ValidatorFactory {
-        @Override
-        public Validator getValidator() {
-            return new CustomValidator();
-        }
-
-        @Override
-        public ValidatorContext usingContext() {
-            return new CustomValidatorContext();
-        }
-
-        @Override
-        public MessageInterpolator getMessageInterpolator() {
-            return null;
-        }
-
-        @Override
-        public TraversableResolver getTraversableResolver() {
-            return null;
-        }
-
-        @Override
-        public ConstraintValidatorFactory getConstraintValidatorFactory() {
-            return null;
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type) {
-            return null;
-        }
-    }
-
-    private static class CustomValidatorContext implements ValidatorContext {
-        @Override
-        public ValidatorContext messageInterpolator(MessageInterpolator messageInterpolator) {
-            return null;
-        }
-
-        @Override
-        public ValidatorContext traversableResolver(TraversableResolver traversableResolver) {
-            return null;
-        }
-
-        @Override
-        public ValidatorContext constraintValidatorFactory(ConstraintValidatorFactory factory) {
-            return null;
-        }
-
-        @Override
-        public Validator getValidator() {
-            return new CustomValidator();
-        }
-    }
-
-    private static class CustomValidator implements Validator {
-        @Override
-        public <T> Set<ConstraintViolation<T>> validate(T object, Class<?>... groups) {
-            return null;
-        }
-
-        @Override
-        public <T> Set<ConstraintViolation<T>> validateProperty(T object, String propertyName, Class<?>... groups) {
-            return null;
-        }
-
-        @Override
-        public <T> Set<ConstraintViolation<T>> validateValue(Class<T> beanType, String propertyName, Object value, Class<?>... groups) {
-            return null;
-        }
-
-        @Override
-        public BeanDescriptor getConstraintsForClass(Class<?> clazz) {
-            return null;
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type) {
-            return null;
-        }
+        assertTrue(bean.getValidator() instanceof CustomValidatorProvider.CustomValidator);
     }
 }
