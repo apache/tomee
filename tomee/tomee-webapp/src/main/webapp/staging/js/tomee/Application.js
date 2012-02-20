b8967db1730a33b5117d6f44ff0211632c687026
Thiago Veronezi <tveronezi@apache.org>
2012-02-20 11:39:18 +0000

    https://issues.apache.org/jira/browse/TOMEE-130
    Creating a carousel to show jndi details.
    
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1291213 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/tomee-webapp/src/main/webapp/staging/js/tomee/Application.js
+++ tomee/tomee-webapp/src/main/webapp/staging/js/tomee/Application.js
@@ -60,5 +60,15 @@ TOMEE.utils = {
             return {};
         }
         return obj;
+    },
+
+    stringFormat: function() {
+        var s = arguments[0];
+        for (var i = 0; i < arguments.length - 1; i++) {
+            var reg = new RegExp("\\{" + i + "\\}", "gm");
+            s = s.replace(reg, arguments[i + 1]);
+        }
+
+        return s;
     }
 };
