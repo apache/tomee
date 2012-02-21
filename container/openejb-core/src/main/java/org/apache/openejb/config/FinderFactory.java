71c86fee477ce46b0003c14c727262e322e15fdd
Romain Manni-Bucau <rmannibucau@apache.org>
2012-02-21 14:06:18 +0000

    using xbean-xml (in maven modules for the moment))
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1291791 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/FinderFactory.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/FinderFactory.java
@@ -16,6 +16,7 @@
  */
 package org.apache.openejb.config;
 
+import org.apache.openejb.xbean.xml.XMLAnnotationFinderHelper;
 import org.apache.xbean.finder.AnnotationFinder;
 import org.apache.xbean.finder.IAnnotationFinder;
 import org.apache.openejb.util.Logger;
@@ -24,12 +25,19 @@ import org.apache.openejb.loader.SystemInstance;
 import org.apache.xbean.finder.archive.ClassesArchive;
 import org.apache.xbean.finder.archive.ClasspathArchive;
 
+import javax.xml.bind.JAXBException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.URL;
 
 public class FinderFactory {
 
     private static final Logger logger = Logger.getInstance(LogCategory.OPENEJB, FinderFactory.class);
+
+    private static final String SCAN_XML = "META-INF/org/apache/xbean/scan.xml";
+    private static final String WEB_SCAN_XML = SCAN_XML.replace("META-INF", "WEB-INF");
 
     private static final FinderFactory factory = new FinderFactory();
 
@@ -46,12 +54,20 @@ public class FinderFactory {
         if (module instanceof WebModule) {
             WebModule webModule = (WebModule) module;
             final ClassLoader webClassLoader = webModule.getClassLoader();
+            final IAnnotationFinder finder = xmlFinder(module, inputStream(webModule.getFile(), WEB_SCAN_XML));
+            if (finder != null) {
+                return finder;
+            }
             return new AnnotationFinder(new AggregatedArchive(webClassLoader, webModule.getScannableUrls())).link();
         }
         
         if (module instanceof ConnectorModule) {
         	ConnectorModule connectorModule = (ConnectorModule) module;
         	final ClassLoader connectorClassLoader = connectorModule.getClassLoader();
+            final IAnnotationFinder finder = xmlFinder(module, connectorClassLoader.getResourceAsStream(SCAN_XML));
+            if (finder != null) {
+                return finder;
+            }
         	return new AnnotationFinder(new ClasspathArchive(connectorClassLoader, connectorModule.getLibraries())).link();
         }
 
@@ -67,13 +83,47 @@ public class FinderFactory {
 				if (webInfClassesFolder.exists() && webInfClassesFolder.isDirectory()) {
                 	url = webInfClassesFolder.toURI().toURL();
                 }
+                if (webInfClassesFolder.getParentFile().exists()) {
+                    final FileInputStream fis = inputStream(webInfClassesFolder.getParentFile().getParentFile(), WEB_SCAN_XML);
+                    final IAnnotationFinder finder = xmlFinder(module, fis);
+                    if (finder != null) {
+                        return finder;
+                    }
+                }
             } else {
                 url = new URL(location);
             }
+
+            final IAnnotationFinder finder = xmlFinder(module, module.getClassLoader().getResourceAsStream(SCAN_XML));
+            if (finder != null) {
+                return finder;
+            }
+
             return new AnnotationFinder(new ClasspathArchive(module.getClassLoader(), url)).link();
         } else {
             return new AnnotationFinder(new ClassesArchive()).link();
         }
+    }
+
+    private static IAnnotationFinder xmlFinder(final DeploymentModule module, final InputStream scanIs) {
+        if (scanIs != null) {
+            try {
+                final IAnnotationFinder finder = XMLAnnotationFinderHelper.finderFromXml(scanIs, module.getClassLoader());
+                logger.info("using scan.xml for module " + module.getModuleId());
+                return finder;
+            } catch (JAXBException jaxbEx) {
+                logger.warning("can't use scan.xml for " + module.getModuleId());
+            }
+        }
+        return null;
+    }
+
+    private static FileInputStream inputStream(final File file, final String xml) throws FileNotFoundException {
+        final File scanFile = new File(file, xml);
+        if (scanFile.exists()) {
+            return new FileInputStream(scanFile);
+        }
+        return null;
     }
 
 }
