a4b57bd40381875ba8653ba72f7b0e4e5a0e68e7
Romain Manni-Bucau <rmannibucau@apache.org>
2012-03-09 18:41:56 +0000

    minor reformatting about PollinRouter
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1298972 13f79535-47bb-0310-9956-ffa450edef68

--- examples/polling-parent/polling-web/src/main/java/jug/routing/PollingRouter.java
+++ examples/polling-parent/polling-web/src/main/java/jug/routing/PollingRouter.java
@@ -10,38 +10,16 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 public class PollingRouter extends AbstractRouter {
-    private static final Logger LOGGER = Logger.getLogger(PollingRouter.class.getName());
-
     private Map<String, DataSource> dataSources = null;
     private ThreadLocal<DataSource> currentDataSource = new ThreadLocal<DataSource>() {
         @Override
-        public DataSource initialValue() {
-            return dataSources.get("jdbc/client1");
-        }
+        public DataSource initialValue() { return dataSources.get("jdbc/client1"); }
     };
-
-    private void init() {
-        dataSources = new HashMap<String, DataSource>();
-        for (String ds : Arrays.asList("jdbc/client1", "jdbc/client2")) {
-            try {
-                final Object o = getOpenEJBResource(ds);
-                if (o instanceof DataSource) {
-                    dataSources.put(ds, DataSource.class.cast(o));
-                }
-            } catch (NamingException e) {
-                // ignored
-            }
-        }
-    }
 
     @Override
     public DataSource getDataSource() {
-        if (dataSources == null) {
-            init();
-        }
-        final DataSource ds = currentDataSource.get();
-        LOGGER.info("using ds " + ds);
-        return ds;
+        if (dataSources == null) { init(); }
+        return currentDataSource.get();
     }
 
     public void setDataSource(final String client) {
@@ -55,6 +33,20 @@ public class PollingRouter extends AbstractRouter {
         }
         final DataSource ds = dataSources.get(datasourceName);
         currentDataSource.set(ds);
+    }
+
+    private void init() {
+        dataSources = new HashMap<String, DataSource>();
+        for (String ds : Arrays.asList("jdbc/client1", "jdbc/client2")) {
+            try {
+                final Object o = getOpenEJBResource(ds);
+                if (o instanceof DataSource) {
+                    dataSources.put(ds, DataSource.class.cast(o));
+                }
+            } catch (NamingException e) {
+                // ignored
+            }
+        }
     }
 
     public void clear() {
