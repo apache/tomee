4033688f53918de070ff927e21f23e68b55f9158
Romain Manni-Bucau <rmannibucau@apache.org>
2012-04-11 08:14:45 +0000

    bunch of missing java header
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1324621 13f79535-47bb-0310-9956-ffa450edef68

--- examples/polling-parent/polling-web/src/main/java/jug/rest/SubjectService.java
+++ examples/polling-parent/polling-web/src/main/java/jug/rest/SubjectService.java
@@ -1,15 +1,24 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package jug.rest;
 
-import jug.dao.SubjectDao;
-import jug.dao.VoteDao;
-import jug.domain.Result;
-import jug.domain.Subject;
-import jug.domain.Value;
-import jug.domain.Vote;
-import jug.monitoring.VoteCounter;
-
+import java.util.Collection;
+import java.util.List;
 import javax.annotation.Resource;
-import javax.ejb.EJB;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
@@ -23,8 +32,13 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Collection;
-import java.util.List;
+import jug.dao.SubjectDao;
+import jug.dao.VoteDao;
+import jug.domain.Result;
+import jug.domain.Subject;
+import jug.domain.Value;
+import jug.domain.Vote;
+import jug.monitoring.VoteCounter;
 
 @Path("subject")
 @Singleton // an ejb just to be able to test in standalone
