674fa559fa686dbc4f40fc44dfb8ff4be78cf16b
David Blevins <dblevins@apache.org>
2012-04-25 13:09:40 +0000

    TOMEE-169 - cache and share results of scanning tld files TOMEE-164 - use default.excludes when scanning for tld files TOMEE-168 - Load OpenEJB System applications directly, without scanning TOMEE-167 - Disable support for legacy v2 openejb-jar.xml by default. Only needed for Geronimo TOMEE-166 - Web.xml metadata-complete effectively ignored
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1330257 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentLoader.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentLoader.java
@@ -51,6 +51,7 @@ import org.apache.xbean.finder.IAnnotationFinder;
 import org.apache.xbean.finder.ResourceFinder;
 import org.apache.xbean.finder.UrlSet;
 import org.apache.xbean.finder.archive.Archive;
+import org.apache.xbean.finder.archive.ClassesArchive;
 import org.apache.xbean.finder.archive.JarArchive;
 import org.xml.sax.SAXException;
 
@@ -71,7 +72,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -83,6 +83,8 @@ import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import static org.apache.openejb.config.NewLoaderLogic.applyBuiltinExcludes;
+import static org.apache.openejb.config.TldScanner.scanForTagLibs;
+import static org.apache.openejb.config.TldScanner.scanWarForTagLibs;
 import static org.apache.openejb.util.URLs.toFile;
 
 /**
@@ -678,14 +680,56 @@ public class DeploymentLoader implements DeploymentFilterable {
 
         try {
             // TODO:  Put our scanning ehnancements back, here
-            final IAnnotationFinder finder = FinderFactory.createFinder(webModule);
-            webModule.setFinder(finder);
-            webEjbModule.setFinder(finder);
+            fillEjbJar(webModule, webEjbModule);
+
+            if (isMetadataComplete(webModule, webEjbModule)) {
+                final IAnnotationFinder finder = new org.apache.xbean.finder.AnnotationFinder(new ClassesArchive());
+                webModule.setFinder(finder);
+                webEjbModule.setFinder(finder);
+            }  else {
+                final IAnnotationFinder finder = FinderFactory.createFinder(webModule);
+                webModule.setFinder(finder);
+                webEjbModule.setFinder(finder);
+            }
         } catch (Exception e) {
             throw new OpenEJBException("Unable to create annotation scanner for web module " + webModule.getModuleId(), e);
         }
 
         addWebservices(webEjbModule);
+    }
+
+    /**
+     * If the web.xml is metadata-complete and there is no ejb-jar.xml
+     * then per specification we use the web.xml metadata-complete setting
+     * to imply the same for EJBs.
+     *
+     * @param webModule
+     * @param ejbModule
+     */
+    private void fillEjbJar(WebModule webModule, EjbModule ejbModule) {
+        final Object o = webModule.getAltDDs().get("ejb-jar.xml");
+        if (o != null) return;
+        if (ejbModule.getEjbJar() != null) return;
+
+        final EjbJar ejbJar = new EjbJar();
+        final WebApp webApp = webModule.getWebApp();
+
+        ejbJar.setMetadataComplete(webApp.isMetadataComplete());
+
+        ejbModule.setEjbJar(ejbJar);
+    }
+
+    private boolean isMetadataComplete(WebModule webModule, EjbModule ejbModule) {
+        if (webModule.getWebApp() == null) return false;
+        if (!webModule.getWebApp().isMetadataComplete()) return false;
+
+        // At this point we know the web.xml is metadata-complete
+        // We need to determine if there are cdi or ejb xml files
+        if (webModule.getAltDDs().get("beans.xml") == null) return true;
+        if (ejbModule.getEjbJar() == null) return true;
+        if (!ejbModule.getEjbJar().isMetadataComplete()) return false;
+
+        return true;
     }
 
     public WebModule createWebModule(final String appId, final String warPath, final ClassLoader parentClassLoader, final String contextRoot, final String moduleName) throws OpenEJBException {
@@ -959,7 +1003,7 @@ public class DeploymentLoader implements DeploymentFilterable {
 
         // Search all libs
         final ClassLoader parentClassLoader = webModule.getClassLoader().getParent();
-        urls = scanClassLoaderForTagLibs(parentClassLoader);
+        urls = TldScanner.scan(parentClassLoader);
         tldLocations.addAll(urls);
 
         // load the tld files
@@ -1042,144 +1086,6 @@ public class DeploymentLoader implements DeploymentFilterable {
                 webModule.getWatchedResources().add(URLs.toFilePath(location));
             }
         }
-    }
-
-    private static Set<URL> scanClassLoaderForTagLibs(final ClassLoader parentClassLoader) throws OpenEJBException {
-        final Set<URL> urls = new HashSet<URL>();
-        if (parentClassLoader == null) return urls;
-
-        UrlSet urlSet;
-        try {
-            urlSet = new UrlSet(parentClassLoader);
-            urlSet = URLs.cullSystemJars(urlSet);
-        } catch (IOException e) {
-            logger.warning("Error scanning class loader for JSP tag libraries", e);
-            return urls;
-        }
-
-        for (URL url : urlSet.getUrls()) {
-            if (url.getProtocol().equals("jar")) {
-                try {
-                    String path = url.getPath();
-                    if (path.endsWith("!/")) {
-                        path = path.substring(0, path.length() - 2);
-                    }
-                    url = new URL(path);
-                } catch (MalformedURLException e) {
-                    logger.warning("JSP tag library location bad: " + url.toExternalForm(), e);
-                    continue;
-                }
-            }
-
-            if (!url.getProtocol().equals("file")) {
-                continue;
-            }
-
-            File file = toFile(url);
-            try {
-                file = file.getCanonicalFile().getAbsoluteFile();
-            } catch (IOException e) {
-                logger.warning("JSP tag library location bad: " + file.getAbsolutePath(), e);
-                continue;
-            }
-
-            urls.addAll(scanForTagLibs(file));
-        }
-        return urls;
-    }
-
-    private static Set<URL> scanWarForTagLibs(final File war) {
-        final Set<URL> urls = new HashSet<URL>();
-
-        final File webInfDir = new File(war, "WEB-INF");
-        if (!webInfDir.isDirectory()) return urls;
-
-
-        // skip the lib and classes dir in WEB-INF
-        final LinkedList<File> files = new LinkedList<File>();
-        final File[] list = webInfDir.listFiles();
-        if (list != null) {
-            for (final File file : list) {
-                if ("lib".equals(file.getName()) || "classes".equals(file.getName())) {
-                    continue;
-                }
-                files.add(file);
-            }
-        }
-
-        if (files.isEmpty()) return urls;
-
-        // recursively scan the directories
-        while (!files.isEmpty()) {
-            File file = files.removeFirst();
-            if (file.isDirectory()) {
-                final File[] a = file.listFiles();
-                if (a != null) {
-                    files.addAll(Arrays.asList(a));
-                }
-            } else if (file.getName().endsWith(".tld")) {
-                try {
-                    file = file.getCanonicalFile().getAbsoluteFile();
-                    urls.add(file.toURI().toURL());
-                } catch (IOException e) {
-                    logger.warning("JSP tag library location bad: " + file.getAbsolutePath(), e);
-                }
-            }
-        }
-
-        return urls;
-    }
-
-    private static Set<URL> scanForTagLibs(final File file) {
-        final Set<URL> tldLocations = new HashSet<URL>();
-        try {
-            final String location = file.toURI().toURL().toExternalForm();
-
-            if (location.endsWith(".jar")) {
-                final Set<URL> urls = scanJarForTagLibs(file);
-                tldLocations.addAll(urls);
-            } else if (file.getName().endsWith(".tld")) {
-                final URL url = file.toURI().toURL();
-                tldLocations.add(url);
-            }
-        } catch (IOException e) {
-            logger.warning("Error scanning for JSP tag libraries: " + file.getAbsolutePath(), e);
-        }
-
-        return tldLocations;
-    }
-
-    private static Set<URL> scanJarForTagLibs(final File file) {
-        final Set<URL> urls = new HashSet<URL>();
-
-        if (!file.isFile()) return urls;
-
-        JarFile jarFile = null;
-        try {
-            jarFile = new JarFile(file);
-
-            final URL jarFileUrl = new URL("jar", "", -1, file.toURI().toURL().toExternalForm() + "!/");
-            for (final JarEntry entry : Collections.list(jarFile.entries())) {
-                final String name = entry.getName();
-                if (!name.startsWith("META-INF/") || !name.endsWith(".tld")) {
-                    continue;
-                }
-                final URL url = new URL(jarFileUrl, name);
-                urls.add(url);
-            }
-        } catch (IOException e) {
-            logger.warning("Error scanning jar for JSP tag libraries: " + file.getAbsolutePath(), e);
-        } finally {
-            if (jarFile != null) {
-                try {
-                    jarFile.close();
-                } catch (IOException e) {
-                    // exception ignored
-                }
-            }
-        }
-
-        return urls;
     }
 
     protected static ConnectorModule createConnectorModule(final String appId, final String rarPath, final ClassLoader parentClassLoader, final String moduleId) throws OpenEJBException {
