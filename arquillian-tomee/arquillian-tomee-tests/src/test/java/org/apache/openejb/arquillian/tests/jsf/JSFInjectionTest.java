f99022ac2f68a0f15209e2c4ca53be2ffe01001c
Romain Manni-Bucau <rmannibucau@apache.org>
2012-05-03 07:29:24 +0000

    adding a test checking ejb injections are done in jsf
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1333331 13f79535-47bb-0310-9956-ffa450edef68

---
+++ arquillian-tomee/arquillian-tomee-tests/src/test/java/org/apache/openejb/arquillian/tests/jsf/JSFInjectionTest.java
@@ -0,0 +1,78 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.arquillian.tests.jsf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.ziplock.JarLocation;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.spec.servlet.web.WebAppDescriptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(Arquillian.class)
+public class JSFInjectionTest {
+    @ArquillianResource
+    private URL url;
+
+    @Deployment
+    public static WebArchive getArchive() {
+        final WebAppDescriptor descriptor = Descriptors.create(WebAppDescriptor.class)
+                .version("3.0");
+
+        return ShrinkWrap.create(WebArchive.class, "jsf-injection-test.war")
+                .addAsWebResource(new ClassLoaderAsset(
+                        JSFInjectionTest.class.getPackage().getName().replace('.', '/').concat("/").concat("dummy.xhtml")), "dummy.xhtml")
+                .addAsLibraries(JarLocation.jarLocation(Test.class)) // junit
+                .setWebXML(new StringAsset(descriptor.exportAsString()));
+    }
+
+    @Test
+    public void testProjectStage() throws Exception {
+        validateTest("foo");
+    }
+
+    private void validateTest(final String expectedOutput) throws IOException {
+        final InputStream is = new URL(url.toExternalForm() + "dummy.xhtml").openStream();
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        int bytesRead;
+        byte[] buffer = new byte[8192];
+        while ((bytesRead = is.read(buffer)) > -1) {
+            os.write(buffer, 0, bytesRead);
+        }
+
+        is.close();
+        os.close();
+
+        String output = new String(os.toByteArray(), "UTF-8");
+        assertNotNull("Response shouldn't be null", output);
+        assertTrue("Output should contain: " + expectedOutput, output.contains(expectedOutput));
+    }
+}
