19aa2bfe3765ce7ccfd1683b78d189943686dcdb
Romain Manni-Bucau <rmannibucau@apache.org>
2012-06-04 20:10:23 +0000

    OPENEJB-1836 specialize warning message for @Local on local bean
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1346137 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/AnnotationDeployer.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/AnnotationDeployer.java
@@ -2799,7 +2799,7 @@ public class AnnotationDeployer implements DynamicDeployer {
                         // just warn for @Local since Glassfish supports it even if it is weird
                         // still fail for @Remote!
                         if (impliedLocal && local.value().length == 0 && interfaces.size() == 0 && !strict) {
-                            validation.warn(ejbName, "ann.local.noAttributes", join(", ", interfaceNames));
+                            validation.warn(ejbName, "ann.local.forLocalBean", join(", ", interfaceNames));
                             // we don't go out to let be deployed
                         } else if (impliedLocal) {
                             validation.fail(ejbName, "ann.local.noAttributes", join(", ", interfaceNames));
