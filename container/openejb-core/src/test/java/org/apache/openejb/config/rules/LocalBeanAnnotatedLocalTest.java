137162f6ddcd1c4bb4dead0cce61e343524fb801
Romain Manni-Bucau <rmannibucau@apache.org>
2012-06-05 18:48:39 +0000

    moving warning error test in good package (@local for local bean)
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1346524 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/rules/LocalBeanAnnotatedLocalTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/rules/LocalBeanAnnotatedLocalTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.openejb.core.stateless;
+package org.apache.openejb.config.rules;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
