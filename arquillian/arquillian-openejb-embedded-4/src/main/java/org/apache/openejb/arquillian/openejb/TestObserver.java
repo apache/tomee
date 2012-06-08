af91e4c0797555d5d859f40e8166d825e18e3018
Romain Manni-Bucau <rmannibucau@apache.org>
2012-06-08 15:10:15 +0000

    using the test bean context as wrapper for the test method execution even in arquillian embedded adapter
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1348102 13f79535-47bb-0310-9956-ffa450edef68

--- arquillian/arquillian-openejb-embedded-4/src/main/java/org/apache/openejb/arquillian/openejb/TestObserver.java
+++ arquillian/arquillian-openejb-embedded-4/src/main/java/org/apache/openejb/arquillian/openejb/TestObserver.java
@@ -1,5 +1,9 @@
 package org.apache.openejb.arquillian.openejb;
 
+import org.apache.openejb.BeanContext;
+import org.apache.openejb.core.ThreadContext;
+import org.apache.openejb.loader.SystemInstance;
+import org.apache.openejb.spi.ContainerSystem;
 import org.jboss.arquillian.core.api.Instance;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.api.annotation.Observes;
@@ -13,12 +17,26 @@ public class TestObserver {
     private Instance<ClassLoader> classLoader;
 
     public void observe(@Observes EventContext<Test> event) {
-        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        setTCCL(classLoader.get());
+        final BeanContext context = SystemInstance.get().getComponent(ContainerSystem.class)
+                                        .getBeanContext(event.getEvent().getTestClass().getJavaClass().getName());
+        ThreadContext oldCtx = null;
+        ClassLoader oldCl = null;
+
+        if (context != null) {
+            oldCtx = ThreadContext.enter(new ThreadContext(context, null));
+        } else {
+            oldCl = Thread.currentThread().getContextClassLoader();
+            setTCCL(classLoader.get());
+        }
+
         try {
             event.proceed();
         } finally {
-            setTCCL(cl);
+            if (context != null) {
+                ThreadContext.exit(oldCtx);
+            } else {
+                setTCCL(oldCl);
+            }
         }
     }
 
