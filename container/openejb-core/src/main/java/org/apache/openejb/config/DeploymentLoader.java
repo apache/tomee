d0439df3203ccb58addacacf935971b9720990a0
Romain Manni-Bucau <rmannibucau@apache.org>
2012-06-28 20:37:18 +0000

    better filtering of unknown module
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1355151 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentLoader.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentLoader.java
@@ -1459,7 +1459,7 @@ public class DeploymentLoader implements DeploymentFilterable {
         final Class<? extends DeploymentModule> defaultType = (Class<? extends DeploymentModule>) SystemInstance.get().getOptions().get("openejb.default.deployment-module", (Class<?>) null);
         if (defaultType != null) {
             // should we do a better filtering? it seems enough for common cases.
-            if (WebModule.class.equals(defaultType) && path.endsWith(".jar")) {
+            if (WebModule.class.equals(defaultType) && (path.endsWith(".jar!") || path.endsWith(".jar"))) {
                 throw new UnknownModuleTypeException("Unknown module type: url=" + path + " which can't be a war.");
             }
 
