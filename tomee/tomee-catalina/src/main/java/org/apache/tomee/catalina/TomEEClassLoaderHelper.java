4d70806870b9c2c2f40e73aeb42a31be27e000d3
Romain Manni-Bucau <rmannibucau@apache.org>
2012-07-01 22:39:33 +0000

    TOMEE-264 adding hooks to filter spec jar from webapp. This commit filter JTA and JPA.
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1356039 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/TomEEClassLoaderHelper.java
+++ tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/TomEEClassLoaderHelper.java
@@ -19,22 +19,37 @@ package org.apache.tomee.catalina;
 import org.apache.openejb.OpenEJB;
 import org.apache.openejb.loader.JarLocation;
 import org.apache.openejb.loader.SystemInstance;
+import org.apache.openejb.util.LogCategory;
+import org.apache.openejb.util.Logger;
 import org.apache.tomee.installer.Paths;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public final class TomEEClassLoaderHelper {
+    private static final Logger LOGGER = Logger.getInstance(LogCategory.OPENEJB, TomEEClassLoaderHelper.class);
+
+    private TomEEClassLoaderHelper() {
+        // no-op
+    }
+
+    /**
+     * Enrichement part
+     */
+
     public static final String TOMEE_WEBAPP_CLASSLOADER_ENRICHMENT_SKIP = "tomee.webapp.classloader.enrichment.skip";
 
     public static final String TOMEE_WEBAPP_CLASSLOADER_ENRICHMENT_CLASSES = "tomee.webapp.classloader.enrichment.classes";
     public static final String TOMEE_WEBAPP_CLASSLOADER_ENRICHMENT_PREFIXES = "tomee.webapp.classloader.enrichment.prefixes";
 
-    private static final String[] DEFAULT_JAR_TO_ADD_CLASS_HELPERS = new String[] {
+    private static final String[] DEFAULT_JAR_TO_ADD_CLASS_HELPERS = new String[]{
             // openejb-jsf and openwebbeans-jsf to be able to embedded the jsf impl keeping CDI features
             "org.apache.openejb.jsf.CustomApplicationFactory",
             "org.apache.webbeans.jsf.OwbApplicationFactory",
@@ -44,7 +59,7 @@ public final class TomEEClassLoaderHelper {
     };
     private static final String[] JAR_TO_ADD_CLASS_HELPERS;
 
-    private static final String[] DEFAULT_PREFIXES_TO_ADD = new String[] {
+    private static final String[] DEFAULT_PREFIXES_TO_ADD = new String[]{
             "tomee-mojarra"
     };
     private static final String[] PREFIXES_TO_ADD;
@@ -71,10 +86,6 @@ public final class TomEEClassLoaderHelper {
         }
         JAR_TO_ADD_CLASS_HELPERS = classes.toArray(new String[classes.size()]);
         PREFIXES_TO_ADD = prefixes.toArray(new String[prefixes.size()]);
-    }
-
-    private TomEEClassLoaderHelper() {
-        // no-op
     }
 
     public static URL[] tomEEWebappIntegrationLibraries() {
@@ -114,5 +125,47 @@ public final class TomEEClassLoaderHelper {
         }
 
         return urls.toArray(new URL[urls.size()]);
+    }
+
+    /**
+     * Validation part
+     */
+    private static final String[] FORBIDDEN_CLASSES = new String[]{
+            "javax.persistence.Entity", // JPA
+            "javax.transaction.Transaction" // JTA
+    };
+
+    public static boolean validateJarFile(final File file) throws IOException {
+        final ClassLoader parent = TomEEClassLoaderHelper.class.getClassLoader();
+
+        JarFile jarFile = null;
+
+        try {
+            jarFile = new JarFile(file);
+            for (String name : FORBIDDEN_CLASSES) {
+                try { // if we can't load if from our classloader we'll not impose anything on this class
+                    parent.loadClass(name);
+                } catch (Exception e) {
+                    continue;
+                }
+
+                final String entry = name.replace('.', '/') + ".class";
+                final JarEntry jarEntry = jarFile.getJarEntry(entry);
+                if (jarEntry != null) {
+                    LOGGER.warning("jar '" + file.getAbsolutePath() + "' contains offending class: " + name
+                                                + ". It will be ignored.");
+                    return false;
+                }
+            }
+            return true;
+        } finally {
+            if (jarFile != null) {
+                try {
+                    jarFile.close();
+                } catch (IOException ioe) {
+                    // Ignored
+                }
+            }
+        }
     }
 }
