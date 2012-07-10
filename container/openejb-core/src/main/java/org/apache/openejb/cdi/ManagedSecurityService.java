27d8d356b6dcd5be2899ad85383470d4bc05444b
Romain Manni-Bucau <rmannibucau@apache.org>
2012-07-10 20:07:06 +0000

    TOMEE-275 managessecurityservice can't be subclassed
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1359878 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/cdi/ManagedSecurityService.java
+++ container/openejb-core/src/main/java/org/apache/openejb/cdi/ManagedSecurityService.java
@@ -19,9 +19,17 @@ package org.apache.openejb.cdi;
 import org.apache.openejb.loader.SystemInstance;
 import org.apache.openejb.spi.SecurityService;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.security.Principal;
+import java.security.PrivilegedActionException;
+import java.util.Properties;
 
-public class ManagedSecurityService extends org.apache.webbeans.corespi.security.ManagedSecurityService {
+public class ManagedSecurityService implements org.apache.webbeans.spi.SecurityService {
+    private final org.apache.webbeans.corespi.security.ManagedSecurityService delegate = new org.apache.webbeans.corespi.security.ManagedSecurityService();
+
     @Override
     public Principal getCurrentPrincipal() {
         final SecurityService<?> service = SystemInstance.get().getComponent(org.apache.openejb.spi.SecurityService.class);
@@ -29,5 +37,65 @@ public class ManagedSecurityService extends org.apache.webbeans.corespi.security
             return service.getCallerPrincipal();
         }
         return null;
+    }
+
+    @Override
+    public <T> Constructor<T> doPrivilegedGetDeclaredConstructor(final Class<T> clazz, final Class<?>... parameterTypes) {
+        return delegate.doPrivilegedGetDeclaredConstructor(clazz, parameterTypes);
+    }
+
+    @Override
+    public <T> Constructor<?>[] doPrivilegedGetDeclaredConstructors(final Class<T> clazz) {
+        return delegate.doPrivilegedGetDeclaredConstructors(clazz);
+    }
+
+    @Override
+    public <T> Method doPrivilegedGetDeclaredMethod(final Class<T> clazz, final String name, final Class<?>... parameterTypes) {
+        return delegate.doPrivilegedGetDeclaredMethod(clazz, name, parameterTypes);
+    }
+
+    @Override
+    public <T> Method[] doPrivilegedGetDeclaredMethods(final Class<T> clazz) {
+        return delegate.doPrivilegedGetDeclaredMethods(clazz);
+    }
+
+    @Override
+    public <T> Field doPrivilegedGetDeclaredField(final Class<T> clazz, final String name) {
+        return delegate.doPrivilegedGetDeclaredField(clazz, name);
+    }
+
+    @Override
+    public <T> Field[] doPrivilegedGetDeclaredFields(final Class<T> clazz) {
+        return delegate.doPrivilegedGetDeclaredFields(clazz);
+    }
+
+    @Override
+    public void doPrivilegedSetAccessible(final AccessibleObject obj, final boolean flag) {
+        delegate.doPrivilegedSetAccessible(obj, flag);
+    }
+
+    @Override
+    public boolean doPrivilegedIsAccessible(final AccessibleObject obj) {
+        return delegate.doPrivilegedIsAccessible(obj);
+    }
+
+    @Override
+    public <T> T doPrivilegedObjectCreate(final Class<T> clazz) throws PrivilegedActionException, IllegalAccessException, InstantiationException {
+        return delegate.doPrivilegedObjectCreate(clazz);
+    }
+
+    @Override
+    public void doPrivilegedSetSystemProperty(final String propertyName, final String value) {
+        delegate.doPrivilegedSetSystemProperty(propertyName, value);
+    }
+
+    @Override
+    public String doPrivilegedGetSystemProperty(final String propertyName, final String defaultValue) {
+        return delegate.doPrivilegedGetSystemProperty(propertyName, propertyName);
+    }
+
+    @Override
+    public Properties doPrivilegedGetSystemProperties() {
+        return delegate.doPrivilegedGetSystemProperties();
     }
 }
