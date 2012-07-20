92dc58aa25ba8c1f0e87f95001e2d2d17323c064
Romain Manni-Bucau <rmannibucau@apache.org>
2012-07-20 15:05:45 +0000

    TOMEE-334 adding some logic to allow to serialize the archive
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1363820 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/cluster/DeployMessage.java
+++ tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/cluster/DeployMessage.java
@@ -1,16 +1,42 @@
 package org.apache.tomee.catalina.cluster;
 
 import org.apache.catalina.ha.ClusterMessageBase;
+import org.apache.openejb.loader.IO;
+import org.apache.openejb.loader.SystemInstance;
+import org.apache.tomee.catalina.TomEERuntimeException;
 
-// TODO: serialize file in byte[] to be able to send it over the network?
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+
 public class DeployMessage extends ClusterMessageBase {
+    public static final String TOMEE_CLUSTER_DEPLOY_SEND_ARCHIVE = "tomee.cluster.deploy.send-archive";
+
     private String file;
+    private byte[] archive = null;
 
     public DeployMessage(final String path) {
         file = path;
+        if (SystemInstance.get().getOptions().get(TOMEE_CLUSTER_DEPLOY_SEND_ARCHIVE, false)) {
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(path);
+                IO.copy(fis, baos);
+                archive = baos.toByteArray();
+            } catch (Exception e) {
+                throw new TomEERuntimeException(e);
+            } finally {
+                IO.close(fis);
+                IO.close(baos);
+            }
+        }
     }
 
     public String getFile() {
         return file;
+    }
+
+    public byte[] getArchive() {
+        return archive;
     }
 }
