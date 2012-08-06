04165d53294d6e1fb82148c60b444f28c11f5224
David Blevins <dblevins@apache.org>
2012-08-06 02:38:49 +0000

    reformatted, final added (no logic change)
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1369714 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/util/proxy/LocalBeanProxyFactory.java
+++ container/openejb-core/src/main/java/org/apache/openejb/util/proxy/LocalBeanProxyFactory.java
@@ -16,24 +16,23 @@
  */
 package org.apache.openejb.util.proxy;
 
+import javax.ejb.EJBException;
 import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import javax.ejb.EJBException;
-
 public class LocalBeanProxyFactory {
 
     private static final java.lang.reflect.InvocationHandler NON_BUSINESS_HANDLER = new NonBusinessHandler();
-    
+
     public static Object newProxyInstance(ClassLoader cl, Class interfce, java.lang.reflect.InvocationHandler h) throws IllegalArgumentException {
-            try {
-            Class proxyCls = new LocalBeanProxyGeneratorImpl().createProxy(interfce, cl);
-            Constructor constructor = proxyCls.getConstructor(java.lang.reflect.InvocationHandler.class, 
-                                                              java.lang.reflect.InvocationHandler.class);
-            Object object = constructor.newInstance(h, NON_BUSINESS_HANDLER);
+        try {
+            final Class proxyCls = new LocalBeanProxyGeneratorImpl().createProxy(interfce, cl);
+            final Constructor constructor = proxyCls.getConstructor(java.lang.reflect.InvocationHandler.class,
+                    java.lang.reflect.InvocationHandler.class);
+            final Object object = constructor.newInstance(h, NON_BUSINESS_HANDLER);
             return object;
         } catch (NoSuchMethodException e) {
             throw new InternalError(e.toString());
@@ -48,7 +47,7 @@ public class LocalBeanProxyFactory {
 
     public static InvocationHandler getInvocationHandler(Object proxy) {
         try {
-            Field field = proxy.getClass().getDeclaredField(LocalBeanProxyGeneratorImpl.BUSSINESS_HANDLER_NAME);
+            final Field field = proxy.getClass().getDeclaredField(LocalBeanProxyGeneratorImpl.BUSSINESS_HANDLER_NAME);
             field.setAccessible(true);
             try {
                 return (InvocationHandler) field.get(proxy);
@@ -61,12 +60,12 @@ public class LocalBeanProxyFactory {
             throw new IllegalArgumentException(e);
         }
     }
-    
+
     private static class NonBusinessHandler implements java.lang.reflect.InvocationHandler, Serializable {
 
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             throw new EJBException("Calling non-public methods of a local bean without any interfaces is not allowed");
         }
-        
+
     }
 }
