6224967b360193977caef00e876f24d2591cbada
Romain Manni-Bucau <rmannibucau@apache.org>
2012-08-07 16:19:03 +0000

    no need of any interceptor finally for deployment exception catching, arquillian gives it to us through an event
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1370339 13f79535-47bb-0310-9956-ffa450edef68

--- arquillian/arquillian-common/src/main/java/org/apache/openejb/arquillian/common/deployment/DeploymentExceptionObserver.java
+++ arquillian/arquillian-common/src/main/java/org/apache/openejb/arquillian/common/deployment/DeploymentExceptionObserver.java
@@ -1,8 +1,7 @@
 package org.apache.openejb.arquillian.common.deployment;
 
-import org.jboss.arquillian.container.spi.event.DeployDeployment;
+import org.jboss.arquillian.container.spi.client.container.DeploymentException;
 import org.jboss.arquillian.core.api.annotation.Observes;
-import org.jboss.arquillian.core.spi.EventContext;
 import org.jboss.arquillian.test.spi.event.suite.AfterClass;
 
 import java.util.HashMap;
@@ -14,24 +13,20 @@ public class DeploymentExceptionObserver {
     private static final Map<Class<?>, Exception> EXCEPTIONS = new HashMap<Class<?>, Exception>();
     private static final Map<Class<?>, Exception> PARENT_EXCEPTIONS = new HashMap<Class<?>, Exception>();
 
-    public void observes(@Observes final EventContext<DeployDeployment> dd) throws Exception {
-        try {
-            dd.proceed();
-        } catch (Exception t) {
-            EXCEPTIONS.put(t.getClass(), t);
+    public void observes(@Observes final DeploymentException t) throws Exception {
+        EXCEPTIONS.put(t.getClass(), t);
 
-            Throwable current = t.getCause();
-            while (current != null) {
-                if (current instanceof Exception) {
-                    PARENT_EXCEPTIONS.put(current.getClass(), (Exception) current);
-                }
-                if (current.getCause() != current) {
-                    current = current.getCause();
-                }
+        Throwable current = t.getCause();
+        while (current != null) {
+            if (current instanceof Exception) {
+                PARENT_EXCEPTIONS.put(current.getClass(), (Exception) current);
             }
-
-            throw t;
+            if (current.getCause() != current) {
+                current = current.getCause();
+            }
         }
+
+        throw t; // don't forget it even if it is an observer and not an interceptor
     }
 
     public static Exception getExceptions(final Class<?> clazz) {
