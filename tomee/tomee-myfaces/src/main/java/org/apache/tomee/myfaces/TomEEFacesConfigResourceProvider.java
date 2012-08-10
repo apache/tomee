7eea44692a31be2da345e06c62e43d23de146ac9
Romain Manni-Bucau <rmannibucau@apache.org>
2012-08-10 13:15:49 +0000

    some more exclusions, and a custom FacesConfigResourceProvider to avoid to scan the whole app libs to find file ending with .faces-config.xml (now myfaces check if it should start for each app), adding a property to skip jsf auto startup openejb.jsf.skip = true)
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1371694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tomee/tomee-myfaces/src/main/java/org/apache/tomee/myfaces/TomEEFacesConfigResourceProvider.java
@@ -0,0 +1,96 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tomee.myfaces;
+
+import org.apache.myfaces.config.DefaultFacesConfigResourceProvider;
+import org.apache.myfaces.shared.util.ClassUtils;
+import org.apache.openejb.config.NewLoaderLogic;
+import org.apache.openejb.loader.Files;
+import org.apache.openejb.util.URLs;
+import org.apache.xbean.finder.UrlSet;
+
+import javax.faces.context.ExternalContext;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
+
+public class TomEEFacesConfigResourceProvider extends DefaultFacesConfigResourceProvider {
+    private static final String META_INF_PREFIX = "META-INF/";
+    private static final String FACES_CONFIG_SUFFIX = ".faces-config.xml";
+    private static final String FACES_CONFIG_IMPLICIT = "META-INF/faces-config.xml";
+    private static final Pattern FILE_PATTERN = Pattern.compile("^.*" + Pattern.quote(FACES_CONFIG_SUFFIX) + "$");
+
+    @Override
+    public Collection<URL> getMetaInfConfigurationResources(final ExternalContext context) throws IOException {
+        final List<URL> urlSet = new ArrayList<URL>();
+        final ClassLoader loader = getClassLoader();
+
+        final Enumeration<URL> resources = loader.getResources(FACES_CONFIG_IMPLICIT);
+        while (resources.hasMoreElements()) {
+            urlSet.add(resources.nextElement());
+        }
+
+        // Scan files inside META-INF ending with .faces-config.xml
+        for (URL url : NewLoaderLogic.applyBuiltinExcludes(new UrlSet(loader)).getUrls()) {
+            final File file = URLs.toFile(url);
+            if (!file.exists()) {
+                continue;
+            }
+
+            if (!file.isDirectory()) { // browse all entries to see if we have a matching file
+                final Enumeration<JarEntry> e = new JarFile(file).entries();
+                while (e.hasMoreElements()) {
+                    try {
+                        final String name = e.nextElement().getName();
+                        if (name.startsWith(META_INF_PREFIX) && name.endsWith(FACES_CONFIG_SUFFIX)) {
+                            final Enumeration<URL> e2 = loader.getResources(name);
+                            while (e2.hasMoreElements()) {
+                                urlSet.add(e2.nextElement());
+                            }
+                        }
+                    } catch (Throwable ignored) {
+                        // no-op
+                    }
+                }
+            } else {
+                final File metaInf = new File(file, META_INF_PREFIX);
+                if (metaInf.exists() && metaInf.isDirectory()) {
+                    for (File f : Files.collect(metaInf, FILE_PATTERN)) {
+                        urlSet.add(f.toURI().toURL());
+                    }
+                }
+            }
+        }
+
+        return urlSet;
+    }
+
+    private ClassLoader getClassLoader() {
+        ClassLoader loader = ClassUtils.getContextClassLoader();
+        if (loader == null) {
+            loader = this.getClass().getClassLoader();
+        }
+        return loader;
+    }
+}
