e2e3bc6b6854f2c7843c6fd091917cd550e4e939
Vishwanath <vishwanathk@apache.org>
2012-09-08 20:16:51 +0000

    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1382352 13f79535-47bb-0310-9956-ffa450edef68

---
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/jsf/jms/DummyManagedBean.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.arquillian.tests.jsf.jms;
+
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.jms.Queue;
+import javax.jms.Topic;
+import javax.jms.ConnectionFactory;
+
+@ManagedBean
+public class DummyManagedBean {
+    @Resource
+    Queue queue;
+
+    @Resource
+    Topic topic;
+
+    @Resource
+    ConnectionFactory connectionFactory;
+
+    public String getFoo()
+    {
+        if(queue!=null){return "queueInjected";}
+        return "queueNotInjected";
+    }
+
+    public String getBoo()
+    {
+        if(topic!=null){return "topicInjected";}
+        return "topicNotInjected";
+    }
+
+    public String getBaz()
+    {
+        if(connectionFactory!=null){return "connectionFactoryInjected";}
+        return "connectionFactoryNotInjected";
+    }
+}
