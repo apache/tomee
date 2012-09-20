2b3aa8bd70ece967d58e4bf48ccb42e5019fcb90
Romain Manni-Bucau <rmannibucau@apache.org>
2012-09-20 10:51:54 +0000

    version 4.5.0
    git-svn-id: https://svn.apache.org/repos/asf/openejb/branches/openejb-4.1.0@1387961 13f79535-47bb-0310-9956-ffa450edef68

--- maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/AbstractTomEEMojo.java
+++ maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/AbstractTomEEMojo.java
@@ -1,4 +1,3 @@
-package org.apache.openejb.maven.plugin;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  *  contributor license agreements.  See the NOTICE file distributed with
@@ -101,7 +100,7 @@ public abstract class AbstractTomEEMojo extends AbstractAddressMojo {
     protected boolean skipCurrentProject;
 
     /**
-     * @parameter expression="${tomee-plugin.version}" default-value="1.1.0"
+     * @parameter expression="${tomee-plugin.version}" default-value="1.5.0"
      */
     protected String tomeeVersion;
 
