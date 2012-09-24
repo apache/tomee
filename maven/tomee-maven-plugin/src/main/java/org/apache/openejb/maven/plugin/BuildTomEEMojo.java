c154100d09fdcbd0c80e391e4ef6f14087ce07f6
Romain Manni-Bucau <rmannibucau@apache.org>
2012-09-24 12:43:10 +0000

    TOMEE-425 zip and atatch a custom tomee distribution
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1389340 13f79535-47bb-0310-9956-ffa450edef68

--- maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/BuildTomEEMojo.java
+++ maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/BuildTomEEMojo.java
@@ -17,11 +17,77 @@
 
 package org.apache.openejb.maven.plugin;
 
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.openejb.maven.plugin.util.Zips.zip;
+
 /**
  * @goal build
  * @requiresDependencyResolution runtime
  */
 public class BuildTomEEMojo extends AbstractTomEEMojo {
+    /**
+     * @parameter expression="${tomee-plugin.zip}" default-value="true"
+     */
+    protected boolean zip;
+
+    /**
+     * @parameter expression="${tomee-plugin.attach}" default-value="true"
+     */
+    protected boolean attach;
+
+    /**
+     * @parameter expression="${tomee-plugin.zip-file}" default-value="${project.build.directory}/${project.build.finalName}.zip""
+     * @required
+     * @readOnly
+     */
+    private File zipFile;
+
+    /**
+     * @component
+     */
+    private MavenProjectHelper projectHelper;
+
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * @parameter expression="${tomee-plugin.classifier}"
+     */
+    protected String classifier = null;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        super.execute();
+        if (zip) {
+            getLog().info("Zipping Custom TomEE Distribution");
+            try {
+                zip(catalinaBase, zipFile);
+            } catch (IOException e) {
+                throw new MojoExecutionException(e.getMessage(), e);
+            }
+
+            if (attach) {
+                getLog().info("Attaching Custom TomEE Distribution");
+                if (classifier != null) {
+                    projectHelper.attachArtifact(project, "zip", classifier, zipFile);
+                } else {
+                    projectHelper.attachArtifact(project, "zip", zipFile);
+                }
+            }
+        }
+    }
+
     @Override
     protected void run() {
         // don't start
