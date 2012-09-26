66552bbccd4c65ee22480a5cbcd53023c960c146
Romain Manni-Bucau <rmannibucau@apache.org>
2012-09-26 10:21:33 +0000

    TOMEE-430 removeTomeeWebapp option
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1390373 13f79535-47bb-0310-9956-ffa450edef68

--- maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/util/Zips.java
+++ maven/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/util/Zips.java
@@ -23,8 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.jar.JarEntry;
-import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
