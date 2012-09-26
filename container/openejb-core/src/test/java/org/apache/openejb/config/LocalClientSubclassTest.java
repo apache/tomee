bc66950e2b807094e46c0506a410c6a236135b04
Jean-Louis MONTEIRO <jlmonteiro@apache.org>
2012-09-26 08:48:55 +0000

    Add Bval preload on core to avoid LinkageError on Mac
    git-svn-id: https://svn.apache.org/repos/asf/openejb/branches/openejb-4.5.0@1390328 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/test/java/org/apache/openejb/config/LocalClientSubclassTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/config/LocalClientSubclassTest.java
@@ -17,6 +17,7 @@
 package org.apache.openejb.config;
 
 import org.apache.openejb.core.LocalInitialContextFactory;
+import org.apache.webbeans.logger.JULLoggerFactory;
 
 import javax.naming.InitialContext;
 import java.util.Properties;
@@ -26,9 +27,13 @@ import java.util.Properties;
 */
 public class LocalClientSubclassTest extends LocalClientTest {
 
+    public void setUp() {
+        //avoid linkage error on mac, only used for tests so don't need to add it in Core
+        JULLoggerFactory.class.getName();
+    }
+
     @Override
     public void test() throws Exception {
-
         Properties properties = new Properties();
         properties.setProperty(javax.naming.Context.INITIAL_CONTEXT_FACTORY, LocalInitialContextFactory.class.getName());
         InitialContext context = new InitialContext(properties);
