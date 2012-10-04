01e61dbf7a186b7f2884df714ae2de0a68540c26
Romain Manni-Bucau <rmannibucau@apache.org>
2012-10-04 09:57:59 +0000

    TOMEE-439 TCK needs delegate for webappclassloader
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1393974 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/TomcatWebAppBuilder.java
+++ tomee/tomee-catalina/src/main/java/org/apache/tomee/catalina/TomcatWebAppBuilder.java
@@ -437,7 +437,7 @@ public class TomcatWebAppBuilder implements WebAppBuilder, ContextListener, Pare
                 // but here we have all the classloading logic
                 if (classLoader != null) {
                     standardContext.setParentClassLoader(classLoader);
-                    //standardContext.setDelegate(true);
+                    standardContext.setDelegate(true);
                     //standardContext.setLoader(new TomEEWebappLoader(appInfo.path, classLoader));
                     //standardContext.getLoader().setDelegate(true);
                 }
