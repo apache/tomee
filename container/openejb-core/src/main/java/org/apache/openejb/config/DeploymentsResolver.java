2b5e9b45d8642ede51ef40f9c59374f2173c5e5a
David Blevins <dblevins@apache.org>
2012-12-02 21:15:02 +0000

    trim out unused code
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1416261 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentsResolver.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/DeploymentsResolver.java
@@ -150,14 +150,6 @@ public class DeploymentsResolver implements DeploymentFilterable {
         return c == '.';
     }
 
-    private static boolean unpackExists(final File file) {
-        final String name = file.getName();
-
-
-        return !(!name.endsWith("ar") || name.length() < 5)
-                && new File(file.getParentFile(), name.substring(0, name.length()  -".war".length())).exists();
-    }
-
     /**
      * The algorithm of OpenEJB deployments class-path inclusion and exclusion is implemented as follows:
      * 1- If the string value of the resource URL matches the include class-path pattern
@@ -300,98 +292,6 @@ public class DeploymentsResolver implements DeploymentFilterable {
             logger.warning("Unable to search classpath for modules: Received Exception: " + e1.getClass().getName() + " " + e1.getMessage(), e1);
         }
 
-    }
-
-    /**
-     * The regular expressions involved in filtering can be costly
-     * In the normal case we will not scan anyway, so if not
-     * no point in optimizing the list of urls in the classpath
-     *
-     * @param include
-     * @param exclude
-     * @param requireDescriptors
-     * @return
-     */
-    private static boolean shouldFilter(final String include, final String exclude, final Set<RequireDescriptors> requireDescriptors) {
-        final boolean includeNothing = include.equals("");
-        final boolean excludeEverything = exclude.equals(".*");
-
-        //  If we are going to eliminate the entire classpath from
-        //  scanning anyway, no sense in taking the time to do it
-        //  bit by bit.  Return false
-        if (includeNothing && excludeEverything) return false;
-
-        //  If we are forcably requiring descriptors for all possible file types
-        //  then there is also no scanning and no point in filtering the
-        //  classpath down bit by bit.  Return false
-        return requireDescriptors.size() != RequireDescriptors.values().length;
-    }
-
-    private static UrlSet applyBuiltinExcludes(UrlSet urlSet) throws MalformedURLException {
-
-        urlSet = urlSet.exclude(".*/activation(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/activeio-core(-[\\d.]+)?(-incubator)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/activemq-(core|ra)(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/annotations-api-6.[01].[\\d.]+.jar(!/)?");
-        urlSet = urlSet.exclude(".*/asm-(all|commons|util|tree)?[\\d.]+.jar(!/)?");
-        urlSet = urlSet.exclude(".*/avalon-framework(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/axis2-jaxws-api(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/backport-util-concurrent(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/bcprov-jdk15(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/catalina(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/cglib-(nodep-)?[\\d.]+.jar(!/)?");
-        urlSet = urlSet.exclude(".*/com\\.ibm\\.ws\\.[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/commons-(logging|logging-api|cli|pool|lang|collections|dbcp|dbcp-all)(-[\\d.r-]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/cxf-bundle(-[\\d.]+)?(incubator)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/openejb-cxf-bundle(-[\\d.]+)?(incubator)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/derby(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/ejb31-api-experimental(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/geronimo-(connector|transaction)(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/geronimo-[^/]+_spec(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/geronimo-javamail_([\\d.]+)_mail(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/hibernate-(entitymanager|annotations)?(-[\\d.]+(ga)?)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/howl(-[\\d.-]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/hsqldb(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/idb(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/idea_rt.jar(!/)?");
-        urlSet = urlSet.exclude(".*/javaee-api(-embedded)?-[\\d.-]+.jar(!/)?");
-        urlSet = urlSet.exclude(".*/javassist[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jaxb-(impl|api)(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jboss-[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jbossall-[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jbosscx-[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jbossjts-?[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jbosssx-[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/jmdns(-[\\d.]+)?(-RC\\d)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/juli(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/junit(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/log4j(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/logkit(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/mail(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/neethi(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/org\\.eclipse\\.persistence\\.[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/org\\.junit_.[^/]*.jar(!/)?");
-        urlSet = urlSet.exclude(".*/openjpa-(jdbc|kernel|lib|persistence|persistence-jdbc)(-5)?(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/openjpa(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/opensaml(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/quartz(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/saaj-impl(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/serp(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/servlet-api(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/slf4j-api(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/slf4j-jdk14(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/stax-api(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/swizzle-stream(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/sxc-(jaxb|runtime)(-[\\d.]+)?(-SNAPSHOT)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/wsdl4j(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/wss4j(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/wstx-asl(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/xbean-(reflect|naming|finder)-(shaded-)?[\\d.]+.jar(!/)?");
-        urlSet = urlSet.exclude(".*/xmlParserAPIs(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/xmlunit(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/xmlsec(-[\\d.]+)?.jar(!/)?");
-        urlSet = urlSet.exclude(".*/XmlSchema(-[\\d.]+)?.jar(!/)?");
-        return urlSet;
     }
 
     public static void processUrls(final List<URL> urls, final ClassLoader classLoader, final Set<RequireDescriptors> requireDescriptors, final FileUtils base, final List<URL> jarList) {
