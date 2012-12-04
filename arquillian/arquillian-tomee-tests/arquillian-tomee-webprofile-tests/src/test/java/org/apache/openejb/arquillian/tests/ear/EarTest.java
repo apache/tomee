d06e34fe4734f1375e4f44b55a4ace04df6ee7b7
Andrew Gumbrecht <andygumbrecht@apache.org>
2012-12-04 11:05:54 +0000

    Finals.
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1416881 13f79535-47bb-0310-9956-ffa450edef68

--- arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/ear/EarTest.java
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/ear/EarTest.java
@@ -44,6 +44,7 @@ import java.net.URL;
 /**
  * @version $Rev$ $Date$
  */
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
 @RunWith(Arquillian.class)
 public class EarTest {
 
@@ -102,7 +103,7 @@ public class EarTest {
         private Bean bean;
 
         @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
             resp.getWriter().print(bean.getMessage());
         }
     }
