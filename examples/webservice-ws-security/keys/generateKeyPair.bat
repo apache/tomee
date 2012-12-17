2189bd293b6a02efb28322d52d2278a70b1349d7
Andrew Gumbrecht <andygumbrecht@apache.org>
2012-12-17 11:41:55 +0000

    Surround keystore path for win platforms. Maven plugin versions.
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/openejb@1422863 13f79535-47bb-0310-9956-ffa450edef68

--- examples/webservice-ws-security/keys/generateKeyPair.bat
+++ examples/webservice-ws-security/keys/generateKeyPair.bat
@@ -26,6 +26,6 @@ echo KeyStorePass %4
 echo keyName %5
 
 echo keyName %5
-keytool -genkey -alias %1 -keypass %2 -keystore %3 -storepass %4  -dname "cn=%1" -keyalg RSA
-keytool -selfcert -alias %1 -keystore %3 -storepass %4 -keypass %2
-keytool -export -alias %1 -file %5 -keystore %3 -storepass %4
+keytool -genkey -alias %1 -keypass %2 -keystore "%3" -storepass %4  -dname "cn=%1" -keyalg RSA
+keytool -selfcert -alias %1 -keystore "%3" -storepass %4 -keypass %2
+keytool -export -alias %1 -file %5 -keystore "%3" -storepass %4
