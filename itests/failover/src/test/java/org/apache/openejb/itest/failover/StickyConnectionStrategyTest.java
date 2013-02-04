92c44311775921a23b8fcc2085963ef9e586199e
Andrew Gumbrecht <andygumbrecht@apache.org>
2013-02-04 14:00:20 +0000

    Oops?
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1442120 13f79535-47bb-0310-9956-ffa450edef68

--- itests/failover/src/test/java/org/apache/openejb/itest/failover/StickyConnectionStrategyTest.java
+++ itests/failover/src/test/java/org/apache/openejb/itest/failover/StickyConnectionStrategyTest.java
@@ -66,7 +66,7 @@ public class StickyConnectionStrategyTest {
         logger.setUseParentHandlers(false);
 		
 		//Set a high socket timeout for tests
-		System.setProperty("openejb.client.connection.socket.timeout", System.getProperty("openejb.client.connection.socket.timeout", "5000");
+		System.setProperty("openejb.client.connection.socket.timeout", System.getProperty("openejb.client.connection.socket.timeout", "5000"));
     }
 
     @Test
