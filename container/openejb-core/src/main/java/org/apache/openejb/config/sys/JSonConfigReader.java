6993a62efe20aa07128b7356d11155ff7724b03a
Romain Manni-Bucau <rmannibucau@apache.org>
2013-06-07 21:32:04 +0000

    ignoring test while needing java 7
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1490839 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/sys/JSonConfigReader.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/sys/JSonConfigReader.java
@@ -17,16 +17,13 @@
 package org.apache.openejb.config.sys;
 
 import org.apache.openejb.OpenEJBException;
-import org.xml.sax.Attributes;
 import org.xml.sax.helpers.AttributesImpl;
 
-import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.SimpleBindings;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 
@@ -36,9 +33,6 @@ public class JSonConfigReader {
     }
 
     public static Openejb read(final String json) throws OpenEJBException {
-        final Bindings bindings = new SimpleBindings();
-        bindings.put("json", json);
-
         final SaxOpenejb config = new SaxOpenejb();
 
         final ScriptEngineManager manager = new ScriptEngineManager();
@@ -54,37 +48,39 @@ public class JSonConfigReader {
             config.startDocument();
             config.startElement(null, "openejb", null, new AttributesImpl());
 
-            engine.eval("var openejbConfig = JSON.parse(json)", bindings);
-            final Map<String, Map<String, Map<String, String>>> rawConfiguration = map(bindings.get("openejbConfig"));
+            final Map<?, ?> jsConfig = map(engine.eval("eval(" + json + ")")); // TODO: see how to do so in java 6
 
-            {
-                final Map<String, Map<String, Map<String, String>>> resources = map(rawConfiguration.get("resources"));
+            for (final String root :
+                    Arrays.asList("Resource", "Container", "JndiProvider", "TransactionManager", "ConnectionManager",
+                            "ProxyFactory", "Connector", "Deployments", "Import", "Service", "SecurityService")) {
+
+                final String currentRoot;
+                if (root.endsWith("s")) {
+                    currentRoot = root.toLowerCase();
+                } else {
+                    currentRoot = root.toLowerCase() + "s";
+                }
+
+                final Map<String, Map<String, Map<String, String>>> resources = map(jsConfig.get(currentRoot));
                 if (resources != null) {
                     for (final Map.Entry<String, Map<String, Map<String, String>>> resource : resources.entrySet()) {
                         final AttributesImpl attributes = toAttributes(map(resource.getValue()), "properties");
                         attributes.addAttribute(null, "id", "id", null, resource.getKey());
-                        if (attributes.getIndex("type") == -1) {
+
+                        if ("resources".equals(root) && attributes.getIndex("type") == -1) {
                             attributes.addAttribute(null, "type", "type", null, "DataSource");
                         }
-                        config.startElement(null, "Resource", "Resource", attributes);
+
+                        config.startElement(null, root, root, attributes);
 
                         final String propertiesAsStr = toString(map(resource.getValue().get("properties")));
 
                         config.characters(propertiesAsStr.toCharArray(), 0, propertiesAsStr.length());
                         // properties
-                        config.endElement(null, "Resource", "Resource");
+                        config.endElement(null, root, root);
                     }
                 }
             }
-
-            {
-                final Map<String, Map<String, String>> containers = map(rawConfiguration.get("containers"));
-                if (containers != null) {
-                    // TODO
-                }
-            }
-
-            // TODO: see org.apache.openejb.config.sys.SaxOpenejb.Root.startElement() for all parent tags)
 
             config.endElement(null, "openejb", null);
         } catch (final Exception e) {
