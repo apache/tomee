5d72eebca21dd822517343a299b1a82183c3efe4
Andrew Gumbrecht <andygumbrecht@apache.org>
2013-08-08 09:48:21 +0000

    Added legacy-client test.
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1511656 13f79535-47bb-0310-9956-ffa450edef68

--- itests/legacy-client/src/test/java/org/apache/openejb/itest/legacy/ClientThread.java
+++ itests/legacy-client/src/test/java/org/apache/openejb/itest/legacy/ClientThread.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.openejb.itest.failover;
+package org.apache.openejb.itest.legacy;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
