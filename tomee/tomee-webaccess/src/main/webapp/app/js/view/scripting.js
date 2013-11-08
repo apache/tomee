1f8a51ba7a7c710c4c1b823f000ce8fc91a70705
Thiago Veronezi <tveronezi@apache.org>
2013-11-08 02:32:16 +0000

    https://issues.apache.org/jira/browse/TOMEE-1072
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1539905 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/tomee-webaccess/src/main/webapp/app/js/view/scripting.js
+++ tomee/tomee-webaccess/src/main/webapp/app/js/view/scripting.js
@@ -31,24 +31,38 @@
             groovy: templates.getValue('script-sample-groovy', {})
         };
 
+        var executeScript = function (evt) {
+            evt.preventDefault();
+            var me = this;
+            me.trigger('execute-action', {
+                engine: me.editor.getOption("mode"),
+                script: me.editor.getValue()
+            });
+            me.$el.find('.ux-execute-script').addClass('disabled');
+            me.$el.find('.ux-clean-execute-script').addClass('disabled');
+        };
+
+        var cleanOutput = function () {
+            var me = this;
+            var pre = $(me.$el.find('.ux-script-output>div.panel-body>pre').get(0));
+            pre.empty();
+        };
+
         var View = Backbone.View.extend({
             tagName: 'div',
             className: 'ux-scripting',
             events: {
-                'click .ux-script-output>div.panel-heading>button': function () {
-                    var me = this;
-                    var pre = $(me.$el.find('.ux-script-output>div.panel-body>pre').get(0));
-                    pre.empty();
-                },
+                'click .ux-script-output>div.panel-heading>button': cleanOutput,
                 'click .ux-execute-script': function (evt) {
-                    // TRICK to avoid full page reload.
-                    evt.preventDefault();
                     var me = this;
-                    me.trigger('execute-action', {
-                        engine: me.editor.getOption("mode"),
-                        script: me.editor.getValue()
-                    });
-                    me.$el.find('.ux-execute-script').addClass('disabled');
+                    executeScript.call(me, evt);
+                    me.updatePrimaryExecuteBtn('ux-execute-script');
+                },
+                'click .ux-clean-execute-script': function (evt) {
+                    var me = this;
+                    cleanOutput.call(me);
+                    executeScript.call(me, evt);
+                    me.updatePrimaryExecuteBtn('ux-clean-execute-script');
                 },
                 'click .ux-source-option': function (evt) {
                     // TRICK to avoid full page reload.
@@ -58,6 +72,19 @@
                     me.trigger('type-chosen', {
                         name: myLink.attr('name')
                     });
+                }
+            },
+
+            updatePrimaryExecuteBtn: function (newCls) {
+                var me = this;
+                var btn = $(me.$el.find('.ux-execute-primary').get(0));
+                btn.removeClass('ux-clean-execute-script');
+                btn.removeClass('ux-execute-script');
+                btn.addClass(newCls);
+                if (newCls === 'ux-execute-script') {
+                    btn.html(i18n.get('execute'));
+                } else {
+                    btn.html(i18n.get('clean.execute'));
                 }
             },
 
@@ -91,6 +118,7 @@
                 var pre = $(me.$el.find('.ux-script-output>div.panel-body>pre').get(0));
                 pre.append(formatted);
                 me.$el.find('.ux-execute-script').removeClass('disabled');
+                me.$el.find('.ux-clean-execute-script').removeClass('disabled');
                 var contentArea = $(me.$el.find('.panel-body').get(0));
                 contentArea.animate({
                     scrollTop: contentArea.prop("scrollHeight")
