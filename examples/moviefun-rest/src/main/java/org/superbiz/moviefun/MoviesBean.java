1ab59ffa749c3b4e6d96edb35100f2ac503fefb0
Thiago Veronezi <tveronezi@apache.org>
2014-01-21 20:33:48 +0000

    Reproducing some of the moviefun tests in moviefun-rest
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1560174 13f79535-47bb-0310-9956-ffa450edef68

--- examples/moviefun-rest/src/main/java/org/superbiz/moviefun/MoviesBean.java
+++ examples/moviefun-rest/src/main/java/org/superbiz/moviefun/MoviesBean.java
@@ -20,7 +20,11 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.EntityType;
 import java.util.List;
 
--- examples/moviefun-rest/src/main/java/org/superbiz/moviefun/rest/MoviesRest.java
+++ examples/moviefun-rest/src/main/java/org/superbiz/moviefun/rest/MoviesRest.java
@@ -20,7 +20,15 @@ import org.superbiz.moviefun.Movie;
 import org.superbiz.moviefun.MoviesBean;
 
 import javax.ejb.EJB;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import java.util.List;
 
 @Path("movies")
