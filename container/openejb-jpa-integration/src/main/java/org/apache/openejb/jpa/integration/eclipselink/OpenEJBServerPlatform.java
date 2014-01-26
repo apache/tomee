495b30a53c1a1e55af7fad57cef507faf69e09b9
Romain Manni-Bucau <rmannibucau@apache.org>
2014-01-26 21:53:28 +0000

    adding OpenEJB server platform for eclipselink
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1561551 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/openejb-jpa-integration/src/main/java/org/apache/openejb/jpa/integration/eclipselink/OpenEJBServerPlatform.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.jpa.integration.eclipselink;
+
+import org.eclipse.persistence.platform.server.JMXServerPlatformBase;
+import org.eclipse.persistence.sessions.DatabaseSession;
+import org.eclipse.persistence.transaction.JTATransactionController;
+
+import javax.transaction.TransactionManager;
+
+public class OpenEJBServerPlatform extends JMXServerPlatformBase {
+    public OpenEJBServerPlatform(final DatabaseSession newDatabaseSession) {
+        super(newDatabaseSession);
+    }
+
+    @Override
+    public boolean isRuntimeServicesEnabledDefault() {
+        return "true".equalsIgnoreCase(System.getProperty("openejb.eclipselink.jmx", "true"));
+    }
+
+    @Override
+    public Class getExternalTransactionControllerClass() {
+        return OpenEJBJTATransactionController.class;
+    }
+
+    public static class OpenEJBJTATransactionController extends JTATransactionController {
+        @Override
+        protected TransactionManager acquireTransactionManager() throws Exception {
+            return TransactionManager.class.cast(
+                    OpenEJBJTATransactionController.class.getClassLoader().loadClass("org.apache.openejb.OpenEJB")
+                            .getDeclaredMethod("getTransactionManager").invoke(null));
+        }
+    }
+}
