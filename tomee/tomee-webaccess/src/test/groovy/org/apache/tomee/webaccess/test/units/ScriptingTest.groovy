faf9746942d65553927ca3e6dec9c83766abdfc7
Thiago Veronezi <tveronezi@apache.org>
2014-02-08 18:38:45 +0000

    https://issues.apache.org/jira/browse/TOMEE-1116 https://issues.apache.org/jira/browse/TOMEE-1117 https://issues.apache.org/jira/browse/TOMEE-1118 missing files
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1566089 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tomee/tomee-webaccess/src/test/groovy/org/apache/tomee/webaccess/test/units/ScriptingTest.groovy
@@ -0,0 +1,73 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License") you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.tomee.webaccess.test.units
+
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.message.BasicNameValuePair
+import org.apache.tomee.webaccess.rest.ApplicationConfig
+import org.apache.tomee.webaccess.rest.Authentication
+import org.apache.tomee.webaccess.rest.Scripting
+import org.apache.tomee.webaccess.service.ScriptingServiceImpl
+import org.apache.tomee.webaccess.test.helpers.Utilities
+import org.jboss.arquillian.container.test.api.Deployment
+import org.jboss.arquillian.junit.Arquillian
+import org.jboss.arquillian.test.api.ArquillianResource
+import org.jboss.shrinkwrap.api.ShrinkWrap
+import org.jboss.shrinkwrap.api.spec.WebArchive
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(Arquillian.class)
+class ScriptingTest {
+
+    @ArquillianResource
+    URL deploymentURL
+
+    @Deployment
+    static WebArchive createDeployment() {
+        Utilities.copyFile('test/loginScript.js', 'conf/loginScript.js')
+        Utilities.copyFile('test/login.config', 'conf/login.config')
+        ShrinkWrap.create(WebArchive.class, 'webaccess.war').addClasses(
+                ApplicationConfig,
+                Authentication,
+                Scripting,
+                ScriptingServiceImpl,
+                ScriptingTest
+        ).addAsWebResource(new File('src/test/resources/test/context.xml'), 'META-INF/context.xml')
+    }
+
+    @Test
+    void test() throws Exception {
+        Utilities.withClient(deploymentURL, { CloseableHttpClient client ->
+            Assert.assertEquals(
+                    '{"scriptingResultDto":{"output":"Hi there!"}}',
+                    Utilities.post(deploymentURL, client, 'rest/scripting',
+                            new BasicNameValuePair('engine', 'js'),
+                            new BasicNameValuePair('script', 'print("Hi there!");')
+                    )
+            )
+            Assert.assertTrue(Utilities.post(deploymentURL, client, 'rest/scripting',
+                    new BasicNameValuePair('engine', 'js'),
+                    new BasicNameValuePair('script', 'throw "my bad!";')
+            ).contains('javax.script.ScriptException: my bad!'))
+        })
+    }
+
+}
