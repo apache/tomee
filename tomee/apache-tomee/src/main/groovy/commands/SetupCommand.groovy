d009acf39926a4d4c3695fd0d4a27b59d74bc1ee
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-02-11 14:10:24 +0000

    Update groovy-maven-plugin usage Ignores.
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1567143 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/apache-tomee/src/main/groovy/commands/SetupCommand.groovy
+++ tomee/apache-tomee/src/main/groovy/commands/SetupCommand.groovy
@@ -24,20 +24,26 @@ import org.apache.tomee.installer.Paths
 import org.apache.tools.ant.taskdefs.optional.net.SetProxy
 
 class SetupCommand {
-    def log
-    def ant
-    def project
 
-    def SetupCommand(source) {
-        this.log = source.log
-        this.project = source.project
-        this.ant = new AntBuilder()
+    def log
+    def project
+    def ant
+    def properties
+
+    def SetupCommand(def log, def project, def ant, def properties) {
+        this.log = log
+        this.project = project
+        this.ant = ant
+        this.properties = properties
     }
 
     def get(name) {
         assert name != null
 
-        def value = project.properties.getProperty(name)
+        def value = properties.getProperty(name)
+
+        if (null == value) value = project.properties.getProperty(name)
+        if (null == value) value = System.properties.getProperty(name)
 
         log.debug("Get property: $name=$value")
 
@@ -64,14 +70,14 @@ class SetupCommand {
 
         log.debug("Require property: $name")
 
-        if (!project.properties.containsKey(name) && !System.properties.containsKey(name)) {
-            throw new Exception("Missing required property: $name")
+        if (!properties.containsKey(name) && !project.properties.containsKey(name) && !System.properties.containsKey(name)) {
+            throw new Exception("Missing required property: $name" as String)
         }
 
         def value = get(name)
 
         if (value == 'null') {
-            throw new Exception("Missing required property: $name (resolved to null)")
+            throw new Exception("Missing required property: $name (resolved to null)" as String)
         }
 
         return value
@@ -80,36 +86,37 @@ class SetupCommand {
     def execute() {
         def workDir = require('tomee.workdir')
         def webapp = require('tomee.webapp')
-        def tomcatVersion = require('tomcat.version')
+        String tomcatVersion = require('tomcat.version')
         System.setProperty('tomcat.version', tomcatVersion)
 
-        def tomeeVersion = require('tomee.version')
+        String tomeeVersion = require('tomee.version')
         System.setProperty('tomee.version', tomeeVersion)
 
-        def openejbVersion = require('openejb.version')
+        String openejbVersion = require('openejb.version')
         System.setProperty('openejb.version', openejbVersion)
         def localRepo = require('localRepository')
 
         def proxyHost = get('http.proxy.host', '')
-        def proxyPort = get('http.proxy.port', '')
+        String proxyPort = get('http.proxy.port', '')
         def proxyUsername = get('http.proxy.username', '')
         def proxyPassword = get('http.proxy.password', '')
         def proxyNonProxyHosts = get('http.proxy.nonProxyHosts', '')
 
-        if ((proxyHost != null && proxyHost.length() > 0) || (proxyPort != null && proxyPort.length() > 0)) {
+        if ((!'${settings.activeProxy.host}'.equals(proxyHost) && proxyHost != null && proxyHost.length() > 0)
+                || (!'${settings.activeProxy.port}'.equals(proxyPort) && proxyPort != null && proxyPort.length() > 0)) {
             log.info("Setting proxy host=${proxyHost} and proxy port=${proxyPort}")
 
             def setProxy = new SetProxy();
-            setProxy.setProxyHost(proxyHost)
-            setProxy.setProxyPort(Integer.parseInt(proxyPort))
-            setProxy.setProxyUser(proxyUsername)
-            setProxy.setProxyPassword(proxyPassword)
-            setProxy.setNonProxyHosts(proxyNonProxyHosts)
+            setProxy.setProxyHost(proxyHost as String)
+            setProxy.setProxyPort(Integer.parseInt(proxyPort) as int)
+            setProxy.setProxyUser(proxyUsername as String)
+            setProxy.setProxyPassword(proxyPassword as String)
+            setProxy.setNonProxyHosts(proxyNonProxyHosts as String)
             setProxy.execute()
         }
 
-        def dest = "${workDir}/apache-tomcat-${tomcatVersion}.zip"
-        def catalinaHome = "${workDir}/apache-tomcat-${tomcatVersion}"
+        def dest = "${workDir}/apache-tomcat-${tomcatVersion}.zip" as String
+        def catalinaHome = "${workDir}/apache-tomcat-${tomcatVersion}" as String
 
         log.info("extracting ${catalinaHome}")
         ant.unzip(src: dest, dest: "${workDir}")
@@ -120,9 +127,9 @@ class SetupCommand {
 
         log.info("Installing to: ${catalinaHome}")
 
-        System.setProperty("catalina.home", "${catalinaHome}")
-        System.setProperty("catalina.base", "${catalinaHome}")
-        Paths paths = new Paths(new File("${catalinaHome}/webapps/tomee"))
+        System.setProperty('catalina.home', "${catalinaHome}" as String)
+        System.setProperty('catalina.base', "${catalinaHome}" as String)
+        Paths paths = new Paths(new File("${catalinaHome}/webapps/tomee" as String))
         Installer installer = new Installer(paths, true)
         installer.installFull()
 
@@ -131,7 +138,8 @@ class SetupCommand {
         ant.delete(file: paths.getJAXBImpl())
         ant.delete(file: paths.getOpenEJBTomcatLoaderJar())
         ant.delete(file: paths.findTomEELibJar("jaxb-impl"))
-        ant.delete(file: paths.findTomEELibJar("openejb-javaagent-${openejbVersion}.jar")) // we need the one without version
+        ant.delete(file: paths.findTomEELibJar("openejb-javaagent-${openejbVersion}.jar" as String))
+        // we need the one without version
 
         ant.delete(file: "${paths.catalinaLibDir}/static-tomee-jquery-${tomeeVersion}.jar")
         ant.delete(file: "${paths.catalinaLibDir}/static-tomee-bootstrap-${tomeeVersion}.jar")
