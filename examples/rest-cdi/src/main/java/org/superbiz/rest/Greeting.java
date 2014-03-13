0b22c6cd0ee54ab54ee0d7248ab83f82ca22bbbe
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-03-13 15:52:49 +0000

    A REST with CDI example
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1577214 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/rest-cdi/src/main/java/org/superbiz/rest/Greeting.java
@@ -0,0 +1,21 @@
+/**
+ * Tomitribe Confidential
+ *
+ * Copyright(c) Tomitribe Corporation. 2014
+ *
+ * The source code for this program is not published or otherwise divested
+ * of its trade secrets, irrespective of what has been deposited with the
+ * U.S. Copyright Office.
+ *
+ * Author: agumbrecht@tomitribe.com
+ * Date: 13.03.14
+ * Time: 16:39
+ */
+package org.superbiz.rest;
+
+public class Greeting {
+
+    public String doSomething(final String value) {
+        return value.toLowerCase();
+    }
+}
