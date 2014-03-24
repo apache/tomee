1b9778fc8b9f9edcca16005999977728eda5d59f
Thiago Veronezi <tveronezi@apache.org>
2014-03-24 15:23:31 +0000

    https://issues.apache.org/jira/browse/TOMEE-245 Ok, it builds and installs a debian package for Apache TomEE Plus. We still need to...  * hack it to create debian packages for all TomEE versions  * improve the init scripts  * meet the debian policy [https://www.debian.org/doc/debian-policy/]
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1580883 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tomee/tomee-deb/src/main/resources/init/tomee.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CATALINA_HOME=/opt/tomee
+TOMCAT_USER=apachetomee
+
+RETVAL=0
+start(){
+   echo "Starting TomEE: "
+   su - $TOMCAT_USER -c "$CATALINA_HOME/bin/startup.sh"
+   RETVAL=$?
+   return $RETVAL
+}
+
+stop(){
+   echo "Shutting down TomEE: "
+   su - $TOMCAT_USER -c "$CATALINA_HOME/bin/shutdown.sh"
+   RETVAL=$?
+   return $RETVAL
+}
+
+case "$1" in
+   start)
+      start
+      ;;
+   stop)
+      stop
+      ;;
+   force-reload)
+      stop
+      start
+      ;;
+   *)
+      echo $"Usage: $0 {start|stop|force-reload}"
+      exit 1
+      ;;
+esac
+exit $?
