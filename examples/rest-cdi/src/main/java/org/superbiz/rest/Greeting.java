ce2defa33cf186fa7f815230cc4703cdcf152bbe
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-04-22 13:40:44 +0000

    Add missing ASF headers
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1589126 13f79535-47bb-0310-9956-ffa450edef68

--- examples/rest-cdi/src/main/java/org/superbiz/rest/Greeting.java
+++ examples/rest-cdi/src/main/java/org/superbiz/rest/Greeting.java
@@ -1,15 +1,18 @@
 /**
- * Tomitribe Confidential
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Copyright(c) Tomitribe Corporation. 2014
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- *
- * Author: agumbrecht@tomitribe.com
- * Date: 13.03.14
- * Time: 16:39
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.superbiz.rest;
 
