e1ac3b7d800e467985bdba34caeb2be93222ed1c
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-04-29 11:12:56 +0000

    Description
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1590951 13f79535-47bb-0310-9956-ffa450edef68

--- examples/async-methods/src/test/java/org/superbiz/async/JobProcessorTest.java
+++ examples/async-methods/src/test/java/org/superbiz/async/JobProcessorTest.java
@@ -56,8 +56,10 @@ public class JobProcessorTest extends TestCase {
         final long total = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start);
 
         // Execution should be around 9 - 21 seconds
-        assertTrue("" + total, total > 9);
-        assertTrue("" + total, total < 21);
+		// The execution time depends on the number of threads available for asynchronous execution.
+		// In the best case it is 10s plus some minimal processing time. 
+        assertTrue("Expected > 9 but was: " + total, total > 9);
+        assertTrue("Expected < 21 but was: " + total, total < 21);
 
     }
 
