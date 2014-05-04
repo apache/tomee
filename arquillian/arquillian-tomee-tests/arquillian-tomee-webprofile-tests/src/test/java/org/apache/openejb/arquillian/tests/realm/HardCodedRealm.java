eb2e5dd4d1a04cd37062f041ef91fb204877c8c1
Romain Manni-Bucau <rmannibucau@apache.org>
2014-05-04 13:05:37 +0000

    TOMEE-1191 TOMEE-1192 Fixing lifecycle handling in LazyRealm + allowing to log on against a particular webapp
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1592342 13f79535-47bb-0310-9956-ffa450edef68

---
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/realm/HardCodedRealm.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.arquillian.tests.realm;
+
+import org.apache.catalina.realm.GenericPrincipal;
+import org.apache.catalina.realm.RealmBase;
+import org.apache.http.auth.BasicUserPrincipal;
+
+import java.security.Principal;
+
+import static java.util.Arrays.asList;
+
+public class HardCodedRealm extends RealmBase
+{
+    @Override
+    protected String getName()
+    {
+        return "hard-coded";
+    }
+
+    @Override
+    protected String getPassword(final String username)
+    {
+        return "tom".equals(username)? "ee" : null;
+    }
+
+    @Override
+    protected Principal getPrincipal(final String username)
+    {
+        return new GenericPrincipal(username, "ee", asList("role    "));
+    }
+}
