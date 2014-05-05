29a1870f62526ccd046c82d3e5ed4b8ad484e9b7
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-05-05 09:15:27 +0000

    Version. TomEE project cleanup
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1592469 13f79535-47bb-0310-9956-ffa450edef68

--- tomee/apache-tomee/src/main/resources/tomee.sh
+++ tomee/apache-tomee/src/main/resources/tomee.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version="${openejb.version}"
+version="${version.openejb}"
 port=8080
 
 DEBUG=
