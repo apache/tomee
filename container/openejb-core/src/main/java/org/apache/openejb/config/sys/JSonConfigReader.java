4a5bcbff5c3bf2176995f8072156b5175c5ab56a
Thiago Veronezi <tveronezi@apache.org>
2014-05-15 17:10:15 +0000

    checkstyle for the apache headers
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1594983 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/config/sys/JSonConfigReader.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/sys/JSonConfigReader.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.openejb.config.sys;
 
 import org.apache.openejb.OpenEJBException;
--- container/openejb-core/src/main/java/org/apache/openejb/core/security/SecurityServiceImpl.java
+++ container/openejb-core/src/main/java/org/apache/openejb/core/security/SecurityServiceImpl.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.openejb.core.security;
 
 import org.apache.openejb.core.security.jaas.UsernamePasswordCallbackHandler;
