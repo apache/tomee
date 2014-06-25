9bc5b953e8919d94d54ebc9dc8e8f69e3cb8b37c
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-06-25 13:26:45 +0000

    Example that may help resolve: http://tomee-openejb.979440.n4.nabble.com/tomee-redeployment-issue-tp4670067p4670176.html
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1605388 13f79535-47bb-0310-9956-ffa450edef68

--- examples/bval-evaluation-redeployment/runner/src/test/java/RedeploymentTest.java
+++ examples/bval-evaluation-redeployment/runner/src/test/java/RedeploymentTest.java
@@ -5,11 +5,14 @@
  */
 
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.jboss.arquillian.container.test.api.Deployer;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,26 +25,42 @@ public class RedeploymentTest {
     public RedeploymentTest() {
     }
 
-    @Deployment(name = "webapp1")
+    @Deployment(name = "webapp1", managed = false)
     public static Archive<?> webapp1() {
         return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../WebApp1/target/WebApp1-1.1.0-SNAPSHOT.war"));
     }
 
-    @Deployment(name = "webapp2")
+    @Deployment(name = "webapp2", managed = false)
     public static Archive<?> webapp2() {
         return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../WebApp2/target/WebApp2-1.1.0-SNAPSHOT.war"));
     }
 
+    @ArquillianResource
+    private Deployer deployer;
+
     @Test
-    public void valid() throws Exception {
+    public void validateTest() throws Exception {
+
         final String port = System.getProperty("server.http.port");
-        System.out.println("Running test on port: " + port);
+        System.out.println("");
         System.out.println("===========================================");
-        System.out.println(WebClient.create("http://localhost:" + port + "/WebApp1/test/")
-            .type(MediaType.APPLICATION_JSON_TYPE).post("valid").getStatus());
-        System.out.println("-------------------------------------------");
-        System.out.println(WebClient.create("http://localhost:" + port + "/WebApp2/test/")
-            .type(MediaType.APPLICATION_JSON_TYPE).post("valid").getStatus());
+        System.out.println("Running test on port: " + port);
+
+        deployer.deploy("webapp1");
+        int result = WebClient.create("http://localhost:" + port + "/WebApp1/test/")
+            .type(MediaType.APPLICATION_JSON_TYPE).post("validd").getStatus();
+        System.out.println(result);
+        Assert.assertEquals(406, result);
+
+        //Not interested in webapp2 output
+        deployer.undeploy("webapp2");
+        deployer.deploy("webapp2");
+
+        result = WebClient.create("http://localhost:" + port + "/WebApp1/test/")
+            .type(MediaType.APPLICATION_JSON_TYPE).post("validd").getStatus();
+        System.out.println(result);
+        Assert.assertEquals(406, result);
+        deployer.undeploy("webapp2");
         System.out.println("===========================================");
         System.out.println("");
     }
