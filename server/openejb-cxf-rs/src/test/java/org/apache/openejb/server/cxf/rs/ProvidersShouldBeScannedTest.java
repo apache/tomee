3a02a8750d9253c89c8824e3a08199625773b173
Romain Manni-Bucau <rmannibucau@apache.org>
2014-07-07 20:52:57 +0000

    jira is down :( - if @Path is on an interface try to get the implemenation (if unique) and scanning by default @Provider like suggested in the spec
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1608611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/openejb-cxf-rs/src/test/java/org/apache/openejb/server/cxf/rs/ProvidersShouldBeScannedTest.java
@@ -0,0 +1,69 @@
+/*
+ *     Licensed to the Apache Software Foundation (ASF) under one or more
+ *     contributor license agreements.  See the NOTICE file distributed with
+ *     this work for additional information regarding copyright ownership.
+ *     The ASF licenses this file to You under the Apache License, Version 2.0
+ *     (the "License"); you may not use this file except in compliance with
+ *     the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package org.apache.openejb.server.cxf.rs;
+
+import org.apache.openejb.jee.WebApp;
+import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.loader.IO;
+import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.EnableServices;
+import org.apache.openejb.testing.Module;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@EnableServices("jax-rs")
+@RunWith(ApplicationComposer.class)
+public class ProvidersShouldBeScannedTest {
+    @Module
+    @Classes({ NeedAProvider.class, DontLetResourcesFail.class })
+    public static WebApp service() throws Exception {
+        return new WebApp().contextRoot("app");
+    }
+
+    @Test
+    public void check() throws Exception {
+        final HttpURLConnection conn = HttpURLConnection.class.cast(new URL("http://127.0.0.1:4204/app/need-provider").openConnection());
+        assertEquals("ok", IO.slurp(conn.getInputStream()));
+        conn.getInputStream().close();
+    }
+
+    @Path("need-provider")
+    public static class NeedAProvider {
+        @GET
+        public String providers() {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Provider
+    public static class DontLetResourcesFail implements ExceptionMapper<IllegalArgumentException> {
+        @Override
+        public Response toResponse(final IllegalArgumentException throwable) {
+            return Response.ok("ok").build();
+        }
+    }
+}
