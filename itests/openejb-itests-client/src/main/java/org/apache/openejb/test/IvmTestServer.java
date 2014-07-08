cedee02569fb7c660012556b0b484693d7e02eef
Romain Manni-Bucau <rmannibucau@apache.org>
2014-07-08 18:59:24 +0000

    mvcc was not a good choice
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1608899 13f79535-47bb-0310-9956-ffa450edef68

--- itests/openejb-itests-client/src/main/java/org/apache/openejb/test/IvmTestServer.java
+++ itests/openejb-itests-client/src/main/java/org/apache/openejb/test/IvmTestServer.java
@@ -36,9 +36,6 @@ public class IvmTestServer implements TestServer {
         try{
             props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.core.LocalInitialContextFactory");
 
-            props.put("My DataSource", "new://Resource?type=DataSource");
-            props.put("My DataSource.JdbcUrl", "jdbc:hsqldb:mem:IvmTestServer;hsqldb.tx=MVCC");
-
             Properties p = new Properties();
             p.putAll(props);
             p.put("openejb.loader", "embed");
