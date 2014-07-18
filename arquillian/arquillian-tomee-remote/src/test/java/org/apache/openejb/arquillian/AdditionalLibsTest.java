5104926d9eee379f245cfd17530e5b662a57d006
Romain Manni-Bucau <rmannibucau@apache.org>
2014-07-18 17:08:41 +0000

    TOMEE-1274 additionalLibs for tomee remote arquillian adapter
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1611717 13f79535-47bb-0310-9956-ffa450edef68

---
+++ arquillian/arquillian-tomee-remote/src/test/java/org/apache/openejb/arquillian/AdditionalLibsTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openejb.arquillian;
+
+import org.apache.sirona.repositories.Repository;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class AdditionalLibsTest {
+    @Deployment
+    public static Archive<?> war() {
+        return ShrinkWrap.create(WebArchive.class, "foo.war")
+            .addClass(Tester.class);
+    }
+
+    @Test
+    public void run() {
+        final File[] sirona = new File(System.getProperty("catalina.base") + "/lib").listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(final File dir, final String name) {
+                return name.startsWith("sirona-");
+            }
+        });
+        assertEquals(3, sirona.length); // core, web, aop
+
+        assertNotNull(Repository.INSTANCE); // would fail if libs are missing
+    }
+
+    private static class Tester {
+    }
+}
