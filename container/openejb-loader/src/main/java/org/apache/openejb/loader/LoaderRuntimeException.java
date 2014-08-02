4ab0ec959d1299accf1d23eb08af9bd1e3946aae
Thiago Veronezi <tveronezi@apache.org>
2014-08-02 14:22:54 +0000

    -No jacoco. We will use https://analysis.apache.org/ -Removing some unused code -Fixing some old TODOs
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1615317 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-loader/src/main/java/org/apache/openejb/loader/LoaderRuntimeException.java
+++ container/openejb-loader/src/main/java/org/apache/openejb/loader/LoaderRuntimeException.java
@@ -17,10 +17,6 @@
 package org.apache.openejb.loader;
 
 public class LoaderRuntimeException extends RuntimeException {
-    public LoaderRuntimeException(final String str) {
-        super(str);
-    }
-
     public LoaderRuntimeException(final String str, final Throwable e) {
         super(str, e);
     }
