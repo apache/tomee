4d5642efd8b175b221a74f1b4eb846387560c194
Romain Manni-Bucau <rmannibucau@apache.org>
2014-08-05 22:12:27 +0000

    TOMEE-1285 @Provider can be CDI beans now
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1616022 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/openejb-cxf-rs/src/test/java/org/apache/openejb/server/cxf/rs/CDIProviderTest.java
@@ -0,0 +1,119 @@
+/*
+ *     Licensed to the Apache Software Foundation (ASF) under one or more
+ *     contributor license agreements.  See the NOTICE file distributed with
+ *     this work for additional information regarding copyright ownership.
+ *     The ASF licenses this file to You under the Apache License, Version 2.0
+ *     (the "License"); you may not use this file except in compliance with
+ *     the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package org.apache.openejb.server.cxf.rs;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.openejb.jee.WebApp;
+import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.Configuration;
+import org.apache.openejb.testing.EnableServices;
+import org.apache.openejb.testing.Module;
+import org.apache.openejb.testng.PropertiesBuilder;
+import org.apache.openejb.util.NetworkUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+@EnableServices("jax-rs")
+@RunWith(ApplicationComposer.class)
+public class CDIProviderTest {
+
+    private static int port = -1;
+
+    @BeforeClass
+    public static void beforeClass() {
+        port = NetworkUtil.getNextAvailablePort();
+    }
+
+    @Configuration
+    public Properties props() {
+        return new PropertiesBuilder().p("httpejbd.port", Integer.toString(port)).build();
+    }
+
+    @Module
+    @Classes(cdi = true, value = {Helper.class, MyPro.class, Res.class})
+    public WebApp war() {
+        return new WebApp()
+            .contextRoot("foo");
+    }
+
+    @Test
+    public void isCdi() {
+        assertEquals("Oh Yeah!", WebClient.create("http://localhost:" + port + "/foo").accept("provider/type").path("res").get(String.class));
+    }
+
+    @Path("res")
+    public static class Res {
+        @GET
+        @Produces("provider/type")
+        public String f() {
+            return "failed";
+        }
+    }
+
+    @Provider
+    @Produces("provider/type")
+    public static class MyPro implements MessageBodyWriter<String> {
+        @Inject
+        private Helper helper;
+
+        @Override
+        public boolean isWriteable(final Class<?> type, final Type genericType,
+                                   final Annotation[] annotations, final MediaType mediaType) {
+            return true;
+        }
+
+        @Override
+        public long getSize(final String s, final Class<?> type, final Type genericType,
+                            final Annotation[] annotations, final MediaType mediaType) {
+            return -1;
+        }
+
+        @Override
+        public void writeTo(final String s, final Class<?> type, final Type genericType,
+                            final Annotation[] annotations,
+                            final MediaType mediaType,
+                            final MultivaluedMap<String, Object> httpHeaders,
+                            final OutputStream entityStream) throws IOException, WebApplicationException {
+            entityStream.write(helper.data().getBytes());
+        }
+    }
+
+    public static class Helper {
+        String data() {
+            return "Oh Yeah!";
+        }
+    }
+}
