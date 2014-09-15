c307548764042f3ccdebb49c46c7149d637b27db
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-09-15 18:04:20 +0000

    PMD
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/branches/tomee-1.7.x@1625100 13f79535-47bb-0310-9956-ffa450edef68

--- container/openejb-core/src/main/java/org/apache/openejb/util/PropertyPlaceHolderHelper.java
+++ container/openejb-core/src/main/java/org/apache/openejb/util/PropertyPlaceHolderHelper.java
@@ -34,6 +34,7 @@ public final class PropertyPlaceHolderHelper {
 
     private static final PropertiesLookup RESOLVER = new PropertiesLookup();
     public static final StrSubstitutor SUBSTITUTOR = new StrSubstitutor(RESOLVER);
+
     static {
         SUBSTITUTOR.setEnableSubstitutionInVariables(true);
         SUBSTITUTOR.setValueDelimiter(System.getProperty("openejb.placehodler.delimiter", ":-")); // default one of [lang3]
@@ -65,10 +66,10 @@ public final class PropertyPlaceHolderHelper {
         return decryptIfNeeded(value.replace(PREFIX, "").replace(SUFFIX, ""));
     }
 
-    private static String decryptIfNeeded(String replace) {
+    private static String decryptIfNeeded(final String replace) {
         if (replace.startsWith(CIPHER_PREFIX)) {
             final String algo = replace.substring(CIPHER_PREFIX.length(), replace.indexOf(':', CIPHER_PREFIX.length() + 1));
-            PasswordCipher cipher = null;
+            PasswordCipher cipher;
             try {
                 cipher = PasswordCipherFactory.getPasswordCipher(algo);
             } catch (final PasswordCipherException ex) {
