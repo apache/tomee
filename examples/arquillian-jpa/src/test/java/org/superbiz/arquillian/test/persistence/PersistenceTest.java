3af7408104ab0466dbdbf8602fb77a0072f0c48f
Romain Manni-Bucau <rmannibucau@apache.org>
2014-10-01 16:33:18 +0000

    TOMEE-1369 arquillian-persistence sample
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1628758 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/arquillian-jpa/src/test/java/org/superbiz/arquillian/test/persistence/PersistenceTest.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.superbiz.arquillian.test.persistence;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.persistence.ShouldMatchDataSet;
+import org.jboss.arquillian.persistence.UsingDataSet;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.superbiz.arquillian.persistence.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class PersistenceTest
+{
+    @Deployment
+    public static Archive<?> createDeploymentPackage()
+    {
+        return ShrinkWrap.create(WebArchive.class, "UserPersistenceTest.war")
+                .addPackage(User.class.getPackage())
+                .addAsManifestResource(new ClassLoaderAsset("META-INF/persistence.xml"), "persistence.xml");
+    }
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    @Transactional(TransactionMode.COMMIT)
+    @UsingDataSet("datasets/users.yml")
+    @ShouldMatchDataSet("datasets/expected-users.yml")
+    public void seriouslyYouAlreadyForgotOpenEJB_questionMark() throws Exception
+    {
+        assertEquals(2, em.createQuery("select count(e) from User e",Number.class).getSingleResult().intValue());
+
+        final User user = em.find(User.class, 2L);
+        assertNotNull(user);
+
+        user.setName("OpenEJB"); // @Transactional(TransactionMode.COMMIT) will commit it and datasets/expected-users.yml will check it
+    }
+}
