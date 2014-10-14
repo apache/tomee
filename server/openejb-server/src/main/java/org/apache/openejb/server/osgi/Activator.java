ab3b1bb610622bd50c7abc86143d4a0ce0f9900b
Andrew Gumbrecht <andygumbrecht@apache.org>
2014-10-14 10:52:01 +0000

    Remove *.exe filter (now global) Do not use version in module
    git-svn-id: https://svn.apache.org/repos/asf/tomee/tomee/trunk@1631714 13f79535-47bb-0310-9956-ffa450edef68

--- server/openejb-server/src/main/java/org/apache/openejb/server/osgi/Activator.java
+++ server/openejb-server/src/main/java/org/apache/openejb/server/osgi/Activator.java
@@ -29,12 +29,12 @@ public class Activator implements BundleActivator {
     private ServiceManagerExtender manager;
     private ServiceRegistration registration;
 
-    public void start(BundleContext bundleContext) throws Exception {
+    public void start(final BundleContext bundleContext) throws Exception {
         manager = new ServiceManagerExtender(bundleContext);
         registration = bundleContext.registerService(ServiceManager.class.getName(), manager, null);
     }
 
-    public void stop(BundleContext bundleContext) throws Exception {
+    public void stop(final BundleContext bundleContext) throws Exception {
         if (manager != null) {
             manager.shutdown();
         }
