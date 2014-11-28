5cb361943d54446f680345e90ab3c3623f4a3c98
Romain Manni-Bucau <rmannibucau@apache.org>
2014-11-28 17:24:03 +0100

    service as well should use UnsetPropertiesRecipe

--- container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/Assembler.java
+++ container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/Assembler.java
@@ -2163,6 +2163,7 @@ public class Assembler extends AssemblerTool implements org.apache.openejb.spi.A
 
     public void createService(final ServiceInfo serviceInfo) throws OpenEJBException {
         final ObjectRecipe serviceRecipe = createRecipe(serviceInfo);
+        serviceRecipe.setProperty("properties", new UnsetPropertiesRecipe());
 
         final Object service = serviceRecipe.create();
         SystemInstance.get().addObserver(service);
