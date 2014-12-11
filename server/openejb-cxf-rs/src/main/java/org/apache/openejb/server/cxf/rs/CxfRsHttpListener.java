ffd7be2c9cb7291093dce88074604cae4a55a57c
Romain Manni-Bucau <rmannibucau@apache.org>
2014-12-11 09:42:04 +0100

    respecting jaxrs singleton (excepted for normalscoped beans)

--- server/openejb-cxf-rs/src/main/java/org/apache/openejb/server/cxf/rs/CxfRsHttpListener.java
+++ server/openejb-cxf-rs/src/main/java/org/apache/openejb/server/cxf/rs/CxfRsHttpListener.java
@@ -140,6 +140,7 @@ public class CxfRsHttpListener implements RsHttpListener {
     private final Collection<Pattern> staticResourcesList = new CopyOnWriteArrayList<Pattern>();
     private final List<ObjectName> jmxNames = new ArrayList<ObjectName>();
     private final Collection<CreationalContext<?>> toRelease = new LinkedHashSet<CreationalContext<?>>();
+    private final Collection<CdiSingletonResourceProvider> singletons = new LinkedHashSet<CdiSingletonResourceProvider>();
 
     private static final char[] URL_SEP = new char[] { '?', '#', ';' };
 
@@ -466,6 +467,13 @@ public class CxfRsHttpListener implements RsHttpListener {
                 LOGGER.warning(e.getMessage(), e);
             }
         }
+        for (final CdiSingletonResourceProvider provider : singletons) {
+            try {
+                provider.release();
+            } catch (final Exception e) {
+                LOGGER.warning(e.getMessage(), e);
+            }
+        }
 
         final ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(CxfUtil.initBusLoader());
@@ -502,33 +510,38 @@ public class CxfRsHttpListener implements RsHttpListener {
             for (final Class<?> clazz : application.getClasses()) {
                 if (!additionalProviders.contains(clazz) && !clazz.isInterface()) {
                     classes.add(clazz);
+
+                    final EJBRestServiceInfo restServiceInfo = getEjbRestServiceInfo(restEjbs, clazz);
+
+                    if (restServiceInfo != null) {
+                        final Object proxy = ProxyEJB.subclassProxy(restServiceInfo.context);
+                        factory.setResourceProvider(clazz, new NoopResourceProvider(restServiceInfo.context.getBeanClass(), proxy));
+                    } else {
+                        factory.setResourceProvider(clazz, new OpenEJBPerRequestPojoResourceProvider(
+                                classLoader, clazz, injections, context, owbCtx));
+                    }
                 }
             }
 
             for (final Object o : application.getSingletons()) {
                 if (!additionalProviders.contains(o)) {
-                    final Class<?> clazz = o.getClass();
+                    final Class<?> clazz = realClass(o.getClass());
                     classes.add(clazz);
-                }
-            }
 
-            for (final Class<?> clazz : classes) {
-                String name = clazz.getName();
-                EJBRestServiceInfo restServiceInfo = restEjbs.get(name);
+                    final EJBRestServiceInfo restServiceInfo = getEjbRestServiceInfo(restEjbs, clazz);
 
-                if (name.endsWith(DynamicSubclass.IMPL_SUFFIX)) {
-                    name = name.substring(0, name.length() - DynamicSubclass.IMPL_SUFFIX.length());
-                    restServiceInfo = restEjbs.get(name);
-                    if (restServiceInfo != null) { // AutoJAXRSInvoker relies on it
-                        restEjbs.put(clazz.getName(), restServiceInfo);
+                    if (restServiceInfo != null) {
+                        final Object proxy = ProxyEJB.subclassProxy(restServiceInfo.context);
+                        factory.setResourceProvider(clazz, new NoopResourceProvider(restServiceInfo.context.getBeanClass(), proxy));
+                    } else {
+                        if (owbCtx.getBeanManagerImpl().isInUse()) {
+                            final CdiSingletonResourceProvider provider = new CdiSingletonResourceProvider(classLoader, clazz, o, injections, context, owbCtx);
+                            singletons.add(provider);
+                            factory.setResourceProvider(clazz, provider);
+                        } else {
+                            factory.setResourceProvider(clazz, new SingletonResourceProvider(o));
+                        }
                     }
-                }
-
-                if (restServiceInfo != null) {
-                    final Object proxy = ProxyEJB.subclassProxy(restServiceInfo.context);
-                    factory.setResourceProvider(clazz, new NoopResourceProvider(restServiceInfo.context.getBeanClass(), proxy));
-                } else {
-                    factory.setResourceProvider(clazz, new OpenEJBPerRequestPojoResourceProvider(classLoader, clazz, injections, context, owbCtx));
                 }
             }
 
@@ -580,6 +593,31 @@ public class CxfRsHttpListener implements RsHttpListener {
                 CxfUtil.clearBusLoader(oldLoader);
             }
         }
+    }
+
+    private EJBRestServiceInfo getEjbRestServiceInfo(Map<String, EJBRestServiceInfo> restEjbs, Class<?> clazz) {
+        String name = clazz.getName();
+        EJBRestServiceInfo restServiceInfo = restEjbs.get(name);
+
+        if (name.endsWith(DynamicSubclass.IMPL_SUFFIX)) {
+            name = name.substring(0, name.length() - DynamicSubclass.IMPL_SUFFIX.length());
+            restServiceInfo = restEjbs.get(name);
+            if (restServiceInfo != null) { // AutoJAXRSInvoker relies on it
+                restEjbs.put(clazz.getName(), restServiceInfo);
+            }
+        }
+        return restServiceInfo;
+    }
+
+    private static Class<?> realClass(final Class<?> aClass) {
+        Class<?> result = aClass;
+        while (result.getName().contains("$$")) {
+            result = result.getSuperclass();
+            if (result == null) {
+                return aClass;
+            }
+        }
+        return result;
     }
 
     private static String createEndpointName(final Application application) {
---
+++ server/openejb-cxf-rs/src/test/java/org/apache/openejb/server/cxf/rs/SingletonTest.java
@@ -0,0 +1,111 @@
+/*
+ *     Licensed to the Apache Software Foundation (ASF) under one or more
+ *     contributor license agreements.  See the NOTICE file distributed with
+ *     this work for additional information regarding copyright ownership.
+ *     The ASF licenses this file to You under the Apache License, Version 2.0
+ *     (the "License"); you may not use this file except in compliance with
+ *     the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package org.apache.openejb.server.cxf.rs;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.openejb.jee.WebApp;
+import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.EnableServices;
+import org.apache.openejb.testing.Module;
+import org.apache.openejb.testing.RandomPort;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+@EnableServices("jax-rs")
+@RunWith(ApplicationComposer.class)
+public class SingletonTest {
+    @RandomPort("httpejbd")
+    private URL base;
+
+    @Module
+    @Classes(cdi = true, innerClassesAsBean = true)
+    public WebApp war() {
+        return new WebApp()
+            .contextRoot("foo")
+            .addServlet("REST Application", Application.class.getName())
+            .addInitParam("REST Application", "javax.ws.rs.Application", MyCdiRESTApplication.class.getName());
+    }
+
+    @Test
+    public void checkStateStays() {
+        for (int i = 0; i < 3; i++) {
+            assertEquals(i, WebClient.create(base.toExternalForm() + "foo/foo").get(Integer.class).intValue());
+        }
+    }
+
+    @Test
+    public void checkCdiInjections() {
+        for (int i = 0; i < 3; i++) {
+            assertEquals(i, WebClient.create(base.toExternalForm() + "foo/cdi").get(Integer.class).intValue());
+        }
+    }
+
+    @Path("foo")
+    public static class TheResource {
+        private int i;
+
+        @GET
+        public int get() {
+            return i++;
+        }
+    }
+
+    @Path("cdi")
+    public static class TheCdiResource {
+        @Inject
+        private Incr incr;
+        private int destroy;
+
+        @GET
+        public int get() {
+            return incr.get() + destroy;
+        }
+
+        @PreDestroy
+        public void incr() {
+            destroy++;
+        }
+    }
+
+    public static class Incr {
+        private int i;
+
+        public int get() {
+            return i++;
+        }
+    }
+
+    public static class MyCdiRESTApplication extends Application {
+        @Override
+        public Set<Object> getSingletons() {
+            return new HashSet<Object>(asList(new TheResource(), new TheCdiResource()));
+        }
+    }
+}
