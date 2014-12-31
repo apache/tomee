61222b9349aeee7b54496a1301044c30c0884ff1
Romain Manni-Bucau <rmannibucau@apache.org>
2014-12-31 08:23:12 +0100

    unused import

--- container/openejb-core/src/main/java/org/apache/openejb/testing/ApplicationComposers.java
+++ container/openejb-core/src/main/java/org/apache/openejb/testing/ApplicationComposers.java
@@ -111,7 +111,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
