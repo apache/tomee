43581f4899f454910fa583b7c8d099d5297aa5e0
andygumbrecht@apache.org <andy.is@gmx.de>
2015-06-16 09:58:17 +0200

    Licenses & rat.xml

--- container/openejb-core/src/main/java/org/apache/openejb/util/OutputScanner.java
+++ container/openejb-core/src/main/java/org/apache/openejb/util/OutputScanner.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.openejb.util;
 
 import org.codehaus.swizzle.stream.ScanBuffer;
--- container/openejb-jee/src/main/java/org/apache/openejb/jee/Scan.java
+++ container/openejb-jee/src/main/java/org/apache/openejb/jee/Scan.java
@@ -1,11 +1,19 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
-// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2014.08.10 à 01:54:19 PM CEST 
-//
-
-
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.openejb.jee;
 
 import java.util.ArrayList;
