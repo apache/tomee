757688e9101627a8af8dcc33eed482792907f872
AndyGee <andy.is@gmx.de>
2015-07-03 14:50:06 +0200

    Set Identity timeout
    Conflicts:
    	container/openejb-core/src/main/java/org/apache/openejb/core/security/AbstractSecurityService.java

--- container/openejb-core/src/main/java/org/apache/openejb/core/security/AbstractSecurityService.java
+++ container/openejb-core/src/main/java/org/apache/openejb/core/security/AbstractSecurityService.java
@@ -225,10 +225,13 @@ public abstract class AbstractSecurityService implements SecurityService<UUID>, 
         }
     }
 
-    protected UUID registerSubject(final Subject subject) {
+    protected UUID registerSubject(final Subject subject, final long accessTimeout) {
         final Identity identity = new Identity(subject);
+        identity.setTimeout(accessTimeout);
+
         final UUID token = identity.getToken();
         identities.put(token, identity);
+
         return token;
     }
 
@@ -261,6 +264,7 @@ public abstract class AbstractSecurityService implements SecurityService<UUID>, 
             throw new LoginException("Identity is not currently logged in: " + securityIdentity);
         }
 
+        identity.access();
         clientIdentity.set(identity);
     }
 
