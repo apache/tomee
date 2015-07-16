e9d42565a1d148fdb8be0f116796b0fc928d4e84
Romain Manni-Bucau <rmannibucau@apache.org>
2015-07-16 14:23:47 +0200

    style

--- container/openejb-core/src/main/java/org/apache/openejb/resource/jdbc/managed/local/ManagedConnection.java
+++ container/openejb-core/src/main/java/org/apache/openejb/resource/jdbc/managed/local/ManagedConnection.java
@@ -247,7 +247,7 @@ public class ManagedConnection implements InvocationHandler {
         }
     }
 
-    private static class Key {
+    private static final class Key {
         private final CommonDataSource ds;
         private final String user;
         private final String pwd;
