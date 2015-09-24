c6b2044a42381ce74b1ebeec99b0508e7c310e85
andygumbrecht@apache.org <andy.is@gmx.de>
2015-09-24 21:29:37 +0200

    Comment out unused test

--- examples/deltaspike-fullstack/src/test/java/org/superbiz/deltaspike/test/MockedPageBeanTest.java
+++ examples/deltaspike-fullstack/src/test/java/org/superbiz/deltaspike/test/MockedPageBeanTest.java
@@ -18,14 +18,12 @@
  */
 package org.superbiz.deltaspike.test;
 
+import org.apache.deltaspike.cdise.api.ContextControl;
 import org.apache.deltaspike.core.spi.scope.window.WindowContext;
-import org.apache.deltaspike.data.api.AbstractEntityRepository;
-import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.apache.deltaspike.testcontrol.api.mock.DynamicMockManager;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.superbiz.deltaspike.WebappMessageBundle;
 import org.superbiz.deltaspike.domain.User;
 import org.superbiz.deltaspike.repository.UserRepository;
@@ -34,14 +32,13 @@ import org.superbiz.deltaspike.view.config.Pages;
 
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-@RunWith(CdiTestRunner.class)
-public class MockedPageBeanTest
-{
+//@RunWith(CdiTestRunner.class)
+public class MockedPageBeanTest {
+
     @Inject
     private RegistrationPage registrationPage; //will use a the mocked UserRepository
 
@@ -57,22 +54,24 @@ public class MockedPageBeanTest
     @Inject
     private UserRepository userRepository; //will inject the mocked instance
 
+    @Inject
+    private ContextControl contextControl;
+
+    @Ignore("Does no work because DS cannot mock dynamic repositories")
     @Test
-    @Ignore("doesn't work cause DS can't mock dynamic reositories")
-    public void saveUserWithMockedBean()
-    {
+    public void saveUserWithMockedBean() {
         final String userName = "gp";
         final String firstName = "Gerhard";
         final String lastName = "Petracek";
 
         // mockito doesn't support interfaces...seriously? but you can mock CDI impl
         // here we don't have one so implementing for the test the interface
-        UserRepository mockedUserRepository = (UserRepository) Proxy.newProxyInstance(
+        final UserRepository mockedUserRepository = (UserRepository) Proxy.newProxyInstance(
                 Thread.currentThread().getContextClassLoader(),
-                new Class<?>[]{ UserRepository.class},
+                new Class<?>[]{UserRepository.class},
                 new InvocationHandler() {
                     @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
                         return new User(userName, firstName, lastName.toUpperCase() /*just to illustrate that the mock-instance is used*/);
                     }
                 });
@@ -86,13 +85,13 @@ public class MockedPageBeanTest
         this.registrationPage.getUser().setLastName(lastName);
         this.registrationPage.getUser().setPassword("123");
 
-        Class<? extends Pages> targetPage = this.registrationPage.register();
+        final Class<? extends Pages> targetPage = this.registrationPage.register();
 
         Assert.assertEquals(Pages.Login.class, targetPage);
         Assert.assertFalse(FacesContext.getCurrentInstance().getMessageList().isEmpty());
         Assert.assertEquals(webappMessageBundle.msgUserRegistered(userName), FacesContext.getCurrentInstance().getMessageList().iterator().next().getSummary());
 
-        User user = this.userRepository.findByUserName(userName);
+        final User user = this.userRepository.findByUserName(userName);
         Assert.assertNotNull(user);
         Assert.assertEquals(firstName, user.getFirstName());
         Assert.assertEquals(lastName.toUpperCase(), user.getLastName());
