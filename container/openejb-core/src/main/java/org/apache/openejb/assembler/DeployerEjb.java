539244bc19e6bc1ccd474c8ce0aef0ea780d68ce
Romain manni-Bucau <rmannibucau@gmail.com>
2016-02-09 18:51:57 +0100

    useless import

--- container/openejb-core/src/main/java/org/apache/openejb/assembler/DeployerEjb.java
+++ container/openejb-core/src/main/java/org/apache/openejb/assembler/DeployerEjb.java
@@ -25,7 +25,6 @@ import org.apache.openejb.UndeployException;
 import org.apache.openejb.assembler.classic.AppInfo;
 import org.apache.openejb.assembler.classic.Assembler;
 import org.apache.openejb.assembler.classic.DeploymentExceptionManager;
-import org.apache.openejb.assembler.classic.WebAppInfo;
 import org.apache.openejb.config.AppModule;
 import org.apache.openejb.config.ConfigurationFactory;
 import org.apache.openejb.config.DeploymentLoader;
