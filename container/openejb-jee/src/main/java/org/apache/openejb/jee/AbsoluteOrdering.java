bf5183121efadf17bf1f074439f341e1ddc04d8e
Tod Bookless <tbookless@tomitribe.com>
2016-07-03 15:06:15 -0700

    Removed to unused imports. Also reduced access to package local as suggested for two items.

--- container/openejb-jee/src/main/java/org/apache/openejb/jee/AbsoluteOrdering.java
+++ container/openejb-jee/src/main/java/org/apache/openejb/jee/AbsoluteOrdering.java
@@ -18,8 +18,6 @@ package org.apache.openejb.jee;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlType;
@@ -46,18 +44,15 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "absoluteOrderingType", propOrder = {
     "nameOrOthers"
 })
-public class AbsoluteOrdering {
+class AbsoluteOrdering {
 
     @XmlElements({
         @XmlElement(name = "name", type = String.class),
         @XmlElement(name = "others", type = OrderingOthers.class)
     })
-    protected List<Object> nameOrOthers;
+    List<Object> nameOrOthers = new ArrayList<>();
 
     public List<Object> getNameOrOthers() {
-        if (nameOrOthers == null) {
-            nameOrOthers = new ArrayList<Object>();
-        }
         return this.nameOrOthers;
     }
 
