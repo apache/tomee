3998036fdb70c72ce9fdc5cf057866850b618b31
Romain manni-Bucau <rmannibucau@gmail.com>
2016-07-04 21:26:26 +0200

    TOMEE-1858 enhancing tomee.bat

--- tomee/apache-tomee/src/main/resources/tomee.bat
+++ tomee/apache-tomee/src/main/resources/tomee.bat
@@ -1,18 +1,14 @@
 @echo off
-rem Licensed to the Apache Software Foundation (ASF) under one or more
-rem contributor license agreements.  See the NOTICE file distributed with
-rem this work for additional information regarding copyright ownership.
-rem The ASF licenses this file to You under the Apache License, Version 2.0
-rem (the "License"); you may not use this file except in compliance with
-rem the License.  You may obtain a copy of the License at
 rem
-rem     http://www.apache.org/licenses/LICENSE-2.0
+rem Tomitribe Confidential
 rem
-rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS,
-rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem See the License for the specific language governing permissions and
-rem limitations under the License.
+rem Copyright Tomitribe Corporation. 2015
+rem
+rem The source code for this program is not published or otherwise divested
+rem of its trade secrets, irrespective of what has been deposited with the
+rem U.S. Copyright Office.
+rem
+
 
 if "%OS%" == "Windows_NT" setlocal
 
@@ -58,24 +54,23 @@ set "CATALINA_TMPDIR=%CATALINA_BASE%\temp"
 
 
 rem create classpath
-setlocal enabledelayedexpansion
+set "CLASSPATH=%CATALINA_HOME%\lib\*"
 
-set "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\tomcat-juli.jar"
-set lib="%CATALINA_HOME%\lib\"
-echo %lib%
-for %%F in (%lib%/*.jar) do (
-  set CLASSPATH=!CLASSPATH!;%%F%
-)
-
+set DEBUG=
+set "args=%*"
 if ""%1"" == ""deploy"" goto doDeploy
 if ""%1"" == ""undeploy"" goto doUndeploy
 if ""%1"" == ""start"" goto unsupportedCmd
 if ""%1"" == ""stop"" goto unsupportedCmd
+if not ""%1"" == ""debug"" goto doExec
+set DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
+set "args=%*"
+set "args=%args:* =%"
 goto doExec
 
 :doDeploy
 :doUndeploy
-"%_RUNJAVA%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" org.apache.openejb.cli.Bootstrap %1 -s http://localhost:%port%/tomee/ejb %2
+%_RUNJAVA% -Djava.io.tmpdir="%CATALINA_TMPDIR%" org.apache.openejb.cli.Bootstrap %1 -s http://localhost:%port%/tomee/ejb %2
 goto end
 
 :unsupportedCmd
@@ -83,7 +78,7 @@ echo start/stop commands are not compatible with tomee.bat, please use catalina.
 goto end
 
 :doExec
-"%_RUNJAVA%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" org.apache.openejb.cli.Bootstrap %*
+%_RUNJAVA% %DEBUG% "-Dopenejb.base=%CATALINA_BASE%" "-Dopenejb.home=%CATALINA_HOME%" "-Djava.io.tmpdir=%CATALINA_TMPDIR%" org.apache.openejb.cli.Bootstrap %args%
 goto end
 
 :end
