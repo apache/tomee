1adec5221c960b9447eb7d8d8666d234efffe225
Romain manni-Bucau <rmannibucau@gmail.com>
2016-07-16 20:04:50 +0200

    TOMEE-1870 avoid to scan when scan.xml gives all the info we need

--- container/openejb-core/src/main/java/org/apache/openejb/config/WebappAggregatedArchive.java
+++ container/openejb-core/src/main/java/org/apache/openejb/config/WebappAggregatedArchive.java
@@ -18,6 +18,7 @@
 package org.apache.openejb.config;
 
 import org.apache.xbean.finder.archive.Archive;
+import org.apache.xbean.finder.archive.ClassesArchive;
 import org.apache.xbean.finder.archive.CompositeArchive;
 import org.apache.xbean.finder.archive.FilteredArchive;
 import org.apache.xbean.finder.filter.Filter;
@@ -26,18 +27,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Collections.singletonList;
+import static org.apache.openejb.loader.JarLocation.jarLocation;
+
 public class WebappAggregatedArchive implements Archive, ScanConstants {
     private final Map<URL, List<String>> map = new HashMap<URL, List<String>>();
     private ScanUtil.ScanHandler handler;
     private boolean scanXmlExists; // faster than using an empty handler
-    private final Archive archive;
+    private Archive archive;
 
     public WebappAggregatedArchive(final Module module, final Iterable<URL> urls, final Filter filter) {
         final List<Archive> archives = new ArrayList<Archive>();
@@ -47,6 +50,28 @@ public class WebappAggregatedArchive implements Archive, ScanConstants {
             try {
                 handler = ScanUtil.read(scanXml);
                 scanXmlExists = true;
+                if ((handler.getPackages() == null || handler.getPackages().isEmpty())
+                        && handler.getClasses() != null && !handler.getClasses().isEmpty()
+                        && handler.isOptimized()) { // only classes, skip scanning
+                    final Collection<Class<?>> loaded = new ArrayList<>(handler.getClasses().size());
+                    for (final String clazz : handler.getClasses()) {
+                        try {
+                            final Class<?> aClass = module.getClassLoader().loadClass(clazz);
+                            loaded.add(aClass);
+                            final URL jar = jarLocation(aClass).toURI().toURL();
+                            List<String> list = map.get(jar);
+                            if (list == null) {
+                                list = new ArrayList<>();
+                                map.put(jar, list);
+                            }
+                            list.add(clazz);
+                        } catch (final ClassNotFoundException e) {
+                            throw new IllegalArgumentException(e);
+                        }
+                    }
+                    archive = new ClassesArchive(loaded.toArray(new Class[loaded.size()]));
+                    return;
+                }
             } catch (final IOException e) {
                 // ignored, will not use filtering with scan.xml
             }
@@ -55,7 +80,8 @@ public class WebappAggregatedArchive implements Archive, ScanConstants {
         for (final URL url : urls) {
             final List<String> classes = new ArrayList<String>();
             final Archive archive = new FilteredArchive(
-                    new ConfigurableClasspathArchive(module.getClassLoader(), Arrays.asList(url)), new ScanXmlSaverFilter(scanXmlExists, handler, classes, filter));
+                    new ConfigurableClasspathArchive(module.getClassLoader(), singletonList(url)),
+                    new ScanXmlSaverFilter(scanXmlExists, handler, classes, filter));
             map.put(url, classes);
             archives.add(archive);
         }
