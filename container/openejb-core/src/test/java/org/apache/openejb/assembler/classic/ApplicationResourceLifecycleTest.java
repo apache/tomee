19b8af03fc6697db0a6c71d1d5b3f9c0d24150ee
Jonathan Gallimore <jon@jrg.me.uk>
2017-07-19 15:47:50 +0100

    Applying Romain's test format

--- container/openejb-core/src/test/java/org/apache/openejb/assembler/classic/ApplicationResourceLifecycleTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/assembler/classic/ApplicationResourceLifecycleTest.java
@@ -16,84 +16,69 @@
  */
 package org.apache.openejb.assembler.classic;
 
-import org.apache.openejb.OpenEJB;
-import org.apache.openejb.OpenEJBException;
-import org.apache.openejb.config.AppModule;
-import org.apache.openejb.config.ConfigurationFactory;
-import org.apache.openejb.config.EjbModule;
-import org.apache.openejb.core.LocalInitialContextFactory;
-import org.apache.openejb.jee.EjbJar;
-import org.apache.openejb.jee.SingletonBean;
+import org.apache.openejb.config.sys.Resources;
+import org.apache.openejb.testing.ApplicationComposers;
+import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.Module;
+import org.apache.openejb.testing.SimpleLog;
 import org.junit.Test;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.concurrent.atomic.AtomicBoolean;
+import javax.annotation.Resource;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@Classes
+@SimpleLog
 public class ApplicationResourceLifecycleTest {
+    @Resource(name = "test")
+    private MyResource resource;
 
-    private static final AtomicBoolean POST_CONSTRUCT = new AtomicBoolean(false);
-    private static final AtomicBoolean PRE_DESTROY = new AtomicBoolean(false);
-
-
-    @Test
-    public void test() throws OpenEJBException, NamingException, IOException {
-        final ConfigurationFactory config = new ConfigurationFactory();
-        final Assembler assembler = new Assembler();
-
-        assembler.createTransactionManager(config.configureService(TransactionServiceInfo.class));
-        assembler.createSecurityService(config.configureService(SecurityServiceInfo.class));
-
-        final AppModule app = new AppModule(ApplicationResourceLifecycleTest.class.getClassLoader(), ApplicationResourceLifecycleTest.class.getSimpleName());
-
-        final EjbJar ejbJar = new EjbJar();
-        ejbJar.addEnterpriseBean(new SingletonBean(ApplicationResource.class));
-        app.getEjbModules().add(new EjbModule(ejbJar));
-        app.getEjbModules().iterator().next().getAltDDs().put("resources.xml", getClass().getClassLoader().getResource("app-resources.xml"));
-
-        assembler.createApplication(config.configureApplication(app));
-
-        final Properties properties = new Properties();
-        properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, LocalInitialContextFactory.class.getName());
-        properties.setProperty("openejb.embedded.initialcontext.close", "destroy");
-
-        // some hack to be sure to call destroy()
-        InitialContext context = new InitialContext(properties);
-        assertNotNull(context);
-
-        assertTrue(POST_CONSTRUCT.getAndSet(false));
-        assertFalse(PRE_DESTROY.get());
-
-        ApplicationResource bean = (ApplicationResource) context.lookup("ApplicationResourceLocalBean");
-
-        assertNotNull(bean);
-        context.close();
-
-        OpenEJB.destroy();
-        assertFalse(POST_CONSTRUCT.get());
-        assertTrue(PRE_DESTROY.get());
+    @Module
+    public Resources resources() {
+        return new Resources() {{
+            getResource().add(new org.apache.openejb.config.sys.Resource() {{
+                setId("test");
+                setClassName(MyResource.class.getName());
+            }});
+        }};
     }
 
-    public static class ApplicationResource {
+    @Test
+    public void lifecycle() throws Exception {
+        new ApplicationComposers(this).evaluate(this, new Runnable() {
+            @Override
+            public void run() {
+                assertTrue(resource.init);
+                assertFalse(resource.destroy);
+            }
+        });
+        assertTrue(resource.init);
+        assertTrue(resource.destroy);
+    }
+
+    public static class MyResource {
+        private boolean init;
+        private boolean destroy;
 
         @PostConstruct
-        public void start() {
-            POST_CONSTRUCT.set(true);
+        private void init() {
+            init = true;
         }
 
         @PreDestroy
-        public void stop() {
-            PRE_DESTROY.set(true);
+        private void destroy() {
+            destroy = true;
         }
 
+        public boolean isInit() {
+            return init;
+        }
+
+        public boolean isDestroy() {
+            return destroy;
+        }
     }
 }
