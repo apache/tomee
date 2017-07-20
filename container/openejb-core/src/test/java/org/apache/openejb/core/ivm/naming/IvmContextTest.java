69631e68aa77e8400f63047daf333b20b5f48425
Svetlin Zarev <svetlin.zarev@sap.com>
2017-07-20 12:12:42 +0300

    Fix testGetBindingPropagatesReadOnlyFlag() test
    The same case as with testGetFromFederatedContextPropagatesReadOnlyFlag()

--- container/openejb-core/src/test/java/org/apache/openejb/core/ivm/naming/IvmContextTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/core/ivm/naming/IvmContextTest.java
@@ -304,7 +304,7 @@ public class IvmContextTest extends TestCase {
       */
      public void testGetBindingPropagatesReadOnlyFlag() throws NamingException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         final IvmContext context = new IvmContext("comp");
-        context.bind("comp/env/test", "test");
+        context.bind("env/test", "test");
 
         context.setReadOnly(true);
         Object result = context.lookup("env");
