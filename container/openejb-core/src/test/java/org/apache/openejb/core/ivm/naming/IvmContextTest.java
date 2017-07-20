7af9eb4ccbdf3f38eb2312c9c9b265a5b6b1edbf
Svetlin Zarev <svetlin.zarev@sap.com>
2017-07-20 12:10:20 +0300

    Fix the testGetFromFederatedContextPropagatesReadOnlyFlag() test
    The test was adjusted to the buggy behaviour so it can just pass,
    but instead of building teh desired test IvmContext (comp/env/test),
    it was actually building (comp/comp/env/test). The test was passing
    because doe to TOMEE-2102, teh IvmContext was creating two alternative
    NameNodes for "comp". So the lookup relative to comp was working,
    but if the same was attempted relative to the root node, it would have failed.

--- container/openejb-core/src/test/java/org/apache/openejb/core/ivm/naming/IvmContextTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/core/ivm/naming/IvmContextTest.java
@@ -324,7 +324,7 @@ public class IvmContextTest extends TestCase {
      public void testGetFromFederatedContextPropagatesReadOnlyFlag() throws NamingException {
         final IvmContext context = new IvmContext();
         final IvmContext federatedContext = new IvmContext("comp");
-        federatedContext.bind("comp/env/test", "test");
+        federatedContext.bind("env/test", "test");
         context.bind("", federatedContext);
 
         context.setReadOnly(true);
