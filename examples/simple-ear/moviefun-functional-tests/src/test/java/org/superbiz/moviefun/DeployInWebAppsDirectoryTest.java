e4e392650c0e080bf2a6fb9c333aef746131ab74
Jonathan Gallimore <jon@jrg.me.uk>
2017-11-06 12:58:05 +0000

    Manual deployment in webapps directory test

--- examples/simple-ear/moviefun-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
+++ examples/simple-ear/moviefun-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
@@ -16,10 +16,20 @@
  */
 package org.superbiz.moviefun;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.openejb.loader.IO;
 import org.apache.tomee.arquillian.remote.RemoteTomEEConfiguration;
 import org.apache.tomee.arquillian.remote.RemoteTomEEContainer;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import static org.junit.Assert.assertTrue;
 
 @Ignore("Not implemented yet")
 /*
@@ -42,16 +52,79 @@ public class DeployInWebAppsDirectoryTest {
         final RemoteTomEEContainer container = new RemoteTomEEContainer();
         container.setup(configuration);
 
-        // TODO: copy application in manually
+        try {
+            container.start();
 
-        container.start();
+            final File webapps = new File(configuration.getDir(), "apache-tomee-" + configuration.getClassifier() + "-" + configuration.getVersion() + "/webapps");
+            webapps.mkdirs();
 
+            final File enterpriseArchive = Maven.resolver().resolve("org.superbiz:moviefun-ear:ear:1.1.0-SNAPSHOT")
+                    .withoutTransitivity().asSingleFile();
 
-        container.stop();
+            IO.copy(enterpriseArchive, new File(webapps, "moviefun-ear.ear"));
+            final String appUrl = "http://localhost:" + configuration.getHttpPort() + "/moviefun";
 
+            runTests(appUrl);
 
+            container.stop();
+
+            // restart the container, make sure all is still well
+            container.start();
+
+        } finally {
+            container.stop();
+        }
     }
 
+    private void runTests(final String appUrl) throws IOException {
+        final WebClient webClient = new WebClient();
 
+        HtmlPage page = attempt(new Callable<HtmlPage>() {
 
+            @Override
+            public HtmlPage call() throws Exception {
+                return getPage(webClient, appUrl);
+            }
+        }, 30);
+
+        assertMoviesPresent(page);
+
+        page = webClient.getPage(appUrl + "/moviefun");
+
+        assertMoviesPresent(page);
+        webClient.closeAllWindows();
+    }
+
+    private <T> T attempt(final Callable<T> callable, int numberOfAttempts) {
+        int tries = 0;
+
+        while (tries < numberOfAttempts) {
+            try {
+                return callable.call();
+            } catch (final Exception e) {
+                // ignore the exception and try again
+                tries++;
+                try {
+                    Thread.sleep(1000);
+                } catch (final InterruptedException ie) {
+                    // ignore
+                }
+            }
+        }
+
+        throw new IllegalStateException("Unable to invoke callable successfully after " + numberOfAttempts + " attempts");
+    }
+
+    private HtmlPage getPage(final WebClient webClient, final String url) throws IOException {
+        return webClient.getPage(url + "/setup.jsp");
+    }
+
+    private void assertMoviesPresent(HtmlPage page) {
+        String pageAsText = page.asText();
+        assertTrue(pageAsText.contains("Wedding Crashers"));
+        assertTrue(pageAsText.contains("Starsky & Hutch"));
+        assertTrue(pageAsText.contains("Shanghai Knights"));
+        assertTrue(pageAsText.contains("I-Spy"));
+        assertTrue(pageAsText.contains("The Royal Tenenbaums"));
+    }
 }
