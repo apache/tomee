18a446b28661d8743f9267f3a3ab89e87fec108c
Jonathan Gallimore <jon@jrg.me.uk>
2017-11-16 21:49:42 +0000

    WIP

---
+++ examples/connector-war/src/main/java/org/superbiz/connector/application/Receiver.java
@@ -0,0 +1,24 @@
+package org.superbiz.connector.application;
+import org.superbiz.connector.api.InboundListener;
+
+import javax.ejb.MessageDriven;
+
+
+import javax.annotation.Resource;
+import javax.ejb.MessageDrivenContext;
+import javax.inject.Inject;
+
+@MessageDriven(name = "Receiver")
+public class Receiver implements InboundListener {
+
+    @Inject
+    private MessagesReceived messagesReceived;
+
+    @Resource
+    private MessageDrivenContext context;
+    
+    @Override
+    public void receiveMessage(String message) {
+        System.out.println(message);
+    }
+}
