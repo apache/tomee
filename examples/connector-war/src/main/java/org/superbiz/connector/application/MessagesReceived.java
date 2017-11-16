edfcb5f46717930810f132272b2201c92e26c933
Jonathan Gallimore <jon@jrg.me.uk>
2017-11-16 21:49:42 +0000

    WIP

---
+++ examples/connector-war/src/main/java/org/superbiz/connector/application/MessagesReceived.java
@@ -0,0 +1,19 @@
+package org.superbiz.connector.application;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class MessagesReceived {
+
+    private final List<String> messagesReceived = new ArrayList<String>();
+
+    public List<String> getMessagesReceived() {
+        return messagesReceived;
+    }
+
+    public void messageReceived(final String message) {
+        messagesReceived.add(message);
+    }
+}
