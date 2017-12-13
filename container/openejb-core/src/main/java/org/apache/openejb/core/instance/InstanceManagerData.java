6e34269abf79b293f4ccbc375513119058319433
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-13 09:41:24 -0300

    fixes instance data creator
    (cherry picked from commit bcec8ce3c11c461c0f283a15058cac2251a23b9f)

--- container/openejb-core/src/main/java/org/apache/openejb/core/instance/InstanceManagerData.java
+++ container/openejb-core/src/main/java/org/apache/openejb/core/instance/InstanceManagerData.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-class InstanceManagerData {
+public class InstanceManagerData {
 
     private final Pool<InstanceManager.Instance> pool;
     private final Duration accessTimeout;
