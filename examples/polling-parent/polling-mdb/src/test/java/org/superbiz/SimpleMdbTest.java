bba479de24764751cde2ea593dc52747354cfe5e
Thiago Veronezi <thiago@veronezi.org>
2018-01-05 11:09:11 -0500

    add classes instead of package -> test

--- examples/polling-parent/polling-mdb/src/test/java/org/superbiz/SimpleMdbTest.java
+++ examples/polling-parent/polling-mdb/src/test/java/org/superbiz/SimpleMdbTest.java
@@ -18,6 +18,7 @@ package org.superbiz;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
