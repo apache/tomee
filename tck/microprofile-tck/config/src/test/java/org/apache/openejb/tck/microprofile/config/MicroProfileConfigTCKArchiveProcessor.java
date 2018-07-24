745de4785ac1f2793c999ce3481cec9468474929
Roberto Cortez <radcortez@yahoo.com>
2018-07-24 21:04:23 +0100

    Fixed MP Config.

--- tck/microprofile-tck/config/src/test/java/org/apache/openejb/tck/microprofile/config/MicroProfileConfigTCKArchiveProcessor.java
+++ tck/microprofile-tck/config/src/test/java/org/apache/openejb/tck/microprofile/config/MicroProfileConfigTCKArchiveProcessor.java
@@ -1,7 +1,5 @@
 package org.apache.openejb.tck.microprofile.config;
 
-import static org.apache.openejb.loader.JarLocation.jarLocation;
-
 import org.eclipse.microprofile.config.tck.converters.UpperCaseDuckConverter;
 import org.hamcrest.object.HasToString;
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
@@ -11,11 +9,9 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
-import java.io.File;
+import static org.apache.openejb.loader.JarLocation.jarLocation;
 
 public class MicroProfileConfigTCKArchiveProcessor implements ApplicationArchiveProcessor {
-    private File hamcrest;
-
     @Override
     public void process(final Archive<?> archive, final TestClass testClass) {
         if (archive instanceof WebArchive) {
@@ -24,15 +20,10 @@ public class MicroProfileConfigTCKArchiveProcessor implements ApplicationArchive
             // TODO - this could be fixed in the TCK by adding UpperCaseDuckConverter into org.eclipse.microprofile.config.tck.ConverterTest
             JavaArchive configJar = ShrinkWrap
                     .create(JavaArchive.class, "config-tck-additional.jar")
-                    .addClass(UpperCaseDuckConverter.class)
-                    ;
+                    .addClass(UpperCaseDuckConverter.class);
 
             war.addAsLibraries(configJar);
-
-            if (hamcrest == null) {
-                hamcrest = jarLocation(HasToString.class);
-            }
-            war.addAsLibrary(hamcrest);
+            war.addAsLibrary(jarLocation(HasToString.class));
         }
     }
 }
