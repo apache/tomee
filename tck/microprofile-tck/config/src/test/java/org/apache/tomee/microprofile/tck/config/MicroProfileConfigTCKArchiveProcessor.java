2bb880c8205220c86639c03e7c7e94a5f335e8de
Roberto Cortez <radcortez@yahoo.com>
2018-08-16 20:43:27 +0100

    Fixed MP Config tests.

--- tck/microprofile-tck/config/src/test/java/org/apache/tomee/microprofile/tck/config/MicroProfileConfigTCKArchiveProcessor.java
+++ tck/microprofile-tck/config/src/test/java/org/apache/tomee/microprofile/tck/config/MicroProfileConfigTCKArchiveProcessor.java
@@ -1,11 +1,13 @@
 package org.apache.tomee.microprofile.tck.config;
 
+import org.eclipse.microprofile.config.tck.converters.UpperCaseDuckConverter;
 import org.hamcrest.object.HasToString;
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.test.spi.TestClass;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
 
 import static org.apache.openejb.loader.JarLocation.jarLocation;
 
@@ -14,7 +16,12 @@ public class MicroProfileConfigTCKArchiveProcessor implements ApplicationArchive
     public void process(final Archive<?> archive, final TestClass testClass) {
         if (archive instanceof WebArchive) {
             final WebArchive war = WebArchive.class.cast(archive);
-            war.addAsLibrary(jarLocation(Assert.class));
+
+            // TODO - this could be fixed in the TCK by adding UpperCaseDuckConverter into org.eclipse.microprofile.config.tck.ConverterTest
+            final JavaArchive configJar = ShrinkWrap.create(JavaArchive.class, "config-tck-additional.jar")
+                                                    .addClass(UpperCaseDuckConverter.class);
+
+            war.addAsLibraries(configJar);
             war.addAsLibrary(jarLocation(HasToString.class));
         }
     }
