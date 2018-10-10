384c9ed4f8c555c7915290349e6eb68428aeb04e
Jonathan Gallimore <jon@jrg.me.uk>
2018-10-10 15:33:19 +0100

    Fix merge conflict

--- container/openejb-core/src/main/java/org/apache/openejb/resource/AutoConnectionTracker.java
+++ container/openejb-core/src/main/java/org/apache/openejb/resource/AutoConnectionTracker.java
@@ -43,6 +43,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
