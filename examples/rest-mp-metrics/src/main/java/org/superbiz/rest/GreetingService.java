017b98e89ac68e4d5b695e06a16c7a7e43561d20
ivanjunckes <ijunckes@tomitribe.com>
2018-11-19 17:42:38 -0200

    adding test to microprofile metrics

--- examples/rest-mp-metrics/src/main/java/org/superbiz/rest/GreetingService.java
+++ examples/rest-mp-metrics/src/main/java/org/superbiz/rest/GreetingService.java
@@ -2,6 +2,7 @@ package org.superbiz.rest;
 
 import org.eclipse.microprofile.metrics.annotation.Counted;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,10 +12,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/greeting")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
 public class GreetingService {
 
-    @Counted(monotonic = true, name = "count_messages")
+    @Counted(monotonic = true, name = "message_counter", absolute = true)
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String message() {
         return "Hi Microprofile Metrics!";
     }
