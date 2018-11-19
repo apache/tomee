ec927200f92d5c61ea96668d96b16af1de3c3db4
brunobat <brunobat@gmail.com>
2018-11-19 10:21:53 +0000

    TOMEE-2278 - add alternative for the safeguard FailsafeExecutionManagerProvider bean. Reorganize project to into 2 libs
    Signed-off-by: brunobat <brunobat@gmail.com>

--- tomee/tomee-microprofile/tomee-microprofile-common/src/main/java/org/apache/tomee/microprofile/faulttolerance/FailsafeContainerExecutionManagerProvider.java
+++ tomee/tomee-microprofile/tomee-microprofile-common/src/main/java/org/apache/tomee/microprofile/faulttolerance/FailsafeContainerExecutionManagerProvider.java
@@ -1,12 +1,19 @@
 package org.apache.tomee.microprofile.faulttolerance;
-/*
- * Tomitribe Confidential
- *
- * Copyright Tomitribe Corporation. 2018
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 
@@ -20,26 +27,27 @@ import org.apache.safeguard.impl.executionPlans.ExecutionPlanFactory;
 import org.apache.safeguard.impl.executorService.DefaultExecutorServiceProvider;
 import org.apache.safeguard.impl.retry.FailsafeRetryManager;
 
+import javax.annotation.Priority;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Specializes;
-import javax.naming.InitialContext;
+import javax.interceptor.Interceptor;
 import javax.ws.rs.Produces;
-import java.util.concurrent.ScheduledExecutorService;
 
-@Specializes
 @Alternative
+@Priority(Interceptor.Priority.APPLICATION+10)
 @ApplicationScoped
 public class FailsafeContainerExecutionManagerProvider extends FailsafeExecutionManagerProvider {
+
+    @Resource(name = "DefaultManagedScheduledExecutorService")
+    ManagedScheduledExecutorService executor;
 
     @Produces
     @ApplicationScoped
     @Override
-    public ExecutionManager createExecutionManager() throws Exception {
-
-        ScheduledExecutorService executor = new InitialContext().doLookup(
-                System.getProperty("apache.safeguard.executorservice.location",
-                        "java:comp/DefaultManagedScheduledExecutorService"));
+    public ExecutionManager createExecutionManager() {
 
         final MicroprofileAnnotationMapper mapper = MicroprofileAnnotationMapper.getInstance();
         final DefaultExecutorServiceProvider executorServiceProvider = new DefaultExecutorServiceProvider(executor);
