533afa8cfa3f8c60d20683603be8b0033a5852e6
ivanjunckes <ijunckes@tomitribe.com>
2018-11-23 12:02:52 -0200

    Adding jax-rs resource

---
+++ examples/mp-metrics-timed/src/main/java/WeatherService.java
@@ -0,0 +1,25 @@
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/weather")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
+public class WeatherService {
+
+    @Path("/day/status")
+    @Timed(name = "weather_day_timed", absolute = true,
+            displayName = "Weather Day Timed",
+            description = "This metric shows the weather status of the day.")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String dayStatus() {
+        return "Hi, today is a sunny day!";
+    }
+}
