cf9d96ea1fba93ecd2decae10893d816372933b2
Jonathan Gallimore <jgallimore@tomitribe.com>
2018-12-05 10:57:42 +0000

    TOMEE-2295 working sample

--- arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/cmp/sample/MoviesBusinessBean.java
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-webprofile-tests/src/test/java/org/apache/openejb/arquillian/tests/cmp/sample/MoviesBusinessBean.java
@@ -1,48 +1,80 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.apache.openejb.arquillian.tests.cmp.sample;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import javax.ejb.CreateException;
 import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
-import java.rmi.RemoteException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 public class MoviesBusinessBean implements SessionBean {
+    public void doLogic() {
+        try {
+            Context initial = new InitialContext();
 
-    private SessionContext ctx;
+            LocalActorHome actorHome = (LocalActorHome) initial.lookup("java:comp/env/ejb/Actor");
+            Context initial1 = new InitialContext();
 
-    @Override
-    public void ejbActivate() throws EJBException, RemoteException {
+            LocalMovieHome movieHome = (LocalMovieHome) initial1.lookup("java:comp/env/ejb/Movie");
+
+            final LocalMovie movie = movieHome.create("Bad Boys", "Action Comedy");
+
+            final LocalActor actor1 = actorHome.create("Will Smith");
+            final LocalActor actor2 = actorHome.create("Martin Lawrence");
+
+            movie.addActor(actor1);
+            movie.addActor(actor2);
+        } catch (Exception ex) {
+            throw new EJBException(ex.getMessage());
+        }
     }
 
-    @Override
-    public void ejbPassivate() throws EJBException, RemoteException {
+    public void ejbCreate() throws CreateException {
     }
 
-    @Override
-    public void ejbRemove() throws EJBException, RemoteException {
+    public void ejbActivate() {
     }
 
-    @Override
-    public void setSessionContext(final SessionContext ctx) throws EJBException, RemoteException {
-        this.ctx = ctx;
+    public void ejbPassivate() {
     }
 
-    public void addActor(final String name) throws MovieException {
-        // this is literally a no-op now
+    public void ejbRemove() {
     }
 
+    public void setSessionContext(SessionContext sc) {
+    }
+
+    private ArrayList copyActorsToDetails(Collection actors) {
+        ArrayList detailsList = new ArrayList();
+        Iterator i = actors.iterator();
+
+        while (i.hasNext()) {
+            LocalActor player = (LocalActor) i.next();
+            ActorDetails details =
+                new ActorDetails(player.getActorId(), player.getName());
+
+            detailsList.add(details);
+        }
+
+        return detailsList;
+    }
 }
