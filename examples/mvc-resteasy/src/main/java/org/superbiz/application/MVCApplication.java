b2a656e8cd4da444ece331740f23af6c1826c815
Daniel Dias <daniel.dias.analistati@gmail.com>
2018-12-15 01:57:41 -0200

    add exemple mvc-resteasy-hello

--- examples/mvc-resteasy/src/main/java/org/superbiz/application/MVCApplication.java
+++
@@ -1,6 +0,0 @@
-package org.superbiz.application;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("app")
-public class MVCApplication extends Application { }
