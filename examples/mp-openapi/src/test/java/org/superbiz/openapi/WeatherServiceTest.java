9b725830b1780e47f75e15283be80063257b4fce
Puneeth PS <puneeth.ps@gmail.com>
2019-01-02 22:28:32 +0530

    formatted code.

--- examples/mp-openapi/src/test/java/org/superbiz/openapi/WeatherServiceTest.java
+++ examples/mp-openapi/src/test/java/org/superbiz/openapi/WeatherServiceTest.java
@@ -56,153 +56,148 @@ public class WeatherServiceTest {
         this.client.close();
     }
 
-    
+
     @Test
     public void dayStatus() {
         WebTarget webTarget = this.client.target(this.base.toExternalForm());
-        final Response message =  webTarget.path("/openapi")
+        final Response message = webTarget.path("/openapi")
                 .request()
                 .accept(MediaType.APPLICATION_JSON)
                 .get();
         final String metaData = message.readEntity(String.class);
         JsonObject jsonObject = Json.createReader(new StringReader(metaData)).readObject();
-        JsonObject response=jsonObject.get("paths").asJsonObject().get("/weather/status/{day}").asJsonObject();
+        JsonObject response = jsonObject.get("paths").asJsonObject().get("/weather/status/{day}").asJsonObject();
         final String expected = "{\r\n" + 
-        		"            \"get\": {\r\n" + 
-        		"                \"deprecated\": false,\r\n" + 
-        		"                \"description\": \"Describes how the day will be.\",\r\n" + 
-        		"                \"operationId\": \"dayStatus\",\r\n" + 
-        		"                \"parameters\": [\r\n" + 
-        		"                    {\r\n" + 
-        		"                        \"allowEmptyValue\": false,\r\n" + 
-        		"                        \"allowReserved\": false,\r\n" + 
-        		"                        \"description\": \"The day for which the weather needs to be fetched.\",\r\n" + 
-        		"                        \"in\": \"path\",\r\n" + 
-        		"                        \"name\": \"\",\r\n" + 
-        		"                        \"required\": true,\r\n" + 
-        		"                        \"schema\": {\r\n" + 
-        		"                            \"type\": \"string\"\r\n" + 
-        		"                        }\r\n" + 
-        		"                    }\r\n" + 
-        		"                ],\r\n" + 
-        		"                \"responses\": {\r\n" + 
-        		"                    \"400\": {\r\n" + 
-        		"                        \"content\": {\r\n" + 
-        		"                            \"200\": {}\r\n" + 
-        		"                        },\r\n" + 
-        		"                        \"description\": \"Weather for this day not found\"\r\n" + 
-        		"                    }\r\n" + 
-        		"                },\r\n" + 
-        		"                \"summary\": \"Finds weather for day specified in the URL \"\r\n" + 
-        		"            }\r\n" + 
-        		"        }";
+                "            \"get\": {\r\n" + 
+                "                \"deprecated\": false,\r\n" + 
+                "                \"description\": \"Describes how the day will be.\",\r\n" + 
+                "                \"operationId\": \"dayStatus\",\r\n" + 
+                "                \"parameters\": [\r\n" + 
+                "                    {\r\n" + 
+                "                        \"allowEmptyValue\": false,\r\n" + 
+                "                        \"allowReserved\": false,\r\n" + 
+                "                        \"description\": \"The day for which the weather needs to be fetched.\",\r\n" + 
+                "                        \"in\": \"path\",\r\n" + 
+                "                        \"name\": \"\",\r\n" + 
+                "                        \"required\": true,\r\n" + 
+                "                        \"schema\": {\r\n" + 
+                "                            \"type\": \"string\"\r\n" + 
+                "                        }\r\n" + 
+                "                    }\r\n" + 
+                "                ],\r\n" + 
+                "                \"responses\": {\r\n" + 
+                "                    \"400\": {\r\n" + 
+                "                        \"content\": {\r\n" + 
+                "                            \"200\": {}\r\n" + 
+                "                        },\r\n" + 
+                "                        \"description\": \"Weather for this day not found\"\r\n" + 
+                "                    }\r\n" + 
+                "                },\r\n" + 
+                "                \"summary\": \"Finds weather for day specified in the URL \"\r\n" + 
+                "            }\r\n" + 
+                "        }";
         JsonObject expectedJson = Json.createReader(new StringReader(expected)).readObject();
         assertEquals(expectedJson.keySet().size(), response.keySet().size());
         String[] expectedKeys = new String[]{"deprecated", "description", "operationId", "parameters", "responses", "summary"};
         Stream.of(expectedKeys).forEach((text) -> {
-          assertTrue("Expected: " + text
-                  + " to be present in " + expected,
-                  expectedJson.getJsonObject("get").get(text) != null);
+            assertTrue("Expected: " + text
+                    + " to be present in " + expected,
+                    expectedJson.getJsonObject("get").get(text) != null);
         });
-        
 
     }
-    
+
     @Test
     public void detailedDayStatus() {
         WebTarget webTarget = this.client.target(this.base.toExternalForm());
-        final Response message =  webTarget.path("/openapi")
+        final Response message = webTarget.path("/openapi")
                 .request()
                 .accept(MediaType.APPLICATION_JSON)
                 .get();
         final String metaData = message.readEntity(String.class);
         JsonObject jsonObject = Json.createReader(new StringReader(metaData)).readObject();
-        JsonObject response=jsonObject.get("paths").asJsonObject().get("/weather/detailedWeather/{day}").asJsonObject();
+        JsonObject response = jsonObject.get("paths").asJsonObject().get("/weather/detailedWeather/{day}").asJsonObject();
         final String expected = "{\r\n" + 
-        		"            \"get\": {\r\n" + 
-        		"                \"operationId\": \"getDetailedWeather\",\r\n" + 
-        		"                \"parameters\": [\r\n" + 
-        		"                    {\r\n" + 
-        		"                        \"allowEmptyValue\": false,\r\n" + 
-        		"                        \"allowReserved\": false,\r\n" + 
-        		"                        \"description\": \"The day for which the detailed weather needs to be fetched.\",\r\n" + 
-        		"                        \"in\": \"path\",\r\n" + 
-        		"                        \"name\": \"\",\r\n" + 
-        		"                        \"required\": true,\r\n" + 
-        		"                        \"schema\": {\r\n" + 
-        		"                            \"type\": \"string\"\r\n" + 
-        		"                        }\r\n" + 
-        		"                    }\r\n" + 
-        		"                ],\r\n" + 
-        		"                \"responses\": {\r\n" + 
-        		"                    \"default\": {\r\n" + 
-        		"                        \"content\": {\r\n" + 
-        		"                            \"application/json\": {\r\n" + 
-        		"                                \"schema\": {\r\n" + 
-        		"                                    \"deprecated\": false,\r\n" + 
-        		"                                    \"description\": \"POJO that represents weather.\",\r\n" + 
-        		"                                    \"exclusiveMaximum\": false,\r\n" + 
-        		"                                    \"exclusiveMinimum\": false,\r\n" + 
-        		"                                    \"items\": {},\r\n" + 
-        		"                                    \"maxLength\": 2147483647,\r\n" + 
-        		"                                    \"minLength\": 0,\r\n" + 
-        		"                                    \"nullable\": false,\r\n" + 
-        		"                                    \"properties\": {\r\n" + 
-        		"                                        \"humidityPercentage\": {\r\n" + 
-        		"                                            \"deprecated\": false,\r\n" + 
-        		"                                            \"example\": \"4\",\r\n" + 
-        		"                                            \"exclusiveMaximum\": false,\r\n" + 
-        		"                                            \"exclusiveMinimum\": false,\r\n" + 
-        		"                                            \"maxLength\": 2147483647,\r\n" + 
-        		"                                            \"minLength\": 0,\r\n" + 
-        		"                                            \"nullable\": false,\r\n" + 
-        		"                                            \"readOnly\": false,\r\n" + 
-        		"                                            \"type\": \"string\",\r\n" + 
-        		"                                            \"uniqueItems\": false,\r\n" + 
-        		"                                            \"writeOnly\": false\r\n" + 
-        		"                                        },\r\n" + 
-        		"                                        \"temperatureInCelsius\": {\r\n" + 
-        		"                                            \"deprecated\": false,\r\n" + 
-        		"                                            \"example\": \"27\",\r\n" + 
-        		"                                            \"exclusiveMaximum\": false,\r\n" + 
-        		"                                            \"exclusiveMinimum\": false,\r\n" + 
-        		"                                            \"maxLength\": 2147483647,\r\n" + 
-        		"                                            \"minLength\": 0,\r\n" + 
-        		"                                            \"nullable\": false,\r\n" + 
-        		"                                            \"readOnly\": false,\r\n" + 
-        		"                                            \"type\": \"string\",\r\n" + 
-        		"                                            \"uniqueItems\": false,\r\n" + 
-        		"                                            \"writeOnly\": false\r\n" + 
-        		"                                        }\r\n" + 
-        		"                                    },\r\n" + 
-        		"                                    \"readOnly\": false,\r\n" + 
-        		"                                    \"required\": [\r\n" + 
-        		"                                        \"temperatureInCelsius\",\r\n" + 
-        		"                                        \"humidityPercentage\"\r\n" + 
-        		"                                    ],\r\n" + 
-        		"                                    \"uniqueItems\": false,\r\n" + 
-        		"                                    \"writeOnly\": false\r\n" + 
-        		"                                }\r\n" + 
-        		"                            }\r\n" + 
-        		"                        },\r\n" + 
-        		"                        \"description\": \"Detailed Weather\"\r\n" + 
-        		"                    }\r\n" + 
-        		"                }\r\n" + 
-        		"            }\r\n" + 
-        		"        }";
+                "            \"get\": {\r\n" + 
+                "                \"operationId\": \"getDetailedWeather\",\r\n" + 
+                "                \"parameters\": [\r\n" + 
+                "                    {\r\n" + 
+                "                        \"allowEmptyValue\": false,\r\n" + 
+                "                        \"allowReserved\": false,\r\n" + 
+                "                        \"description\": \"The day for which the detailed weather needs to be fetched.\",\r\n" + 
+                "                        \"in\": \"path\",\r\n" + 
+                "                        \"name\": \"\",\r\n" + 
+                "                        \"required\": true,\r\n" + 
+                "                        \"schema\": {\r\n" + 
+                "                            \"type\": \"string\"\r\n" + 
+                "                        }\r\n" + 
+                "                    }\r\n" + 
+                "                ],\r\n" + 
+                "                \"responses\": {\r\n" + 
+                "                    \"default\": {\r\n" + 
+                "                        \"content\": {\r\n" + 
+                "                            \"application/json\": {\r\n" + 
+                "                                \"schema\": {\r\n" + 
+                "                                    \"deprecated\": false,\r\n" + 
+                "                                    \"description\": \"POJO that represents weather.\",\r\n" + 
+                "                                    \"exclusiveMaximum\": false,\r\n" + 
+                "                                    \"exclusiveMinimum\": false,\r\n" + 
+                "                                    \"items\": {},\r\n" + 
+                "                                    \"maxLength\": 2147483647,\r\n" + 
+                "                                    \"minLength\": 0,\r\n" + 
+                "                                    \"nullable\": false,\r\n" + 
+                "                                    \"properties\": {\r\n" + 
+                "                                        \"humidityPercentage\": {\r\n" + 
+                "                                            \"deprecated\": false,\r\n" + 
+                "                                            \"example\": \"4\",\r\n" + 
+                "                                            \"exclusiveMaximum\": false,\r\n" + 
+                "                                            \"exclusiveMinimum\": false,\r\n" + 
+                "                                            \"maxLength\": 2147483647,\r\n" + 
+                "                                            \"minLength\": 0,\r\n" + 
+                "                                            \"nullable\": false,\r\n" + 
+                "                                            \"readOnly\": false,\r\n" + 
+                "                                            \"type\": \"string\",\r\n" + 
+                "                                            \"uniqueItems\": false,\r\n" + 
+                "                                            \"writeOnly\": false\r\n" + 
+                "                                        },\r\n" + 
+                "                                        \"temperatureInCelsius\": {\r\n" + 
+                "                                            \"deprecated\": false,\r\n" + 
+                "                                            \"example\": \"27\",\r\n" + 
+                "                                            \"exclusiveMaximum\": false,\r\n" + 
+                "                                            \"exclusiveMinimum\": false,\r\n" + 
+                "                                            \"maxLength\": 2147483647,\r\n" + 
+                "                                            \"minLength\": 0,\r\n" + 
+                "                                            \"nullable\": false,\r\n" + 
+                "                                            \"readOnly\": false,\r\n" + 
+                "                                            \"type\": \"string\",\r\n" + 
+                "                                            \"uniqueItems\": false,\r\n" + 
+                "                                            \"writeOnly\": false\r\n" + 
+                "                                        }\r\n" + 
+                "                                    },\r\n" + 
+                "                                    \"readOnly\": false,\r\n" + 
+                "                                    \"required\": [\r\n" + 
+                "                                        \"temperatureInCelsius\",\r\n" + 
+                "                                        \"humidityPercentage\"\r\n" + 
+                "                                    ],\r\n" + 
+                "                                    \"uniqueItems\": false,\r\n" + 
+                "                                    \"writeOnly\": false\r\n" + 
+                "                                }\r\n" + 
+                "                            }\r\n" + 
+                "                        },\r\n" + 
+                "                        \"description\": \"Detailed Weather\"\r\n" + 
+                "                    }\r\n" + 
+                "                }\r\n" + 
+                "            }\r\n" + 
+                "        }";
         JsonObject expectedJson = Json.createReader(new StringReader(expected)).readObject();
         assertEquals(expectedJson.keySet().size(), response.keySet().size());
         String[] expectedKeys = new String[]{ "operationId", "parameters", "responses"};
         Stream.of(expectedKeys).forEach((text) -> {
-          assertTrue("Expected: " + text
-                  + " to be present in " + expected,
-                  expectedJson.getJsonObject("get").get(text) != null);
+            assertTrue("Expected: " + text
+                    + " to be present in " + expected,
+                    expectedJson.getJsonObject("get").get(text) != null);
         });
-        
-        
-        
 
     }
-
 
 }
