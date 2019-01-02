81dc4120a8a5315d42ecceb8a46a857666382638
Puneeth PS <puneeth.ps@gmail.com>
2019-01-02 22:24:50 +0530

    formatted code.
    formatted code.

--- examples/mp-openapi/src/main/java/org/superbiz/openapi/Weather.java
+++ examples/mp-openapi/src/main/java/org/superbiz/openapi/Weather.java
@@ -2,24 +2,24 @@ package org.superbiz.openapi;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-@Schema(name="Weather", description="POJO that represents weather.")
+@Schema(name = "Weather", description = "POJO that represents weather.")
 public class Weather {
-	
-	@Schema(required = true, example = "27")
-	private String temperatureInCelsius;
-	
-	@Schema(required = true, example = "4")
-	private String humidityPercentage;
-	public String getTemperature() {
-		return temperatureInCelsius;
-	}
-	public void setTemperature(String temperature) {
-		this.temperatureInCelsius = temperature;
-	}
-	public String getHumidity() {
-		return humidityPercentage;
-	}
-	public void setHumidity(String humidity) {
-		this.humidityPercentage = humidity;
-	}
+
+    @Schema(required = true, example = "27")
+    private String temperatureInCelsius;
+
+    @Schema(required = true, example = "4")
+    private String humidityPercentage;
+    public  String getTemperature() {
+        return temperatureInCelsius;
+    }
+    public  void setTemperature(final String temperature) {
+        this.temperatureInCelsius = temperature;
+    }
+    public  String getHumidity() {
+        return humidityPercentage;
+    }
+    public  void setHumidity(final String humidity) {
+        this.humidityPercentage = humidity;
+    }
 }
