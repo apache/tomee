8e5caac668e1719391ab12a6b314182a5a74c78a
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-22 10:18:07 -0300

    creates adicional structure

---
+++ examples/ejb-remote-call/src/main/java/org/superbiz/ws/App.java
@@ -0,0 +1,23 @@
+package org.superbiz.ws;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class App {
+
+    public static void main(String[] args) throws NamingException {
+        Properties properties = new Properties();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
+        properties.put(Context.PROVIDER_URL, "http://localhost:8080/tomee/ejb");
+
+        Context ctx = new InitialContext(properties);
+        Object ref = ctx.lookup("global/ejb_remote_call_war/Calculator!org.superbiz.ws.Calculator");
+
+        Calculator calculator = Calculator.class.cast(ref);
+        System.out.println(calculator.sum(1, 2));
+
+
+    }
+}
