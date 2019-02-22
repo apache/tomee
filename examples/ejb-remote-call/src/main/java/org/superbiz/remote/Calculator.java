3d22d4342357eb5a118716c71d4ec05e61007221
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-22 10:45:30 -0300

    renames package name

--- examples/ejb-remote-call/src/main/java/org/superbiz/remote/Calculator.java
+++ examples/ejb-remote-call/src/main/java/org/superbiz/remote/Calculator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.ws;
+package org.superbiz.remote;
 
 public interface Calculator   {
 
