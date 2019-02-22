898af3bf7ae1877aa2f59ea19c4b337af4707112
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-22 10:18:07 -0300

    creates adicional structure

---
+++ examples/ejb-remote-call/src/main/java/org/superbiz/ws/DefaultCalculator.java
@@ -0,0 +1,20 @@
+package org.superbiz.ws;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+@Stateless(name = "Calculator", description = "Calculator", mappedName = "Calculator")
+@Remote(Calculator.class)
+public class DefaultCalculator implements Calculator {
+    @Override
+    public int sum(int add1, int add2) {
+        return add1 + add2;
+    }
+
+    @Override
+    public int multiply(int mul1, int mul2) {
+        return mul1 * mul2;
+    }
+
+
+}
