54f7541039c75b59149739601fb14b18c22ad23b
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-25 11:59:05 -0300

    removes annotation from DefaultCalculator

--- examples/ejb-remote-call-2/src/main/java/org/superbiz/remote/DefaultCalculator.java
+++ examples/ejb-remote-call-2/src/main/java/org/superbiz/remote/DefaultCalculator.java
@@ -17,14 +17,11 @@
 package org.superbiz.remote;
 
 import javax.ejb.EJBException;
-import javax.ejb.Remote;
 import javax.ejb.SessionContext;
-import javax.ejb.Stateless;
 import java.rmi.RemoteException;
 
-@Stateless(name = "Calculator", description = "Calculator", mappedName = "Calculator")
-@Remote(Calculator.class)
 public class DefaultCalculator implements Calculator {
+
     @Override
     public int sum(int add1, int add2) {
         return add1 + add2;
