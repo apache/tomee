43f562718429fe9fab6973c466fda42d0646472a
Doychin Bondzhev <doychin@dsoft-bg.com>
2019-03-26 18:44:59 +0200

    Remove unused method
    Signed-off-by: Doychin Bondzhev <doychin@dsoft-bg.com>

--- container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/Assembler.java
+++ container/openejb-core/src/main/java/org/apache/openejb/assembler/classic/Assembler.java
@@ -2259,22 +2259,6 @@ public class Assembler extends AssemblerTool implements org.apache.openejb.spi.A
         }
     }
 
-    public void destroyApplication(final AppContext appContext) throws UndeployException {
-
-        final ReentrantLock l = lock;
-        l.lock();
-
-        try {
-            final AppInfo appInfo = deployedApplications.remove(appContext.getId());
-            if (appInfo == null) {
-                throw new IllegalStateException(String.format("Cannot find AppInfo for app: %s", appContext.getId()));
-            }
-            destroyApplication(appInfo);
-        } finally {
-            l.unlock();
-        }
-    }
-
     public void destroyApplication(final AppInfo appInfo) throws UndeployException {
 
         final ReentrantLock l = lock;
