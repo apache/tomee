720084020fbdd7ddb7b0d517597d842f43acaf44
David Blevins <david.blevins@gmail.com>
2019-05-15 13:55:12 +0900

    Re-add test

--- mp-jwt/src/test/java/org/apache/tomee/microprofile/jwt/bval/ValidationGeneratorTest.java
+++ mp-jwt/src/test/java/org/apache/tomee/microprofile/jwt/bval/ValidationGeneratorTest.java
@@ -24,7 +24,6 @@ public class ValidationGeneratorTest {
         Assert.assertFalse(map.containsKey("green"));
     }
 
-    @Ignore
     @Test
     public void test() throws Exception {
         final String actual = Asmifier.asmify(ValidationGenerator.generateFor(Colors.class));
