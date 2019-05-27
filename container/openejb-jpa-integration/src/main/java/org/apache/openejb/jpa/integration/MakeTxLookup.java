b746ad03363dc779a241c816b7d7ee4fd354eaea
Jean-Louis Monteiro <jeanouii@gmail.com>
2019-05-27 19:02:49 +0200

    TOMEE-2529 XBean 4.14 dependency upgrade - missing changes in packages

--- container/openejb-jpa-integration/src/main/java/org/apache/openejb/jpa/integration/MakeTxLookup.java
+++ container/openejb-jpa-integration/src/main/java/org/apache/openejb/jpa/integration/MakeTxLookup.java
@@ -16,11 +16,11 @@
  */
 package org.apache.openejb.jpa.integration;
 
-import org.apache.xbean.asm6.ClassWriter;
-import org.apache.xbean.asm6.Label;
-import org.apache.xbean.asm6.MethodVisitor;
-import org.apache.xbean.asm6.Opcodes;
-import org.apache.xbean.asm6.Type;
+import org.apache.xbean.asm7.ClassWriter;
+import org.apache.xbean.asm7.Label;
+import org.apache.xbean.asm7.MethodVisitor;
+import org.apache.xbean.asm7.Opcodes;
+import org.apache.xbean.asm7.Type;
 
 import java.io.File;
 import java.io.FileOutputStream;
