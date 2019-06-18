5da21d16bab2f3abdc89b16e6868a2673c2d27f9
Jonathan Gallimore <jon@jrg.me.uk>
2019-06-18 11:05:32 +0100

    Fix up Grinder harness and test resources

--- examples/xa-datasource/src/test/resources/grinder.py
+++ examples/xa-datasource/src/test/resources/grinder.py
@@ -1,19 +1,6 @@
-# Hello World
-#
-# A minimal script that tests The Grinder logging facility.
-#
-# This script shows the recommended style for scripts, with a
-# TestRunner class. The script is executed just once by each worker
-# process and defines the TestRunner class. The Grinder creates an
-# instance of TestRunner for each worker thread, and repeatedly calls
-# the instance for each run of that thread.
-
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 from org.superbiz.perf import DBTestPerf
-
-# A shorter alias for the grinder.logger.output() method.
-log = grinder.logger.output
 
 tests = {
     "get" : Test(1, "get")
