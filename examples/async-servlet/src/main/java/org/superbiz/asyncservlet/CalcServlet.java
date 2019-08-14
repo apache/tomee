e4d0a2613b07f0beeaa4c0d86b3e26ac56dbf2f6
Jonathan Gallimore <jon@jrg.me.uk>
2019-08-14 15:07:59 +0100

    Improvements to this example

--- examples/async-servlet/src/main/java/org/superbiz/asyncservlet/CalcServlet.java
+++ examples/async-servlet/src/main/java/org/superbiz/asyncservlet/CalcServlet.java
@@ -18,102 +18,101 @@ package org.superbiz.asyncservlet;
 
 import javax.ejb.EJB;
 import javax.servlet.AsyncContext;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @WebServlet(urlPatterns = "/*", asyncSupported = true)
 public class CalcServlet extends HttpServlet {
 
+	public static final String RESULT_ATTRIBUTE = "RESULT";
+
 	@EJB
 	private CalcBean bean;
-
-	private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
 	private static final List<String> OPERATIONS = Arrays.asList("ADD", "SUBTRACT", "MULTIPLY", "DIVIDE");
 
 	@Override
-	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
 		process(req, resp);
 	}
 
 
 	@Override
-	protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+		process(req, resp);
 	}
 
 	private void process(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
-		final String operation = req.getParameter("op");
-		final String asyncParam = req.getParameter("async");
-		final String delayParam = req.getParameter("delay");
-		final String timeoutParam = req.getParameter("timeout");
-		final String xParam = req.getParameter("x");
-		final String yParam = req.getParameter("y");
 
-		if (operation == null || (! OPERATIONS.contains(operation.toUpperCase()))) {
-			displayUsage(resp);
-			return;
-		}
+		if (req.getAttribute(RESULT_ATTRIBUTE) == null) {
 
-		final int x;
-		try {
-			x = Integer.parseInt(xParam);
-		} catch (final Exception e) {
-			displayUsage(resp);
-			return;
-		}
+			final ResultHolder result = new ResultHolder();
+			req.setAttribute("RESULT", result);
 
-		final int y;
-		try {
-			y = Integer.parseInt(yParam);
-		} catch (final Exception e) {
-			displayUsage(resp);
-			return;
-		}
+			final String operation = req.getParameter("op");
+			final String asyncParam = req.getParameter("async");
+			final String delayParam = req.getParameter("delay");
+			final String timeoutParam = req.getParameter("timeout");
+			final String xParam = req.getParameter("x");
+			final String yParam = req.getParameter("y");
 
-		int delay = 0;
-		try {
-			delay = Integer.parseInt(delayParam);
-		} catch (final Exception e) {
-			// ignore
-		}
+			if (operation == null || (!OPERATIONS.contains(operation.toUpperCase()))) {
+				displayUsage(resp);
+				return;
+			}
 
-		int timeout = -1;
-		try {
-			timeout = Integer.parseInt(timeoutParam);
-		} catch (final Exception e) {
-			// ignore
-		}
+			final int x;
+			try {
+				x = Integer.parseInt(xParam);
+			} catch (final Exception e) {
+				displayUsage(resp);
+				return;
+			}
 
-		boolean async = false;
-		try {
-			async = Boolean.parseBoolean(asyncParam);
-		} catch (final Exception e) {
-			// ignore
-		}
+			final int y;
+			try {
+				y = Integer.parseInt(yParam);
+			} catch (final Exception e) {
+				displayUsage(resp);
+				return;
+			}
 
-		final PrintWriter writer = resp.getWriter();
+			int delay = 0;
+			try {
+				delay = Integer.parseInt(delayParam);
+			} catch (final Exception e) {
+				// ignore
+			}
 
-		if (! async) {
-			process(operation, x, y, writer);
-			return;
-		}
+			int timeout = -1;
+			try {
+				timeout = Integer.parseInt(timeoutParam);
+			} catch (final Exception e) {
+				// ignore
+			}
 
-		final int threadDelay = delay;
-		final AsyncContext asyncContext = req.startAsync();
-		asyncContext.setTimeout(timeout);
-		executorService.submit(new Runnable() {
-			@Override
-			public void run() {
+			boolean async = false;
+			try {
+				async = Boolean.parseBoolean(asyncParam);
+			} catch (final Exception e) {
+				// ignore
+			}
+
+			if (!async) {
+				process(operation, x, y, result);
+				resp.getWriter().print(result.getResult());
+				return;
+			}
+
+			final int threadDelay = delay;
+			final AsyncContext asyncContext = req.startAsync();
+			asyncContext.setTimeout(timeout);
+			asyncContext.start(() -> {
 
 				try {
 					Thread.sleep(threadDelay);
@@ -122,33 +121,55 @@ public class CalcServlet extends HttpServlet {
 				}
 
 				try {
-					process(operation, x, y, writer);
+					process(operation, x, y, result);
 				} catch (final Exception e) {
 
 				} finally {
-					asyncContext.complete();
+					asyncContext.dispatch();
 				}
 
-			}
-		});
-	}
-
-	private void process(final String operation, final int x, final int y, final PrintWriter writer) throws IOException {
-		if ("ADD".equals(operation.toUpperCase())) {
-			writer.print(bean.add(x, y));
-		} else if ("SUBTRACT".equals(operation.toUpperCase())) {
-			writer.print(bean.subtract(x, y));
-		} else if ("MULTIPLY".equals(operation.toUpperCase())) {
-			writer.print(bean.multiply(x, y));
-		} else if ("DIVIDE".equals(operation.toUpperCase())) {
-			writer.print(bean.divide(x, y));
+			});
+		} else {
+			final ResultHolder result = (ResultHolder) req.getAttribute("RESULT");
+			resp.getWriter().print(result.getResult());
 		}
 	}
 
-
-	void displayUsage(final HttpServletResponse resp) throws IOException {
-		resp.getWriter().println("Usage information will appear here");
+	private void process(final String operation, final int x, final int y, final ResultHolder result) {
+		if ("ADD".equals(operation.toUpperCase())) {
+			result.setResult(bean.add(x, y));
+		} else if ("SUBTRACT".equals(operation.toUpperCase())) {
+			result.setResult(bean.subtract(x, y));
+		} else if ("MULTIPLY".equals(operation.toUpperCase())) {
+			result.setResult(bean.multiply(x, y));
+		} else if ("DIVIDE".equals(operation.toUpperCase())) {
+			result.setResult(bean.divide(x, y));
+		}
 	}
 
-	
+	void displayUsage(final HttpServletResponse resp) throws IOException {
+		resp.getWriter().println("Parameters:");
+		resp.getWriter().println("\tx: 1st Operand");
+		resp.getWriter().println("\ty: 2nd Operand");
+		resp.getWriter().println("\top: Operator - add | subtract | multiply | divide");
+		resp.getWriter().println("\tasync: Whether to run asynchronously - true | false");
+		resp.getWriter().println("\tdelay: Delay for async calls (in ms)");
+		resp.getWriter().println("\ttimeout: Timeout for async calls (in ms)\n\n");
+		resp.getWriter().println("Example:");
+		resp.getWriter().println("\tSync: http://localhost:8080/async-servlet?x=2&y=4&op=multiply");
+		resp.getWriter().println("\tAsync (1 second delay): http://localhost:8080/async-servlet?x=2&y=4&op=multiply&async=true&delay=1000");
+		resp.getWriter().println("\tAsync Timeout (10 second delay, 1 second timeout): http://localhost:8080/async-servlet?x=2&y=4&op=multiply&async=true&delay=10000&timeout=1000");
+	}
+
+	public static class ResultHolder {
+		private int result = 0;
+
+		public int getResult() {
+			return result;
+		}
+
+		public void setResult(final int result) {
+			this.result = result;
+		}
+	}
 }
