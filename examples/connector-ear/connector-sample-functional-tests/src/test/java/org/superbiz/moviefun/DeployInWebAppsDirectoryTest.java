b1a2f3490263a4e9632d387ebb7262ed44878bc9
Jonathan Gallimore <jon@jrg.me.uk>
2019-09-10 12:22:09 +0100

    Update version

--- examples/connector-ear/connector-sample-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
+++ examples/connector-ear/connector-sample-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
@@ -105,7 +105,7 @@ public class DeployInWebAppsDirectoryTest {
         configuration.setGroupId("org.apache.openejb");
         configuration.setArtifactId("apache-tomee");
         configuration.setClassifier("plus");
-        configuration.setVersion("1.7.6-SNAPSHOT");
+        configuration.setVersion("1.7.6-TT.14");
 //        configuration.setDebug(true);
         configuration.setHttpPort(-1);
 
