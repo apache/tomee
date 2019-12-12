34c56a3e2c6444a57d0195b570c5281f3557c30a
Jonathan Gallimore <jon@jrg.me.uk>
2019-12-12 20:47:52 +0000

    Imports

--- server/openejb-cxf-rs/src/test/java/org/apache/openejb/server/cxf/rs/AutoBValNoCdiTest.java
+++ server/openejb-cxf-rs/src/test/java/org/apache/openejb/server/cxf/rs/AutoBValNoCdiTest.java
@@ -26,7 +26,11 @@ import org.junit.runner.RunWith;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
