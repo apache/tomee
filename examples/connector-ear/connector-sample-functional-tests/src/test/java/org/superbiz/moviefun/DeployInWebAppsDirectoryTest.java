7896b93381d5cbc7bfd2ee02c2cf0b0c73a9c09d
Jonathan Gallimore <jon@jrg.me.uk>
2020-01-22 13:24:05 +0000

    Update to 7.0.8-SNAPSHOT

--- examples/connector-ear/connector-sample-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
+++ examples/connector-ear/connector-sample-functional-tests/src/test/java/org/superbiz/moviefun/DeployInWebAppsDirectoryTest.java
@@ -105,7 +105,7 @@ public class DeployInWebAppsDirectoryTest {
         configuration.setGroupId("org.apache.tomee");
         configuration.setArtifactId("apache-tomee");
         configuration.setClassifier("plus");
-        configuration.setVersion("7.0.7-SNAPSHOT");
+        configuration.setVersion("7.0.8-SNAPSHOT");
 //        configuration.setDebug(true);
         configuration.setHttpPort(-1);
 
