21370a7049ab346eaf7e27b3ef7b16a21e441501
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2022-01-20 11:11:08 +0100

    Add a bit more tests to make sure we don't have a bug deploying and calling bother POJO and EJB webservices under different paths
    (cherry picked from commit 9a7d168e6b65e4947416fad34d568b2d4b155313)

--- arquillian/arquillian-tomee-tests/arquillian-tomee-jaxws-tests/src/test/java/org/apache/openejb/arquillian/tests/jaxws/WebServiceContextEJBTest.java
+++ arquillian/arquillian-tomee-tests/arquillian-tomee-jaxws-tests/src/test/java/org/apache/openejb/arquillian/tests/jaxws/WebServiceContextEJBTest.java
@@ -20,24 +20,29 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 import org.jboss.shrinkwrap.descriptor.api.webapp31.WebAppDescriptor;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.namespace.QName;
-import javax.xml.transform.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.ws.Dispatch;
@@ -51,7 +56,7 @@ import java.nio.charset.StandardCharsets;
 
 
 @RunWith(Arquillian.class)
-@Ignore
+// @Ignore
 public class WebServiceContextEJBTest {
     @ArquillianResource
     private URL url;
@@ -66,25 +71,109 @@ public class WebServiceContextEJBTest {
                 .createServletMapping()
                 .servletName("HelloService")
                 .urlPattern("/ws/Hello")
-                .up();
+                .up()
+                .createServletMapping()
+                .servletName("HelloService")
+                .urlPattern("/internal/Hello")
+                .up()
+            ;
 
         return ShrinkWrap.create(WebArchive.class, "ROOT.war")
-                .addClasses(HelloService.class)
+                .addClasses(HelloService.class, HelloServicePort.class)
+                         .addAsWebInfResource(new ClassLoaderAsset("ejb-jar.xml"), "ejb-jar.xml")
+                         .addAsWebInfResource(new ClassLoaderAsset("openejb-jar.xml"), "openejb-jar.xml")
+                         .addAsWebInfResource(new ClassLoaderAsset("webservices.xml"), "webservices.xml")
                 .addAsWebInfResource(new StringAsset(webAppDescriptor.exportAsString()), "web.xml");
     }
 
     @Test
-    public void invoke() throws Exception {
+    public void invokePojo() throws Exception {
+        // System.out.println(IO.slurp(new URL(url.toExternalForm() + "/ws/Hello?wsdl")));
         final Service service = Service.create(new URL(url.toExternalForm() + "/ws/Hello?wsdl"), new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "Hello"));
         final QName portQName = new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "HelloService");
+        assertServiceInvocation(service, portQName);
+    }
 
+    @Test
+    public void invokePojoAlternate() throws Exception {
+        final Service service = Service.create(new URL(url.toExternalForm() + "/internal/Hello?wsdl"), new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "Hello"));
+        final QName portQName = new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "HelloService");
+        assertServiceInvocation(service, portQName);
+    }
+
+    /*
+        CAUTION: by default JAX-WS webservices are deployed under /webservices subcontext
+        It's possible to override or change it to something else.
+        For instance if you don't want any prefix at all, go to arquillian.xml
+        and uncomment the following line
+
+        # tomee.jaxws.subcontext = /
+
+        Of course, the URLs bellow need to be updated.
+        I'm not committing the change because it will cause other tests to fail
+     */
+    @Test
+    public void invokeEjb() throws Exception {
+        // System.out.println(IO.slurp(new URL(url.toExternalForm() + "/webservices/ws/HelloEjb?wsdl")));
+        final Service service = Service.create(new URL(url.toExternalForm() + "/webservices/ws/HelloEjb?wsdl"), new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "Hello"));
+        assertServiceInvocationWithPort(service);
+    }
+
+    @Test
+    public void invokeEjbAlternate() throws Exception {
+        final Service service = Service.create(new URL(url.toExternalForm() + "/webservices/internal/HelloEjb?wsdl"), new QName("http://jaxws.tests.arquillian.openejb.apache.org/", "Hello"));
+        assertServiceInvocationWithPort(service);
+    }
+
+    @WebService(name = "Hello", targetNamespace = "http://jaxws.tests.arquillian.openejb.apache.org/", serviceName = "Hello", portName = "HelloService")
+    @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED, use = SOAPBinding.Use.LITERAL)
+    @Stateless
+    public static class HelloService implements HelloServicePort {
+
+        @Resource
+        private WebServiceContext context;
+
+        @Inject
+        private HttpServletRequest httpServletRequest;
+
+        @WebMethod
+        public String sayHello(final @WebParam(name="name") String name) {
+
+            // new Throwable().printStackTrace();
+
+            final MessageContext messageContext = context.getMessageContext();
+            final HttpServletRequest request = (HttpServletRequest) messageContext.get(MessageContext.SERVLET_REQUEST);
+
+            final String testHeader = request.getHeader("XX-Test");
+            return "Hello, " + name + ", test header is set to " + testHeader;
+        }
+    }
+
+    @WebService(targetNamespace = "http://jaxws.tests.arquillian.openejb.apache.org/")
+    public interface HelloServicePort {
+        String sayHello(final @WebParam(name="name")  String name);
+    }
+
+    private void assertServiceInvocationWithPort(final Service service) {
+        final HelloServicePort hello = service.getPort(HelloServicePort.class);
+        final String result = hello.sayHello("tomee");
+        System.out.println(result);
+        Assert.assertTrue(result.contains("Hello, tomee, test header is set to null"));
+    }
+
+    private void assertServiceInvocation(final Service service, final QName portQName) throws TransformerException {
         final Dispatch<Source> dispatch = service.createDispatch(portQName, Source.class, Service.Mode.PAYLOAD);
         final String request =
-                "      <jax:sayHello xmlns:jax=\"http://jaxws.tests.arquillian.openejb.apache.org/\">\n" +
-                "         <name>tomee</name>\n" +
-                "      </jax:sayHello>\n";
+            "      <jax:sayHello xmlns:jax=\"http://jaxws.tests.arquillian.openejb.apache.org/\">\n" +
+            "         <name>tomee</name>\n" +
+            "      </jax:sayHello>\n";
         final Source response = dispatch.invoke(new StreamSource(new StringReader(request)));
+        final String result = asString(response);
+        System.out.println(result);
+        Assert.assertTrue(result.contains("<return>Hello, tomee, test header is set to null</return>"));
+    }
 
+    private String asString(final Source response) throws TransformerException {
         final TransformerFactory factory = TransformerFactory.newInstance();
         final Transformer transformer = factory.newTransformer();
         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
@@ -94,26 +183,6 @@ public class WebServiceContextEJBTest {
         streamResult.setOutputStream(os);
         transformer.transform(response, streamResult);
 
-        final String result = new String(os.toByteArray(), StandardCharsets.UTF_8);
-        System.out.println(result);
-        Assert.assertTrue(result.contains("<return>Hello, tomee, test header is set to null</return>"));
-    }
-
-    @WebService(name = "Hello", targetNamespace = "http://jaxws.tests.arquillian.openejb.apache.org/", serviceName = "Hello", portName = "HelloService")
-    @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED, use = SOAPBinding.Use.LITERAL)
-    @Stateless
-    public static class HelloService {
-
-        @Resource
-        private WebServiceContext context;
-
-        @WebMethod
-        public String sayHello(final @WebParam(name="name") String name) {
-            final MessageContext messageContext = context.getMessageContext();
-            final HttpServletRequest request = (HttpServletRequest) messageContext.get(MessageContext.SERVLET_REQUEST);
-
-            final String testHeader = request.getHeader("XX-Test");
-            return "Hello, " + name + ", test header is set to " + testHeader;
-        }
+        return new String(os.toByteArray(), StandardCharsets.UTF_8);
     }
 }
