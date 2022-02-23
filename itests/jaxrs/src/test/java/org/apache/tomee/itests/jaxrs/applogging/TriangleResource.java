1cacdca2ad1a93da2bfa642f71f71e6a438fbc82
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2022-02-23 19:31:40 +0100

    Move to jakarta namespace

--- itests/jaxrs/src/test/java/org/apache/tomee/itests/jaxrs/applogging/TriangleResource.java
+++ itests/jaxrs/src/test/java/org/apache/tomee/itests/jaxrs/applogging/TriangleResource.java
@@ -16,8 +16,8 @@
  */
 package org.apache.tomee.itests.jaxrs.applogging;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 
 @Path("/triangle")
 public class TriangleResource {
