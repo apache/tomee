f4543816918aba412fec57d1cc6c9af1ed28f73c
Jonathan Gallimore <jon@jrg.me.uk>
2022-03-30 10:27:45 +0100

    Something screwed up here - fixing this test

--- itests/ejb/src/test/java/org/apache/tomee/itests/ejb/CalculatorBean.java
+++ itests/ejb/src/test/java/org/apache/tomee/itests/ejb/CalculatorBean.java
@@ -17,8 +17,14 @@
 package org.apache.tomee.itests.ejb;
 
 import javax.ejb.Remote;
+import javax.ejb.Stateless;
 
+@Stateless
 @Remote
-public interface CalculatorRemote {
-    int sum (int a1, int a2);
+public class CalculatorBean implements CalculatorRemote {
+
+    @Override
+    public int sum(int a1, int a2) {
+        return a1 + a2;
+    }
 }
