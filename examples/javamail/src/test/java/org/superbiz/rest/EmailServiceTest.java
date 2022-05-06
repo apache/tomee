a576b4ee7a357aadefc4b47ab9b1335de1af3099
Richard Zowalla <rzo1@apache.org>
2022-05-06 08:24:47 +0200

    TOMEE-3940 - Fix TomEE :: Examples :: JakartaMail API: Use a test mail server (greenmail) to actually sent an e-mail instead of testing, that the mail sending will always fail.

--- examples/javamail/src/test/java/org/superbiz/rest/EmailServiceTest.java
+++ examples/javamail/src/test/java/org/superbiz/rest/EmailServiceTest.java
@@ -16,16 +16,24 @@
  */
 package org.superbiz.rest;
 
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.openejb.jee.WebApp;
 import org.apache.openejb.junit.ApplicationComposer;
 import org.apache.openejb.testing.Classes;
+import org.apache.openejb.testing.Configuration;
 import org.apache.openejb.testing.EnableServices;
 import org.apache.openejb.testing.Module;
+import org.apache.openejb.util.NetworkUtil;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.assertEquals;
 
@@ -33,15 +41,70 @@ import static org.junit.Assert.assertEquals;
 @RunWith(ApplicationComposer.class)
 public class EmailServiceTest {
 
+    private static final int SMTP_TEST_PORT = NetworkUtil.getNextAvailablePort();
+
+    private static final String USER_PASSWORD = "s3cr3t";
+    private static final String USER_NAME = "admin@localhost";
+    private static final String EMAIL_USER_ADDRESS = "admin@localhost";
+
+    private static GreenMail mailServer;
+    private static CountDownLatch started = new CountDownLatch(1);
+
     @Module
     @Classes(EmailService.class)
     public WebApp app() {
         return new WebApp().contextRoot("test");
     }
 
+    @Configuration
+    public Properties config() {
+        //Note: We can also configure this via a resource.xml or via tomee.xml
+        Properties properties = new Properties();
+        properties.put("tomee/mail/mySMTP", "new://Resource?type=jakarta.mail.Session");
+        properties.put("tomee/mail/mySMTP.mail.debug", "false");
+        properties.put("tomee/mail/mySMTP.mail.transport.protocol", "smtp");
+        properties.put("tomee/mail/mySMTP.mail.smtp.host", "localhost");
+        properties.put("tomee/mail/mySMTP.mail.smtp.port", SMTP_TEST_PORT);
+        properties.put("tomee/mail/mySMTP.mail.smtp.auth", "true");
+        properties.put("tomee/mail/mySMTP.mail.smtp.user", USER_NAME);
+        properties.put("tomee/mail/mySMTP.password", USER_PASSWORD);
+        return properties;
+    }
+
+    @BeforeClass
+    public static void setUp() throws InterruptedException {
+        mailServer = new CustomGreenMailServer(new ServerSetup(SMTP_TEST_PORT, null, "smtp"));
+        mailServer.start();
+
+        //wait for the server startup...
+        started.await();
+
+        // create user on mail server
+        mailServer.setUser(EMAIL_USER_ADDRESS, USER_NAME, USER_PASSWORD);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (mailServer != null) {
+            mailServer.stop();
+        }
+    }
+
     @Test
     public void post() throws IOException {
         final String message = WebClient.create("http://localhost:4204").path("/test/email/").post("Hello TomEE", String.class);
-        assertEquals("Failed to send message: Unknown SMTP host: your.mailserver.host", message);
+        assertEquals("Sent", message);
+    }
+
+    public static class CustomGreenMailServer extends GreenMail {
+
+        public CustomGreenMailServer(ServerSetup config) {
+            super(new ServerSetup[]{config});
+        }
+
+        public synchronized void start() {
+            super.start();
+            started.countDown();
+        }
     }
 }
