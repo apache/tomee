37c31c81d3cb6dec8f2b34ac69065f1df061b249
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2022-07-06 14:31:06 +0200

    Fix BVal test failures

--- tck/bval-tomee/src/test/java/org/apache/tomee/bval/tck/BValTCKArchiveProcessor.java
+++ tck/bval-tomee/src/test/java/org/apache/tomee/bval/tck/BValTCKArchiveProcessor.java
@@ -18,20 +18,18 @@
 
 package org.apache.tomee.bval.tck;
 
-import com.sun.javafx.PlatformUtil;
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.test.spi.TestClass;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-
-import static org.apache.openejb.loader.JarLocation.jarLocation;
 
 public class BValTCKArchiveProcessor implements ApplicationArchiveProcessor {
     @Override
     public void process(final Archive<?> archive, final TestClass testClass) {
+        /*
         if (archive instanceof WebArchive) {
             WebArchive.class.cast(archive).addAsLibrary(jarLocation(PlatformUtil.class));
         }
+        */
         System.out.println(archive.toString(true));
     }
 }
