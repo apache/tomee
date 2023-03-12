ab06028fa1686eb4d6f06061b71ba8cb09f29544
G-Ork <tsakumagos@gmail.com>
2023-03-12 08:53:11 +0100

    TOMEE-4190 RunWithApplicationComposer should support inheritance
    - Added Inherited annotation.
    - Added unittests to proove fix.

--- container/openejb-junit5/src/test/java/org/apache/openejb/junit5/AppComposerPerClassInheritanceTest.java
+++ container/openejb-junit5/src/test/java/org/apache/openejb/junit5/AppComposerPerClassInheritanceTest.java
@@ -14,25 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.openejb.junit5;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.apache.openejb.testing.Classes;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Inherited
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@ExtendWith(ApplicationComposerExtension.class)
-public @interface RunWithApplicationComposer {
-
-    /**
-     * The test container lifecycle <em>mode</em> to use.
-     */
-    ExtensionMode mode() default ExtensionMode.AUTO;
+@Classes(cdi = true, innerClassesAsBean = true)
+public class AppComposerPerClassInheritanceTest extends AppComposerPerClassBase {
 }
