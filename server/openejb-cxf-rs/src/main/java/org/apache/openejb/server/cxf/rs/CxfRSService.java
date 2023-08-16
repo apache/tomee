97cd707e639ca6a239e0cb2c4827a269f9251bd1
Richard Zowalla <rzo1@apache.org>
2023-08-16 08:42:48 +0200

    TOMEE-4139 - CXF 4.0.3-SNAPSHOT

--- server/openejb-cxf-rs/src/main/java/org/apache/openejb/server/cxf/rs/CxfRSService.java
+++ server/openejb-cxf-rs/src/main/java/org/apache/openejb/server/cxf/rs/CxfRSService.java
@@ -227,7 +227,9 @@ public class CxfRSService extends RESTService {
             List<String> jsonProviders;
             String userConfiguredJsonProviders = SystemInstance.get().getProperty("openejb.jaxrs.jsonProviders");
             if (userConfiguredJsonProviders == null) {
-                jsonProviders = Collections.emptyList();
+                jsonProviders = asList(
+                        "org.apache.openejb.server.cxf.rs.johnzon.TomEEJsonbProvider",
+                        "org.apache.openejb.server.cxf.rs.johnzon.TomEEJsonpProvider");
             } else {
                 jsonProviders = asList(userConfiguredJsonProviders.split(","));
             }
