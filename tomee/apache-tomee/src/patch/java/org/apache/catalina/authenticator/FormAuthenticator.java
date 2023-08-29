b5e88d1534c457905a7411b8182928a528f6f8d9
Richard Zowalla <rzo1@apache.org>
2023-08-29 11:06:19 +0200

    TOMEE-4239 - Backport fix for CVE-2023-41080 from https://github.com/apache/tomcat/commit/bb4624a9f3e69d495182ebfa68d7983076407a27

--- tomee/apache-tomee/src/patch/java/org/apache/catalina/authenticator/FormAuthenticator.java
+++ tomee/apache-tomee/src/patch/java/org/apache/catalina/authenticator/FormAuthenticator.java
@@ -728,6 +728,12 @@ public class FormAuthenticator
             sb.append('?');
             sb.append(saved.getQueryString());
         }
+
+        // Avoid protocol relative redirects
+        while (sb.length() > 1 && sb.charAt(1) == '/') {
+            sb.deleteCharAt(0);
+        }
+
         return sb.toString();
     }
 }
