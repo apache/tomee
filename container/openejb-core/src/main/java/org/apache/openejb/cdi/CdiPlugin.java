ced1434edcf2c6451d422d78a033ae3343726739
Richard Zowalla <rzo1@apache.org>
2023-09-29 16:06:49 +0200

    OWB 4.0.0

--- container/openejb-core/src/main/java/org/apache/openejb/cdi/CdiPlugin.java
+++ container/openejb-core/src/main/java/org/apache/openejb/cdi/CdiPlugin.java
@@ -463,7 +463,7 @@ public class CdiPlugin extends AbstractOwbPlugin implements OpenWebBeansJavaEEPl
 
     @Override
     public <T> Bean<T> defineNewSessionBean(final Class<T> clazz) {
-        return new NewCdiEjbBean<>(findBeanContext(webBeansContext, clazz).get(CdiEjbBean.class));
+        return findBeanContext(webBeansContext, clazz).get(CdiEjbBean.class);
     }
 
     private static Map<Class<?>, BeanContext> pluginBeans(final WebBeansContext ctx) {
