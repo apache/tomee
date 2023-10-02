a4228d3c8b088054e61d7fc656c4b9c39ba059a0
Richard Zowalla <rzo1@apache.org>
2023-10-02 08:26:34 +0200

    MyFaces 4 (picked from https://github.com/apache/tomee/pull/1019)

--- tomee/tomee-myfaces/src/main/java/org/apache/tomee/myfaces/TomEEFacesConfigResourceProvider.java
+++ tomee/tomee-myfaces/src/main/java/org/apache/tomee/myfaces/TomEEFacesConfigResourceProvider.java
@@ -17,7 +17,7 @@
 package org.apache.tomee.myfaces;
 
 import org.apache.myfaces.config.DefaultFacesConfigResourceProvider;
-import org.apache.myfaces.shared.util.ClassUtils;
+import org.apache.myfaces.util.lang.ClassUtils;
 import org.apache.openejb.config.NewLoaderLogic;
 import org.apache.openejb.loader.Files;
 import org.apache.openejb.util.AppFinder;
