87136d0eac5cf06a75eb94325ffa328455b4860c
Richard Zowalla <rzo1@apache.org>
2023-10-11 09:02:02 +0200

    TOMEE-4255 - Add patched classes from https://github.com/apache/tomcat/commit/76bb4bfbfeae827dce896f650655bbf6e251ed49#diff-9105b119c42abec1b9a22cd5d6fc2c0b4edc63175a12858b8b383fb7554ab57f for CVE-2023-44487.

--- tomee/apache-tomee/src/patch/java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ tomee/apache-tomee/src/patch/java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -381,7 +381,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                                 stream.close(se);
                             }
                         } finally {
-                            if (overheadCount.get() > 0) {
+                            if (isOverheadLimitExceeded()) {
                                 throw new ConnectionException(
                                         sm.getString("upgradeHandler.tooMuchOverhead", connectionId),
                                         Http2Error.ENHANCE_YOUR_CALM);
@@ -1495,6 +1495,11 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
+    boolean isOverheadLimitExceeded() {
+        return overheadCount.get() > 0;
+    }
+
+
     // ----------------------------------------------- Http2Parser.Input methods
 
     @Override
@@ -1768,6 +1773,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
             log.debug(sm.getString("upgradeHandler.reset.receive", getConnectionId(), Integer.toString(streamId),
                     Long.toString(errorCode)));
         }
+        increaseOverheadCount(FrameType.RST, getProtocol().getOverheadResetFactor());
         AbstractNonZeroStream abstractNonZeroStream = getAbstractNonZeroStream(streamId, true);
         abstractNonZeroStream.checkState(FrameType.RST);
         if (abstractNonZeroStream instanceof Stream) {
