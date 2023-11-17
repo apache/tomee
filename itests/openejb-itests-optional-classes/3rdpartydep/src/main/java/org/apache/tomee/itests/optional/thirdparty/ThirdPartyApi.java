5c41ae8adffb2e47787181c0276620a287378b13
Mark Struberg <struberg@apache.org>
2023-11-17 19:02:49 +0100

    add an integration test for optional beans
    if a class creates a NoClassDefFoundError or ClassNotFoundException then it's simply
    not an eligible bean class.

---
+++ itests/openejb-itests-optional-classes/3rdpartydep/src/main/java/org/apache/tomee/itests/optional/thirdparty/ThirdPartyApi.java
@@ -0,0 +1,10 @@
+package org.apache.tomee.itests.optional.thirdparty;
+
+/**
+ * This is an example of an optional 3rd party library
+ * used in the opt-lib project.
+ */
+public interface ThirdPartyApi {
+
+    int meaningOfLife();
+}
