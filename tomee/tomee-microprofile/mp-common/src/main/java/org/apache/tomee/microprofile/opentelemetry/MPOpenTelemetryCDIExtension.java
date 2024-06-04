8fa631ac422d3244812b8e6206a2139dacfb05b3
Richard Zowalla <rzo1@apache.org>
2024-06-04 21:22:06 +0200

    TOMEE-4343 - WIP: OpenTelemetry 1.0

--- tomee/tomee-microprofile/mp-common/src/main/java/org/apache/tomee/microprofile/opentelemetry/MPOpenTelemetryCDIExtension.java
+++ tomee/tomee-microprofile/mp-common/src/main/java/org/apache/tomee/microprofile/opentelemetry/MPOpenTelemetryCDIExtension.java
@@ -16,149 +16,23 @@
  */
 package org.apache.tomee.microprofile.opentelemetry;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import io.opentelemetry.instrumentation.annotations.WithSpan;
-import io.smallrye.opentelemetry.implementation.cdi.OpenTelemetryProducer;
 import io.smallrye.opentelemetry.implementation.config.OpenTelemetryConfigProducer;
 import jakarta.enterprise.event.Observes;
-import jakarta.enterprise.inject.spi.AnnotatedConstructor;
-import jakarta.enterprise.inject.spi.AnnotatedField;
-import jakarta.enterprise.inject.spi.AnnotatedMethod;
-import jakarta.enterprise.inject.spi.AnnotatedParameter;
-import jakarta.enterprise.inject.spi.AnnotatedType;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
 import jakarta.enterprise.inject.spi.Extension;
-import jakarta.enterprise.util.Nonbinding;
+import org.apache.openejb.loader.SystemInstance;
 
 public class MPOpenTelemetryCDIExtension implements Extension {
 
-    public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery beforeBeanDiscovery, BeanManager beanManager) {
-        beforeBeanDiscovery.addInterceptorBinding(
-            new MPOpenTelemetryCDIExtension.WithSpanAnnotatedType(beanManager.createAnnotatedType(WithSpan.class)));
+  public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery beforeBeanDiscovery, BeanManager beanManager) {
+    final String mpScan = SystemInstance.get().getOptions().get("tomee.mp.scan", "none");
 
-        beforeBeanDiscovery.addAnnotatedType(beanManager.createAnnotatedType(OpenTelemetryProducer.class), "OpenTelemetryProducer");
-        beforeBeanDiscovery.addAnnotatedType(beanManager.createAnnotatedType(WithSpanInterceptorProducer.class), "WithSpanInterceptorProducer");
-        beforeBeanDiscovery.addAnnotatedType(beanManager.createAnnotatedType(OpenTelemetryConfigProducer.class), "OpenTelemetryConfigProducer");
-
+    if (mpScan.equals("none")) {
+      SystemInstance.get().setProperty(MPOpenTelemetryCDIExtension.class.getName() + ".active", "false");
+      return;
     }
 
-    // To add Nonbinding to @WithSpan members
-    @SuppressWarnings("unchecked")
-    static class WithSpanAnnotatedType implements AnnotatedType<WithSpan> {
-        private final AnnotatedType<WithSpan> delegate;
-        private final Set<AnnotatedMethod<? super WithSpan>> methods;
-
-        WithSpanAnnotatedType(final AnnotatedType<WithSpan> delegate) {
-            this.delegate = delegate;
-            this.methods = new HashSet<>();
-
-            for (AnnotatedMethod<? super WithSpan> method : delegate.getMethods()) {
-                methods.add(new AnnotatedMethod<WithSpan>() {
-                    private final AnnotatedMethod<WithSpan> delegate = (AnnotatedMethod<WithSpan>) method;
-                    private final Set<Annotation> annotations = Collections.singleton(Nonbinding.Literal.INSTANCE);
-
-                    @Override
-                    public Method getJavaMember() {
-                        return delegate.getJavaMember();
-                    }
-
-                    @Override
-                    public List<AnnotatedParameter<WithSpan>> getParameters() {
-                        return delegate.getParameters();
-                    }
-
-                    @Override
-                    public boolean isStatic() {
-                        return delegate.isStatic();
-                    }
-
-                    @Override
-                    public AnnotatedType<WithSpan> getDeclaringType() {
-                        return delegate.getDeclaringType();
-                    }
-
-                    @Override
-                    public Type getBaseType() {
-                        return delegate.getBaseType();
-                    }
-
-                    @Override
-                    public Set<Type> getTypeClosure() {
-                        return delegate.getTypeClosure();
-                    }
-
-                    @Override
-                    public <T extends Annotation> T getAnnotation(final Class<T> annotationType) {
-                        if (annotationType.equals(Nonbinding.class)) {
-                            return (T) annotations.iterator().next();
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    public Set<Annotation> getAnnotations() {
-                        return annotations;
-                    }
-
-                    @Override
-                    public boolean isAnnotationPresent(final Class<? extends Annotation> annotationType) {
-                        return annotationType.equals(Nonbinding.class);
-                    }
-                });
-            }
-        }
-
-        @Override
-        public Class<WithSpan> getJavaClass() {
-            return delegate.getJavaClass();
-        }
-
-        @Override
-        public Set<AnnotatedConstructor<WithSpan>> getConstructors() {
-            return delegate.getConstructors();
-        }
-
-        @Override
-        public Set<AnnotatedMethod<? super WithSpan>> getMethods() {
-            return this.methods;
-        }
-
-        @Override
-        public Set<AnnotatedField<? super WithSpan>> getFields() {
-            return delegate.getFields();
-        }
-
-        @Override
-        public Type getBaseType() {
-            return delegate.getBaseType();
-        }
-
-        @Override
-        public Set<Type> getTypeClosure() {
-            return delegate.getTypeClosure();
-        }
-
-        @Override
-        public <T extends Annotation> T getAnnotation(final Class<T> annotationType) {
-            return delegate.getAnnotation(annotationType);
-        }
-
-        @Override
-        public Set<Annotation> getAnnotations() {
-            return delegate.getAnnotations();
-        }
-
-        @Override
-        public boolean isAnnotationPresent(final Class<? extends Annotation> annotationType) {
-            return delegate.isAnnotationPresent(annotationType);
-        }
-    }
+    beforeBeanDiscovery.addAnnotatedType(beanManager.createAnnotatedType(OpenTelemetryConfigProducer.class), "OpenTelemetryConfigProducer");
+  }
 }
