9e3a33e2416879157ff8d341e662287604859008
Richard Zowalla <rzo1@apache.org>
2024-11-23 21:14:39 +0100

    Disable OTEL SDK by default via custom config source. Remove Geronimo MP specific things from previous TomEEConfigSource

--- tck/microprofile-tck/opentelemetry/src/test/java/org/apache/tomee/microprofile/tck/opentelemetry/ArquillianExtension.java
+++ tck/microprofile-tck/opentelemetry/src/test/java/org/apache/tomee/microprofile/tck/opentelemetry/ArquillianExtension.java
@@ -22,7 +22,6 @@ import org.jboss.arquillian.core.spi.LoadableExtension;
 public class ArquillianExtension implements LoadableExtension {
     @Override
     public void register(ExtensionBuilder extensionBuilder) {
-        extensionBuilder.service(ApplicationArchiveProcessor.class, DeploymentProcessor.class);
         extensionBuilder.observer(ArquillianLifecycle.class);
     }
 }
