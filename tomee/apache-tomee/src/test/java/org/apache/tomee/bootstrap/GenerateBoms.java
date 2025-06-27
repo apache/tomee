2e6b138529b836f3618c4fdac8b0f08f253b9cfe
Markus Jung <jungm@apache.org>
2025-06-27 07:42:40 +0200

    update bom generation

--- tomee/apache-tomee/src/test/java/org/apache/tomee/bootstrap/GenerateBoms.java
+++ tomee/apache-tomee/src/test/java/org/apache/tomee/bootstrap/GenerateBoms.java
@@ -532,7 +532,7 @@ public class GenerateBoms {
             }
 
             if (jar.getName().startsWith("ecj-")) {
-                return new Artifact("org.eclipse.jdt", "ecj", "3.40.0", null);
+                return new Artifact("org.eclipse.jdt", "ecj", "3.42.0", null);
             }
 
             if (jar.getName().equals("openejb-javaagent.jar")) {
