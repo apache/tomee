b6412b8591759202b9d310c2da3b4c1ebb17daa8
ksirigeri <ksirigeri@opentext.com>
2026-01-11 18:17:20 +0530

    added unit tests for different scenarios

--- container/openejb-core/src/test/java/org/apache/openejb/core/stateful/StatefulBeanRegistryCleanupTest.java
+++ container/openejb-core/src/test/java/org/apache/openejb/core/stateful/StatefulBeanRegistryCleanupTest.java
@@ -18,7 +18,10 @@ package org.apache.openejb.core.stateful;
 
 import junit.framework.TestCase;
 import org.apache.openejb.OpenEJB;
-import org.apache.openejb.assembler.classic.*;
+import org.apache.openejb.assembler.classic.Assembler;
+import org.apache.openejb.assembler.classic.SecurityServiceInfo;
+import org.apache.openejb.assembler.classic.StatefulSessionContainerInfo;
+import org.apache.openejb.assembler.classic.TransactionServiceInfo;
 import org.apache.openejb.config.ConfigurationFactory;
 import org.apache.openejb.core.OpenEJBInitialContextFactory;
 import org.apache.openejb.jee.EjbJar;
@@ -32,7 +35,7 @@ import javax.naming.InitialContext;
 import java.lang.reflect.Proxy;
 import java.util.concurrent.ConcurrentMap;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  *
@@ -57,6 +60,7 @@ public class StatefulBeanRegistryCleanupTest extends TestCase {
 
         final EjbJar ejbJar = new EjbJar();
         ejbJar.addEnterpriseBean(new StatefulBean(MyBean.class));
+        ejbJar.addEnterpriseBean(new StatefulBean(MyBean2.class));
 
         assembler.createApplication(config.configureApplication(ejbJar));
     }
@@ -76,6 +80,69 @@ public class StatefulBeanRegistryCleanupTest extends TestCase {
         assertTrue("Live handle registry should be empty after removal", reg.isEmpty());
     }
 
+    public void testLiveHandleRegistryForSameBean() throws Exception {
+        final Context context = new InitialContext();
+
+        // Create two proxies/handles
+        final MyBeanInterface bean1 = (MyBeanInterface) context.lookup("MyBeanRemote");
+        final MyBeanInterface bean2 = (MyBeanInterface) context.lookup("MyBeanRemote");
+
+        final StatefulEjbObjectHandler handler1 = (StatefulEjbObjectHandler) Proxy.getInvocationHandler(bean1);
+        final StatefulEjbObjectHandler handler2 = (StatefulEjbObjectHandler) Proxy.getInvocationHandler(bean2);
+
+        final ConcurrentMap<?, ?> reg1 = handler1.getLiveHandleRegistry();
+        final ConcurrentMap<?, ?> reg2 = handler2.getLiveHandleRegistry();
+
+        assertEquals("Both the registry references should be same", reg1, reg2);
+
+        // Exercise the proxies to ensure handles are registered
+        bean1.echo("a");
+        bean2.echo("b");
+
+        assertEquals("Registry size should have references for both the beans", 2, reg1.size());
+
+        // Remove first bean and ensure its registry is cleared
+        bean1.cleanup();
+        assertEquals("Registry should have reference for only bean2 after bean1 removal", 1, reg1.size());
+
+        // Remove second bean
+        bean2.cleanup();
+        assertEquals("Registry should be empty after removing both the beans", 0, reg1.size());
+    }
+
+    public void testLiveHandleRegistryForDifferentBeans() throws Exception {
+        final Context context = new InitialContext();
+
+        // Create two proxies/handles
+        final MyBeanInterface bean1 = (MyBeanInterface) context.lookup("MyBeanRemote");
+        final MyBeanInterface2 bean2 = (MyBeanInterface2) context.lookup("MyBean2Remote");
+
+        final StatefulEjbObjectHandler handler1 = (StatefulEjbObjectHandler) Proxy.getInvocationHandler(bean1);
+        final StatefulEjbObjectHandler handler2 = (StatefulEjbObjectHandler) Proxy.getInvocationHandler(bean2);
+
+        final ConcurrentMap<?, ?> reg1 = handler1.getLiveHandleRegistry();
+        final ConcurrentMap<?, ?> reg2 = handler2.getLiveHandleRegistry();
+
+        assertNotEquals("Both the registry references should be different", reg1, reg2);
+
+        // Exercise the proxies to ensure handles are registered
+        bean1.echo("a");
+        bean2.echo("b");
+
+        assertEquals("Registry for bean1 should have single entry", 1, reg1.size());
+        assertEquals("Registry for bean2 should have single entry", 1, reg2.size());
+
+        // Remove first bean and ensure its registry is cleared
+        bean1.cleanup();
+        assertEquals("Registry for bean1 should be empty", 0, reg1.size());
+        assertEquals("Registry for bean2 should have single entry", 1, reg2.size());
+
+        // Remove second bean
+        bean2.cleanup();
+        assertEquals("Registry for bean1 should be empty", 0, reg1.size());
+        assertEquals("Registry for bean2 should be empty", 0, reg2.size());
+    }
+
     public interface MyBeanInterface {
         String echo(String string);
 
@@ -87,6 +154,32 @@ public class StatefulBeanRegistryCleanupTest extends TestCase {
     @Stateful
     @Remote
     public static class MyBean implements MyBeanInterface {
+
+        @Override
+        public String echo(final String string) {
+            final StringBuilder sb = new StringBuilder(string);
+            return sb.reverse().toString();
+        }
+
+        @Override
+        public void cleanup() {
+            System.out.println("cleaning up MyBean instance");
+        }
+
+    }
+
+
+    public interface MyBeanInterface2 {
+        String echo(String string);
+
+        @Remove
+        void cleanup();
+    }
+
+
+    @Stateful
+    @Remote
+    public static class MyBean2 implements MyBeanInterface2 {
 
         @Override
         public String echo(final String string) {
