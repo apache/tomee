773bc6e855d5272ae311e0a5d5442bdd207b5d8e
Jonathan S. Fisher <exabrial@gmail.com>
2026-08-24 12:15:13 -0500

    Additional fixes to Jonathan Gallimore's branch

---
+++ tomee/tomee-security/src/test/java/org/apache/tomee/security/servlet/LoginToContinueNoSessionTest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tomee.security.servlet;
+
+import org.apache.tomee.security.AbstractTomEESecurityTest;
+import org.junit.Test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class LoginToContinueNoSessionTest extends AbstractTomEESecurityTest {
+
+    @Test
+    public void unprotectedResource_doesNotCreateSession() {
+        final Client client = ClientBuilder.newBuilder().build();
+        final Response response =
+                client.target(getAppUrl() + "/public")
+                      .request()
+                      .get();
+
+        assertEquals(200, response.getStatus());
+        assertEquals("public", response.readEntity(String.class));
+
+        final Map<String, NewCookie> cookies = response.getCookies();
+        assertNull("JSESSIONID must not be set for unprotected resources",
+                   cookies.get("JSESSIONID"));
+    }
+
+    @WebServlet(urlPatterns = "/public")
+    public static class PublicServlet extends HttpServlet {
+        @Override
+        protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
+                throws ServletException, IOException {
+            resp.getWriter().write("public");
+        }
+    }
+}
